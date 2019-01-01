5663d243be14caac6f1547e24a25cd275f83da0b
Willem Pienaar <git@willem.co>
2019-01-01 16:37:54 +0800

    Added Terraform config for setting up a testing cluster

---
+++ testing/tf/k8s-cluster/variables.tf
@@ -0,0 +1,15 @@
+variable "gcp_project" {
+  description = "GCP Project name"
+}
+
+variable "default_region" {
+  description = "Kubernetes cluster region"
+}
+
+variable "default_network" {
+  description = "GCP Project name"
+}
+
+variable "default_subnet" {
+  description = "Kubernetes cluster subnet name"
+}
---
+++ testing/tf/terraform.tfvars
@@ -0,0 +1,7 @@
+gcp_project = "kf-feast"
+
+default_network = "default"
+
+default_subnet = "default"
+
+default_region = "us-central1"
