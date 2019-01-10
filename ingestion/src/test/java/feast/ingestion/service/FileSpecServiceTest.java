89d81d8b29b6bb436e9bbce99ff889c03ba7a0d8
Pradithya Aria <pradithya.pura@go-jek.com>
2019-01-10 14:18:43 +0800

    Fix failing unit test

--- ingestion/src/test/java/feast/ingestion/service/FileSpecServiceTest.java
+++ ingestion/src/test/java/feast/ingestion/service/FileSpecServiceTest.java
@@ -17,7 +17,6 @@
 
 package feast.ingestion.service;
 
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
@@ -26,9 +25,6 @@ import com.google.common.io.Resources;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class FileSpecServiceTest {
@@ -45,14 +41,21 @@ public class FileSpecServiceTest {
   public void testGetSpecs() {
     FileSpecService service = getFileSpecService();
 
-    List<String> storageIds = Lists.newArrayList("TEST_WAREHOUSE", "TEST_ERRORS", "TEST_SERVING");
+    List<String> storageIds = Lists.newArrayList("TEST_WAREHOUSE", "TEST_SERVING");
     List<String> entities = Lists.newArrayList("testEntity");
-    List<String> featureIds = Lists
-        .newArrayList("testEntity.hour.redisString", "testEntity.none.testString",
-            "testEntity.none.redisString", "testEntity.day.testInt64", "testEntity.none.redisInt32",
-            "testEntity.none.testInt32", "testEntity.none.testInt64");
+    List<String> featureIds =
+        Lists.newArrayList(
+            "testEntity.hour.redisString",
+            "testEntity.none.testString",
+            "testEntity.none.redisString",
+            "testEntity.day.testInt64",
+            "testEntity.none.redisInt32",
+            "testEntity.none.testInt32",
+            "testEntity.none.testInt64");
     assertThat(service.getEntitySpecs(entities).keySet(), containsInAnyOrder(entities.toArray()));
-    assertThat(service.getFeatureSpecs(featureIds).keySet(), containsInAnyOrder(featureIds.toArray()));
-    assertThat(service.getStorageSpecs(storageIds).keySet(), containsInAnyOrder(storageIds.toArray()));
+    assertThat(
+        service.getFeatureSpecs(featureIds).keySet(), containsInAnyOrder(featureIds.toArray()));
+    assertThat(
+        service.getStorageSpecs(storageIds).keySet(), containsInAnyOrder(storageIds.toArray()));
   }
 }
