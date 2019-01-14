43b0752e20eaf8cb2dbc71fabab65427cfbb6eba
zhilingc <zhiling.c@go-jek.com>
2019-01-14 14:37:47 +0800

    Fix github tag regex in cloud build tf file

--- testing/tf/cloud-build/cloud-build.tf
+++ testing/tf/cloud-build/cloud-build.tf
@@ -1,8 +1,12 @@
+variable "base64tagRegex" {
+  default = "XigwfFsxLTldXFxkKilcXC4oMHxbMS05XVxcZCopXFwuKDB8WzEtOV1cXGQqKSg/Oi0oKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKD86XFwuKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKSopKT8oPzpcXCsoWzAtOWEtekEtWi1dKyg/OlxcLlswLTlhLXpBLVotXSspKikpPyQ="
+}
+
 resource "google_cloudbuild_trigger" "build_trigger" {
   project  = "${var.gcp_project}"
   trigger_template {
     branch_name = "master"
-    tag_name = "^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+    tag_name = "${base64decode(var.base64tagRegex)}"
     project     = "${var.gcp_project}"
     repo_name   = "http://github.com/gojek/feast"
   }
