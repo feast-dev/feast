b2143a22380fb69dec57acc705a3b6f794df6ff7
zhilingc <zhiling.c@go-jek.com>
2019-01-16 10:09:05 +0800

    Remove base64 variable and move it inline

--- testing/tf/cloud-build/cloud-build.tf
+++ testing/tf/cloud-build/cloud-build.tf
@@ -1,12 +1,9 @@
-variable "base64tagRegex" {
-  default = "XigwfFsxLTldXFxkKilcXC4oMHxbMS05XVxcZCopXFwuKDB8WzEtOV1cXGQqKSg/Oi0oKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKD86XFwuKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKSopKT8oPzpcXCsoWzAtOWEtekEtWi1dKyg/OlxcLlswLTlhLXpBLVotXSspKikpPyQ="
-}
 
 resource "google_cloudbuild_trigger" "build_trigger" {
   project  = "${var.gcp_project}"
   trigger_template {
     branch_name = "master"
-    tag_name = "${base64decode(var.base64tagRegex)}"
+    tag_name = "${base64decode("XigwfFsxLTldXFxkKilcXC4oMHxbMS05XVxcZCopXFwuKDB8WzEtOV1cXGQqKSg/Oi0oKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKD86XFwuKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKSopKT8oPzpcXCsoWzAtOWEtekEtWi1dKyg/OlxcLlswLTlhLXpBLVotXSspKikpPyQ=")}"
     project     = "${var.gcp_project}"
     repo_name   = "http://github.com/gojek/feast"
   }
