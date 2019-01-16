5d7735c8142456250fbdb255d29c201553a9adca
Willem Pienaar <6728866+woop@users.noreply.github.com>
2019-01-16 11:50:09 +0800

    Remove branch from cloud build trigger

--- testing/tf/cloud-build/cloud-build.tf
+++ testing/tf/cloud-build/cloud-build.tf
@@ -1,8 +1,6 @@
-
 resource "google_cloudbuild_trigger" "build_trigger" {
   project  = "${var.gcp_project}"
   trigger_template {
-    branch_name = "master"
     tag_name = "${base64decode("XigwfFsxLTldXFxkKilcXC4oMHxbMS05XVxcZCopXFwuKDB8WzEtOV1cXGQqKSg/Oi0oKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKD86XFwuKD86MHxbMS05XVxcZCp8XFxkKlthLXpBLVotXVswLTlhLXpBLVotXSopKSopKT8oPzpcXCsoWzAtOWEtekEtWi1dKyg/OlxcLlswLTlhLXpBLVotXSspKikpPyQ=")}"
     project     = "${var.gcp_project}"
     repo_name   = "http://github.com/gojek/feast"
