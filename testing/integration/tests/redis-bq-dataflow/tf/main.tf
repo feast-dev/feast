04bb2d0419807679876af2736bdb3ca6c317390d
zhilingc <zhiling.c@go-jek.com>
2019-02-28 23:59:40 +0800

    Add docker builder image, remove revision from feast module

--- testing/integration/tests/redis-bq-dataflow/tf/main.tf
+++ testing/integration/tests/redis-bq-dataflow/tf/main.tf
@@ -47,7 +47,6 @@ module "feast" {
   project_name               = "${local.project_name}"
   region                     = "${local.region}"
   subnetwork                 = "${local.subnetwork}"
-  revision                   = "master"
   docker_tag                 = "${var.docker_tag == "" ? var.revision : var.docker_tag}"
   core_address               = "10.128.0.99"
   serving_address            = "10.128.0.100"
