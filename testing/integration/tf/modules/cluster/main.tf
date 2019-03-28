a7b9a8a19646839c58e2c710478710d836cdfb57
zhilingc <zhiling.c@go-jek.com>
2019-03-28 19:42:22 +0800

    Give ability to create folders in bucket

--- testing/integration/tf/modules/cluster/main.tf
+++ testing/integration/tf/modules/cluster/main.tf
@@ -26,6 +26,7 @@ resource "google_container_cluster" "feast_k8s" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_write",
+      "https://www.googleapis.com/auth/devstorage.full_control",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/bigquery",
