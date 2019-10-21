8ef3e2a15f8a10d9b6927ab49e0ccc06abc9190e
zhilingc <zhiling.c@go-jek.com>
2019-10-21 16:55:53 +0800

    Re-implement metrics for ingestion, remove more redundant code

--- ingestion/src/main/java/feast/ingestion/transform/metrics/WriteRowMetricsDoFn.java
+++ ingestion/src/main/java/feast/ingestion/transform/metrics/WriteRowMetricsDoFn.java
@@ -1,5 +1,6 @@
 package feast.ingestion.transform.metrics;
 
+import com.google.auto.value.AutoValue;
 import feast.core.FeatureSetProto.FeatureSetSpec;
 import feast.types.FeatureRowProto.FeatureRow;
 import feast.types.FieldProto.Field;
@@ -13,23 +14,34 @@ import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.values.KV;
 
 @Slf4j
-public class WriteMetricsDoFn extends DoFn<KV<Integer, Iterable<FeatureRow>>, Void> {
+@AutoValue
+public abstract class WriteRowMetricsDoFn extends DoFn<KV<Integer, Iterable<FeatureRow>>, Void> {
 
   private final String STORE_TAG_KEY = "feast_store";
   private final String FEATURE_SET_NAME_TAG_KEY = "feast_featureSet_name";
   private final String FEATURE_SET_VERSION_TAG_KEY = "feast_featureSet_version";
   private final String FEATURE_TAG_KEY = "feast_feature_name";
-  private final String pipelineName;
-  private final String storeName;
-  private final FeatureSetSpec featureSetSpec;
-  private String pgAddress;
 
-  public WriteMetricsDoFn(String pipelineName, String storeName, FeatureSetSpec featureSetSpec,
-      String pgAddress) {
-    this.pipelineName = pipelineName;
-    this.storeName = storeName;
-    this.featureSetSpec = featureSetSpec;
-    this.pgAddress = pgAddress;
+  public abstract String getStoreName();
+
+  public abstract FeatureSetSpec getFeatureSetSpec();
+
+  public abstract String getPgAddress();
+
+  public static Builder newBuilder() {
+    return new AutoValue_WriteRowMetricsDoFn.Builder();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+
+    public abstract Builder setStoreName(String storeName);
+
+    public abstract Builder setFeatureSetSpec(FeatureSetSpec featureSetSpec);
+
+    public abstract Builder setPgAddress(String pgAddress);
+
+    public abstract WriteRowMetricsDoFn build();
   }
 
 
@@ -52,18 +64,20 @@ public class WriteMetricsDoFn extends DoFn<KV<Integer, Iterable<FeatureRow>>, Vo
         .labelNames(STORE_TAG_KEY, FEATURE_SET_NAME_TAG_KEY, FEATURE_SET_VERSION_TAG_KEY,
             FEATURE_TAG_KEY)
         .register(registry);
+
     Long currentTimestamp = System.currentTimeMillis();
+    FeatureSetSpec featureSetSpec = getFeatureSetSpec();
 
     for (FeatureRow row : c.element().getValue()) {
       long eventTimestamp = row.getEventTimestamp().getSeconds() * 1000;
       long lag = currentTimestamp - eventTimestamp;
       rowLag
-          .labels(storeName, featureSetSpec.getName(), String.valueOf(featureSetSpec.getVersion()))
+          .labels(getStoreName(), featureSetSpec.getName(), String.valueOf(featureSetSpec.getVersion()))
           .observe(lag);
       for (Field field : row.getFieldsList()) {
         if (!field.getValue().getValCase().equals(ValCase.VAL_NOT_SET)) {
           featureLag
-              .labels(storeName, featureSetSpec.getName(),
+              .labels(getStoreName(), featureSetSpec.getName(),
                   String.valueOf(featureSetSpec.getVersion()), field.getName())
               .observe(lag);
         }
@@ -71,8 +85,8 @@ public class WriteMetricsDoFn extends DoFn<KV<Integer, Iterable<FeatureRow>>, Vo
     }
 
     try {
-      PushGateway pg = new PushGateway(pgAddress);
-      pg.pushAdd(registry, pipelineName);
+      PushGateway pg = new PushGateway(getPgAddress());
+      pg.pushAdd(registry, c.getPipelineOptions().getJobName());
     } catch (IOException e) {
       log.warn("Unable to push metrics to server", e);
     }
