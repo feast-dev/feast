21fc993b3bb69711a0515e5b81e2f560db52e8ee
zhilingc <zhiling.c@go-jek.com>
2019-10-23 22:37:18 +0800

    Remove accidental imports

--- sdk/python/feast/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .client import Client
-from .entity import Entity
-from .feature_set import FeatureSet
-from .feature import Feature
-from .source import Source, KafkaSource
-from .value_type import ValueType
