9166ba142b71cd82264bbc332ab37a988daafe46
David Heryanto <david.heryanto@hotmail.com>
2019-10-23 18:26:57 +0800

    Clean up prow config

--- .prow/scripts/run_unit_test.sh
+++ .prow/scripts/run_unit_test.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 # This script will run unit test for a specific Feast component:
-# - core, ingestion, serving or cli
+# - core, serving and [python/java/go]-sdk
 #
-# This script includes the pre and post test scripts, such as
-# - downloading maven cache repository
-# - saving the test output report so it can be viewed with Spyglass in Prow
-
-# Bucket in GCS used for running unit tests, when the unit tests need an 
-# actual running GCS (e.g. because there is no existing mock implementation of the function to test)
-TEST_BUCKET=feast-templocation-kf-feast
+# This script includes the pre and post test scripts, such as:
+# - Download cached Maven packages for faster tests
+# - Saving the test output report so it can be viewed with Spyglass in Prow.
+#   By default, the configured log path is "/logs" and test artifacts should
+#   be saved to "/logs/artifacts" directory.
 
 usage()
 {
@@ -31,33 +29,53 @@ fi
 
 . .prow/scripts/install_google_cloud_sdk.sh
 
-if [[ ${COMPONENT} == "core" ]] || [[ ${COMPONENT} == "ingestion" ]] || [[ ${COMPONENT} == "serving" ]]; then
+if [[ ${COMPONENT} == "serving" ]]; then
 
-    .prow/scripts/prepare_maven_cache.sh --archive-uri gs://feast-templocation-kf-feast/.m2.tar --output-dir /root/
-    mvn --projects ${COMPONENT} -Dtestbucket=feast-templocation-kf-feast test
-    TEST_EXIT_CODE=$?
-    cp -r ${COMPONENT}/target/surefire-reports /logs/artifacts/surefire-reports
+  .prow/scripts/prepare_maven_cache.sh \
+    --archive-uri gs://feast-templocation-kf-feast/.m2.tar --output-dir /root/
 
-elif [[ ${COMPONENT} == "cli" ]]; then
+  mvn --define skipTests=true --projects serving install
+  mvn --projects serving test
 
-    # https://stackoverflow.com/questions/6871859/piping-command-output-to-tee-but-also-save-exit-code-of-command
-    set -o pipefail
+  TEST_EXIT_CODE=$?
+  cp -r serving/target/surefire-reports /logs/artifacts/surefire-reports
 
-    go get -u github.com/jstemmer/go-junit-report
-    go test -v ./cli/feast/... 2>&1 | tee test_output
-    TEST_EXIT_CODE=$?
-    cat test_output | ${GOPATH}/bin/go-junit-report > ${ARTIFACTS}/unittest-cli-report.xml
+elif [[ ${COMPONENT} == "core" ]]; then
+
+  .prow/scripts/prepare_maven_cache.sh \
+    --archive-uri gs://feast-templocation-kf-feast/.m2.tar --output-dir /root/
+
+  # Core depends on Ingestion so they are tested together
+  mvn --define skipTests=true --projects core,ingestion install
+  mvn --projects core,ingestion test
+
+  TEST_EXIT_CODE=$?
+  cp -r core/target/surefire-reports /logs/artifacts/surefire-reports
+  cp -r ingestion/target/surefire-reports /logs/artifacts/surefire-reports
+
+elif [[ ${COMPONENT} == "java-sdk" ]]; then
+
+  .prow/scripts/prepare_maven_cache.sh \
+    --archive-uri gs://feast-templocation-kf-feast/.m2.tar --output-dir /root/
+
+  # Core depends on Ingestion so they are tested together
+  mvn --define skipTests=true --projects core,ingestion install
+  mvn --projects core,ingestion test
+
+  TEST_EXIT_CODE=$?
+  cp -r core/target/surefire-reports /logs/artifacts/surefire-reports
+  cp -r ingestion/target/surefire-reports /logs/artifacts/surefire-reports
 
 elif [[ ${COMPONENT} == "python-sdk" ]]; then
 
-    cd sdk/python
-    pip install -r requirements-test.txt
-    pip install .
-    pytest ./tests --junitxml=${ARTIFACTS}/unittest-pythonsdk-report.xml
-    TEST_EXIT_CODE=$?
+  cd sdk/python
+  pip install -r requirements-test.txt
+  pip install .
+  python -m pytest --junitxml=/logs/artifacts/unittest-pythonsdk-report.xml
+  TEST_EXIT_CODE=$?
 
 else
-    usage; exit 1
+  usage; exit 1
 fi
 
 exit ${TEST_EXIT_CODE}
