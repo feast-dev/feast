66d2f6f8edd904ad833a70d77fd1565ca6ae6f20
David Heryanto <david.heryanto@hotmail.com>
2019-10-25 00:05:04 +0800

    Fix incorrect order for java-sdk test script

--- .prow/scripts/run_test.sh
+++ .prow/scripts/run_test.sh
@@ -70,8 +70,9 @@ elif [[ ${COMPONENT} == "java-sdk" ]]; then
   .prow/scripts/prepare_maven_cache.sh \
     --archive-uri gs://feast-templocation-kf-feast/.m2.2019-10-24.tar --output-dir /root/
 
+  # Skip Maven enforcer: https://stackoverflow.com/questions/50647223/maven-enforcer-issue-when-running-from-reactor-level
   mvn --projects sdk/java --batch-mode --define skipTests=true --define enforcer.skip=true clean install
-  mvn --projects --define enforcer.skip=true sdk/java test
+  mvn --projects sdk/java --define enforcer.skip=true test
   TEST_EXIT_CODE=$?
 
   cp -r sdk/java/target/surefire-reports ${LOGS_ARTIFACT_PATH}/surefire-reports
