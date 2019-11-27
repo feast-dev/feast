d7ea74a256268599d8899031000340f0d15a03c8
Ches Martin <ches@whiskeyandgrits.net>
2019-11-27 14:19:50 +0700

    Enable Apache license header in Spotless for .java

--- serving/src/main/java/feast/serving/service/RedisServingService.java
+++ serving/src/main/java/feast/serving/service/RedisServingService.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2019 The Feast Authors
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2018-2019 The Feast Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package feast.serving.service;
 
 import static feast.serving.util.Metrics.missingKeyCount;
