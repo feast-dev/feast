747bc3a9dea28aff3fdafab7ce3241fea0ffbd41
Chen Zhiling <zhiling.c@go-jek.com>
2019-11-28 11:03:44 +0800

    Remove metrics windowing, clean up step names for metrics writing (#334)
    * Remove metrics windowing, clean up step names
    
    * Remove unused variables

--- ingestion/src/main/java/feast/ingestion/transform/metrics/WriteMetricsTransform.java
+++ ingestion/src/main/java/feast/ingestion/transform/metrics/WriteMetricsTransform.java
@@ -1,7 +1,6 @@
 package feast.ingestion.transform.metrics;
 
 import com.google.auto.value.AutoValue;
-import feast.core.FeatureSetProto.FeatureSetSpec;
 import feast.ingestion.options.ImportOptions;
 import feast.ingestion.values.FailedElement;
 import feast.types.FeatureRowProto.FeatureRow;
@@ -16,9 +15,6 @@ import org.slf4j.Logger;
 
 @AutoValue
 public abstract class WriteMetricsTransform extends PTransform<PCollectionTuple, PDone> {
-
-  private static final long WINDOW_SIZE_SECONDS = 15;
-  private static final Logger log = org.slf4j.LoggerFactory.getLogger(WriteMetricsTransform.class);
 
   public abstract String getStoreName();
 
@@ -49,9 +45,7 @@ public abstract class WriteMetricsTransform extends PTransform<PCollectionTuple,
       case "statsd":
 
         input.get(getFailureTag())
-            .apply("Window records",
-                new WindowRecords<>(WINDOW_SIZE_SECONDS))
-            .apply("Write deadletter metrics", ParDo.of(
+            .apply("WriteDeadletterMetrics", ParDo.of(
                 WriteDeadletterRowMetricsDoFn.newBuilder()
                     .setStatsdHost(options.getStatsdHost())
                     .setStatsdPort(options.getStatsdPort())
@@ -59,9 +53,7 @@ public abstract class WriteMetricsTransform extends PTransform<PCollectionTuple,
                     .build()));
 
         input.get(getSuccessTag())
-            .apply("Window records",
-                new WindowRecords<>(WINDOW_SIZE_SECONDS))
-            .apply("Write row metrics", ParDo
+            .apply("WriteRowMetrics", ParDo
                 .of(WriteRowMetricsDoFn.newBuilder()
                     .setStatsdHost(options.getStatsdHost())
                     .setStatsdPort(options.getStatsdPort())
