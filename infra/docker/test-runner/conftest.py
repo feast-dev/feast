58bc6ce345fd13f1945c03b930cf797f51471d60
Shu Heng <khor.heng@go-jek.com>
2019-11-28 18:04:39 +0530

    Add end to end test runner

---
+++ infra/docker/test-runner/conftest.py
@@ -0,0 +1,4 @@
+def pytest_addoption(parser):
+    parser.addoption("--core_url", action="store", default="core:6565")
+    parser.addoption("--serving_url", action="store", default="serving:6566")
+    parser.addoption("--allow_dirty", action="store", default="True")
