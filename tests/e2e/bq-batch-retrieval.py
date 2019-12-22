5601a09aa54197167a4c2270881c253b796f2b47
voonhous <voonhousu@gmail.com>
2019-12-22 16:11:29 +0800

    Added support to accept local avro files, GCS avro files and GCS wildcard paths (#375)

--- tests/e2e/bq-batch-retrieval.py
+++ tests/e2e/bq-batch-retrieval.py
@@ -2,6 +2,7 @@ import random
 import time
 from datetime import datetime
 from datetime import timedelta
+from urllib.parse import urlparse
 
 import numpy as np
 import pandas as pd
@@ -12,7 +13,9 @@ from feast.entity import Entity
 from feast.feature import Feature
 from feast.feature_set import FeatureSet
 from feast.type_map import ValueType
+from google.cloud import storage
 from google.protobuf.duration_pb2 import Duration
+from pandavro import to_avro
 
 pd.set_option('display.max_columns', None)
 
@@ -32,6 +35,11 @@ def allow_dirty(pytestconfig):
 
 
 @pytest.fixture(scope="module")
+def gcs_path(pytestconfig):
+    return pytestconfig.getoption("gcs_path")
+
+
+@pytest.fixture(scope="module")
 def client(core_url, serving_url, allow_dirty):
     # Get client for core and serving
     client = Client(core_url=core_url, serving_url=serving_url)
@@ -43,6 +51,94 @@ def client(core_url, serving_url, allow_dirty):
             raise Exception("Feast cannot have existing feature sets registered. Exiting tests.")
 
     return client
+
+
+def test_get_batch_features_with_file(client):
+    file_fs1 = FeatureSet(
+        "file_feature_set",
+        features=[Feature("feature_value", ValueType.STRING)],
+        entities=[Entity("entity_id", ValueType.INT64)],
+        max_age=Duration(seconds=100),
+    )
+
+    client.apply(file_fs1)
+    file_fs1 = client.get_feature_set(name="file_feature_set", version=1)
+
+    N_ROWS = 10
+    time_offset = datetime.utcnow().replace(tzinfo=pytz.utc)
+    features_1_df = pd.DataFrame(
+        {
+            "datetime": [time_offset] * N_ROWS,
+            "entity_id": [i for i in range(N_ROWS)],
+            "feature_value": [f"{i}" for i in range(N_ROWS)],
+        }
+    )
+    client.ingest(file_fs1, features_1_df)
+
+    # Rename column (datetime -> event_timestamp)
+    features_1_df = features_1_df.rename(columns={"datetime": "event_timestamp"})
+
+    to_avro(df=features_1_df, file_path_or_buffer="file_feature_set.avro")
+
+    feature_retrieval_job = client.get_batch_features(
+        entity_rows="file://file_feature_set.avro", feature_ids=["file_feature_set:1:feature_value"]
+    )
+
+    output = feature_retrieval_job.to_dataframe()
+    print(output.head())
+
+    assert output["entity_id"].to_list() == [int(i) for i in output["file_feature_set_v1_feature_value"].to_list()]
+
+
+def test_get_batch_features_with_gs_path(client, gcs_path):
+    gcs_fs1 = FeatureSet(
+        "gcs_feature_set",
+        features=[Feature("feature_value", ValueType.STRING)],
+        entities=[Entity("entity_id", ValueType.INT64)],
+        max_age=Duration(seconds=100),
+    )
+
+    client.apply(gcs_fs1)
+    gcs_fs1 = client.get_feature_set(name="gcs_feature_set", version=1)
+
+    N_ROWS = 10
+    time_offset = datetime.utcnow().replace(tzinfo=pytz.utc)
+    features_1_df = pd.DataFrame(
+        {
+            "datetime": [time_offset] * N_ROWS,
+            "entity_id": [i for i in range(N_ROWS)],
+            "feature_value": [f"{i}" for i in range(N_ROWS)],
+        }
+    )
+    client.ingest(gcs_fs1, features_1_df)
+
+    # Rename column (datetime -> event_timestamp)
+    features_1_df = features_1_df.rename(columns={"datetime": "event_timestamp"})
+
+    # Output file to local
+    file_name = "gcs_feature_set.avro"
+    to_avro(df=features_1_df, file_path_or_buffer=file_name)
+
+    uri = urlparse(gcs_path)
+    bucket = uri.hostname
+    ts = int(time.time())
+    remote_path = str(uri.path).strip("/") + f"{ts}/{file_name}"
+
+    # Upload file to gcs
+    storage_client = storage.Client(project=None)
+    bucket = storage_client.get_bucket(bucket)
+    blob = bucket.blob(remote_path)
+    blob.upload_from_filename(file_name)
+
+    feature_retrieval_job = client.get_batch_features(
+        entity_rows=f"{gcs_path}{ts}/*",
+        feature_ids=["gcs_feature_set:1:feature_value"]
+    )
+
+    output = feature_retrieval_job.to_dataframe()
+    print(output.head())
+
+    assert output["entity_id"].to_list() == [int(i) for i in output["gcs_feature_set_v1_feature_value"].to_list()]
 
 
 def test_order_by_creation_time(client):
