4aedd7c82afeb7edd8bb0e7b59bfe2e658acaec1
David Heryanto <david.heryanto@hotmail.com>
2020-01-21 18:50:20 +0800

    Add metric for feature_value, cleanup tags - Ignore StatsD execption since it is not gonna be thrown when using non blocking StatsD client: https://github.com/DataDog/java-dogstatsd-client#unix-domain-socket-support - Reuse tags variables - Add validation for correct feature set reference in FeatureRow

--- ingestion/src/main/java/feast/ingestion/transform/metrics/WriteRowMetricsDoFn.java
+++ ingestion/src/main/java/feast/ingestion/transform/metrics/WriteRowMetricsDoFn.java
@@ -17,27 +17,28 @@
 package feast.ingestion.transform.metrics;
 
 import com.google.auto.value.AutoValue;
+import com.google.protobuf.util.Timestamps;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
 import com.timgroup.statsd.StatsDClientException;
 import feast.types.FeatureRowProto.FeatureRow;
 import feast.types.FieldProto.Field;
-import feast.types.ValueProto.Value.ValCase;
+import feast.types.ValueProto.Value;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.slf4j.Logger;
 
 @AutoValue
 public abstract class WriteRowMetricsDoFn extends DoFn<FeatureRow, Void> {
 
-  private static final Logger log = org.slf4j.LoggerFactory.getLogger(WriteRowMetricsDoFn.class);
+  private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(WriteRowMetricsDoFn.class);
 
-  private final String METRIC_PREFIX = "feast_ingestion";
-  private final String STORE_TAG_KEY = "feast_store";
-  private final String FEATURE_SET_PROJECT_TAG_KEY = "feast_project_name";
-  private final String FEATURE_SET_NAME_TAG_KEY = "feast_featureSet_name";
-  private final String FEATURE_SET_VERSION_TAG_KEY = "feast_featureSet_version";
-  private final String FEATURE_TAG_KEY = "feast_feature_name";
-  private final String INGESTION_JOB_NAME_KEY = "ingestion_job_name";
+  private static final String METRIC_PREFIX = "feast_ingestion";
+  private static final String STORE_TAG_KEY = "feast_store";
+  private static final String FEATURE_SET_PROJECT_TAG_KEY = "feast_project_name";
+  private static final String FEATURE_SET_NAME_TAG_KEY = "feast_featureSet_name";
+  private static final String FEATURE_SET_VERSION_TAG_KEY = "feast_featureSet_version";
+  private static final String FEATURE_TAG_KEY = "feast_feature_name";
+  private static final String INGESTION_JOB_NAME_KEY = "ingestion_job_name";
 
   public abstract String getStoreName();
 
@@ -45,8 +46,8 @@ public abstract class WriteRowMetricsDoFn extends DoFn<FeatureRow, Void> {
 
   public abstract int getStatsdPort();
 
-  public static WriteRowMetricsDoFn create(
-      String newStoreName, String newStatsdHost, int newStatsdPort) {
+  public static WriteRowMetricsDoFn create(String newStoreName, String newStatsdHost,
+      int newStatsdPort) {
     return newBuilder()
         .setStoreName(newStoreName)
         .setStatsdHost(newStatsdHost)
@@ -74,74 +75,95 @@ public abstract class WriteRowMetricsDoFn extends DoFn<FeatureRow, Void> {
 
   @Setup
   public void setup() {
-    statsd = new NonBlockingStatsDClient(METRIC_PREFIX, getStatsdHost(), getStatsdPort());
+    try {
+      statsd = new NonBlockingStatsDClient(METRIC_PREFIX, getStatsdHost(), getStatsdPort());
+    } catch (StatsDClientException e) {
+      LOG.warn("Failed to create StatsD client");
+    }
   }
 
   @ProcessElement
   public void processElement(ProcessContext c) {
+    if (statsd == null) {
+      LOG.warn(
+          "No StatsD client available (maybe it failed to initialize). No FeatureRow metrics will be sent.");
+      return;
+    }
 
-    try {
-      FeatureRow row = c.element();
-      long eventTimestamp = com.google.protobuf.util.Timestamps.toMillis(row.getEventTimestamp());
+    FeatureRow row = c.element();
+    String[] colonSplits = row.getFeatureSet().split(":");
+    if (colonSplits.length < 1) {
+      LOG.warn(
+          "FeatureRow an invalid FeatureSet reference: " + row.getFeatureSet()
+              + ". Expected format: PROJECT/FEATURE_SET:VERSION");
+      return;
+    }
+    String[] slashSplits = colonSplits[0].split("/");
+    if (slashSplits.length < 2) {
+      LOG.warn(
+          "FeatureRow an invalid FeatureSet reference: " + row.getFeatureSet()
+              + ". Expected format: PROJECT/FEATURE_SET:VERSION");
+      return;
+    }
 
-      String[] split = row.getFeatureSet().split(":");
-      String featureSetProject = split[0].split("/")[0];
-      String featureSetName = split[0].split("/")[1];
-      String featureSetVersion = split[1];
+    String featureSetProject = slashSplits[0];
+    String featureSetName = slashSplits[1];
+    String featureSetVersion = colonSplits[1];
+    String[] tags = new String[]{
+        STORE_TAG_KEY + ":" + getStoreName(),
+        FEATURE_SET_PROJECT_TAG_KEY + ":" + featureSetProject,
+        FEATURE_SET_NAME_TAG_KEY + ":" + featureSetName,
+        FEATURE_SET_VERSION_TAG_KEY + ":" + featureSetVersion,
+        INGESTION_JOB_NAME_KEY + ":" + c.getPipelineOptions().getJobName()
+    };
+    long eventTimestamp = Timestamps.toMillis(row.getEventTimestamp());
 
-      statsd.histogram(
-          "feature_row_lag_ms",
-          System.currentTimeMillis() - eventTimestamp,
+    statsd.histogram("feature_row_lag_ms", System.currentTimeMillis() - eventTimestamp, tags);
+    statsd.histogram("feature_row_event_time_epoch_ms", eventTimestamp, tags);
+    statsd.count("feature_row_ingested_count", 1, tags);
+
+    // Feature value metrics will be used for validation
+    for (Field field : row.getFieldsList()) {
+      tags = new String[]{
           STORE_TAG_KEY + ":" + getStoreName(),
           FEATURE_SET_PROJECT_TAG_KEY + ":" + featureSetProject,
           FEATURE_SET_NAME_TAG_KEY + ":" + featureSetName,
           FEATURE_SET_VERSION_TAG_KEY + ":" + featureSetVersion,
-          INGESTION_JOB_NAME_KEY + ":" + c.getPipelineOptions().getJobName());
+          FEATURE_TAG_KEY + ":" + field.getName(),
+          INGESTION_JOB_NAME_KEY + ":" + c.getPipelineOptions().getJobName()
+      };
 
-      statsd.histogram(
-          "feature_row_event_time_epoch_ms",
-          eventTimestamp,
-          STORE_TAG_KEY + ":" + getStoreName(),
-          FEATURE_SET_PROJECT_TAG_KEY + ":" + featureSetProject,
-          FEATURE_SET_NAME_TAG_KEY + ":" + featureSetName,
-          FEATURE_SET_VERSION_TAG_KEY + ":" + featureSetVersion,
-          INGESTION_JOB_NAME_KEY + ":" + c.getPipelineOptions().getJobName());
-
-      for (Field field : row.getFieldsList()) {
-        if (!field.getValue().getValCase().equals(ValCase.VAL_NOT_SET)) {
-          statsd.histogram(
-              "feature_value_lag_ms",
-              System.currentTimeMillis() - eventTimestamp,
-              STORE_TAG_KEY + ":" + getStoreName(),
-              FEATURE_SET_PROJECT_TAG_KEY + ":" + featureSetProject,
-              FEATURE_SET_NAME_TAG_KEY + ":" + featureSetName,
-              FEATURE_SET_VERSION_TAG_KEY + ":" + featureSetVersion,
-              FEATURE_TAG_KEY + ":" + field.getName(),
-              INGESTION_JOB_NAME_KEY + ":" + c.getPipelineOptions().getJobName());
-        } else {
-          statsd.count(
-              "feature_value_missing_count",
-              1,
-              STORE_TAG_KEY + ":" + getStoreName(),
-              FEATURE_SET_PROJECT_TAG_KEY + ":" + featureSetProject,
-              FEATURE_SET_NAME_TAG_KEY + ":" + featureSetName,
-              FEATURE_SET_VERSION_TAG_KEY + ":" + featureSetVersion,
-              FEATURE_TAG_KEY + ":" + field.getName(),
-              INGESTION_JOB_NAME_KEY + ":" + c.getPipelineOptions().getJobName());
-        }
+      Value val = field.getValue();
+      switch (val.getValCase()) {
+        case INT32_VAL:
+          statsd.histogram("feature_value", val.getInt32Val(), tags);
+          break;
+        case INT64_VAL:
+          statsd.histogram("feature_value", val.getInt64Val(), tags);
+          break;
+        case DOUBLE_VAL:
+          statsd.histogram("feature_value", val.getDoubleVal(), tags);
+          break;
+        case FLOAT_VAL:
+          statsd.histogram("feature_value", val.getFloatVal(), tags);
+          break;
+        case BOOL_VAL:
+          statsd.histogram("feature_value", val.getBoolVal() ? 1 : 0, tags);
+          break;
+        case BYTES_VAL:
+        case STRING_VAL:
+        case BYTES_LIST_VAL:
+        case FLOAT_LIST_VAL:
+        case STRING_LIST_VAL:
+        case INT32_LIST_VAL:
+        case INT64_LIST_VAL:
+        case DOUBLE_LIST_VAL:
+        case BOOL_LIST_VAL:
+          break;
+        case VAL_NOT_SET:
+          statsd.count("feature_value_missing_count", 1, tags);
+          break;
       }
-
-      statsd.count(
-          "feature_row_ingested_count",
-          1,
-          STORE_TAG_KEY + ":" + getStoreName(),
-          FEATURE_SET_PROJECT_TAG_KEY + ":" + featureSetProject,
-          FEATURE_SET_NAME_TAG_KEY + ":" + featureSetName,
-          FEATURE_SET_VERSION_TAG_KEY + ":" + featureSetVersion,
-          INGESTION_JOB_NAME_KEY + ":" + c.getPipelineOptions().getJobName());
-
-    } catch (StatsDClientException e) {
-      log.warn("Unable to push metrics to server", e);
     }
   }
 }
