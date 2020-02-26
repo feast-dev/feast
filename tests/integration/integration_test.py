aaff4b9ddf26082ed392b576201ae262c16d4f15
grace.christina <grace.christina@go-jek.com>
2020-02-26 11:18:58 +0800

    Modify integration test to remove the roles concept

--- tests/integration/integration_test.py
+++ tests/integration/integration_test.py
@@ -2,6 +2,8 @@ import uuid
 import pytest
 from feast import Client
 
+PROJECT_USER = "user_" + uuid.uuid4().hex.upper()[0:6]
+PROJECT_NAME = "basic_" + uuid.uuid4().hex.upper()[0:6]
 
 @pytest.fixture(scope="module")
 def core_url(pytestconfig):
@@ -30,76 +32,59 @@ def client(core_url, serving_url, allow_dirty):
             raise Exception(
                 "Feast cannot have existing feature sets registered. Exiting tests."
             )
-
     return client
 
 @pytest.mark.integration
-def test_client_add_list_member(client):
-    client.create_project(project="customer_satisfaction")
-    client.set_project(project="customer_satisfaction")
+def test_client_project(client):
+    # Create new project
+    client.create_project(PROJECT_NAME)
 
-    # User does not have access to project: customer_satisfaction
+    # List project and check if the new project is available
+    assert PROJECT_NAME in client.list_projects();
+
+    # Archive the project
+    client.set_project(project=PROJECT_NAME)
+    client.archive_project(PROJECT_NAME)
+
+    # Check that the project is already archived
+    assert PROJECT_NAME not in client.list_projects();
+
+@pytest.mark.integration
+def test_client_project_members(client):
+    # Create new project
+    client.create_project(PROJECT_NAME)
+    client.set_project(project=PROJECT_NAME)
+
+    # Add member to the new project
+    client.add_member(user=PROJECT_USER, project=PROJECT_NAME)
+    assert PROJECT_USER in client.list_members(project=PROJECT_NAME)
+
+    # Remove member from the project
+    client.remove_member(user=PROJECT_USER, project=PROJECT_NAME)
+    assert PROJECT_USER not in client.list_members(project=PROJECT_NAME)
+
+
+@pytest.mark.integration
+def test_client_non_existent_project_and_member(client):
+    # List member from the non existent project
     with pytest.raises(Exception):
-      assert client.list_members(project="customer_satisfaction")
+        client.list_members(PROJECT_NAME)
 
-    # User is granted access to project: customer_satisfaction
-    client.add_member(user="kelly", project="customer_satisfaction")
-    assert client.list_members() is not None
+    # Create new project
+    client.create_project(project=PROJECT_NAME)
+    client.set_project(project=PROJECT_NAME)
 
-    # Check that the user is added to the members's list of the active project
-    members_list = client.list_members("customer_satisfaction")
-    assert "kelly" in members_list
-
-@pytest.mark.integration
-def test_client_remove_list_member(client):
-    client.set_project(project="customer_satisfaction")
-
-    # User has access to list members in the project: customer_satisfaction
-    assert client.list_members(project="customer_satisfaction") is not None
-
-    # User does not have access to project: customer_satisfaction
-    client.remove_member(user="kelly", project="customer_satisfaction")
+    # Remove member that is not registered in the project
     with pytest.raises(Exception):
-      assert client.list_members()
-
-    # Check that the user is removed from the members's list of the active project
-    members_list_updated = client.list_members("customer_satisfaction")
-    assert "kelly" not in members_list_updated
+        client.remove_member(user=PROJECT_USER, project=PROJECT_NAME)
 
 @pytest.mark.integration
-def test_client_list_projects(client):
-    # User does not have access to list active projects
+def test_client_add_same_member_multiple_times(client):
+    # Create new project
+    client.create_project(PROJECT_NAME)
+    client.set_project(PROJECT_NAME)
+
+    # Add the same member to the project multiple times
+    client.add_member(user=PROJECT_USER, project=PROJECT_NAME)
     with pytest.raises(Exception):
-        client.list_projects()
-
-    # User has access to archive project: customer_satisfaction
-    client.add_member(user="kelly", project="customer_satisfaction")
-    assert client.list_projects()
-
-
-@pytest.mark.integration
-def test_client_archive_project(client):
-    client.set_project(project="customer_satisfaction")
-
-    # User does not have access to archive project: customer_satisfaction
-    client.remove_member(user="kelly", project="customer_satisfaction")
-    with pytest.raises(Exception):
-        client.archive_project("customer_satisfaction")
-
-    # User has access to archive project: customer_satisfaction
-    client.add_member(user="kelly", project="customer_satisfaction")
-    client.archive_project("customer_satisfaction")
-
-    # Check that the project is removed from the active Feast projects list
-    projects_list_updated = client.list_projects()
-    assert "customer_satisfaction" not in projects_list_updated
-
-
-@pytest.mark.integration
-def test_client_set_project(client):
-    # TODO: Set project to the non-existent project
-    pass
-
-@pytest.mark.integration
-def test_client_create_project(client):
-    pass
+        client.add_member(user=PROJECT_USER, project=PROJECT_NAME)
