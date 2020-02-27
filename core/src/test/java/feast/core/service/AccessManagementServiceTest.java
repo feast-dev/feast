0db6466fcf19cafd9abd0160d1ae1f0d8501229f
grace.christina <grace.christina@go-jek.com>
2020-02-28 07:40:37 +0800

    Add unit tests for archive project, list project and list members

--- core/src/test/java/feast/core/service/AccessManagementServiceTest.java
+++ core/src/test/java/feast/core/service/AccessManagementServiceTest.java
@@ -2,6 +2,7 @@ package feast.core.service;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -10,7 +11,14 @@ import static org.mockito.MockitoAnnotations.initMocks;
 import feast.core.dao.ProjectRepository;
 import feast.core.dao.UserRepository;
 import feast.core.model.Project;
+import feast.core.model.User;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import org.apache.avro.generic.GenericData.Array;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -28,6 +36,8 @@ public class AccessManagementServiceTest {
   @Mock
   private UserRepository userRepository;
 
+
+
   @Rule
   public final ExpectedException expectedException = ExpectedException.none();
 
@@ -43,7 +53,6 @@ public class AccessManagementServiceTest {
   public void shouldCreateProjectIfItDoesntExist() {
     String project_name = "project1";
     Project project = new Project(project_name);
-
     when(projectRepository.saveAndFlush(any(Project.class))).thenReturn(project);
     accessManagementService.createProject(project_name);
     verify(projectRepository, times(1)).saveAndFlush(any());
@@ -52,21 +61,45 @@ public class AccessManagementServiceTest {
   @Test(expected = IllegalArgumentException.class)
   public void shouldNotCreateProjectIfItExist(){
     String project_name = "project1";
-
     when(projectRepository.existsById(project_name)).thenReturn(true);
     accessManagementService.createProject(project_name);
   }
 
   @Test
-  public void archiveProject() {
+  public void shouldArchiveProjectIfItExists(){
+    String project_name = "project1";
+    when(projectRepository.findById(project_name)).thenReturn(Optional.of(new Project(project_name)));
+    accessManagementService.archiveProject(project_name);
+    verify(projectRepository, times(1)).saveAndFlush(any(Project.class));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void shouldNotArchiveProjectIfItIsAlreadyArchived(){
+    String project_name = "project1";
+    when(projectRepository.findById(project_name)).thenReturn(Optional.empty());
+    accessManagementService.archiveProject(project_name);
   }
 
   @Test
-  public void listProjects() {
+  public void shouldListProjects() {
+    String project_name = "project1";
+    Project project = new Project(project_name);
+    List<Project> expected = Arrays.asList(project);
+    when(projectRepository.findAllByArchivedIsFalse()).thenReturn(expected);
+    List<Project> actual = accessManagementService.listProjects();
+    Assert.assertEquals(expected, actual);
   }
 
   @Test
-  public void listMembers() {
+  public void shouldListMembers() {
+    String project_name = "project1";
+    User user = new User("user1");
+    Set<User> expected = new HashSet<>(Arrays.asList(user));
+    Project mockProject = mock(Project.class);
+    when(projectRepository.findById(project_name)).thenReturn(Optional.of(mockProject));
+    when(mockProject.getProjectMembers()).thenReturn(new HashSet<>(Arrays.asList(user)));
+    Set<User> actual = accessManagementService.listMembers(project_name);
+    Assert.assertEquals(expected, actual);
   }
 
   @Test
