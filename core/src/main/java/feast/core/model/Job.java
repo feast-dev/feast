643f0429fcf156a4f21517c4e2aec1ffd59dff05
Zhu Zhanyan <zhu.zhanyan@gojek.com>
2020-03-12 11:35:21 +0800

    Added hashCode() & equals() to Job model compare and hash jobs
    This would allow jobs to be used as elements in HashSets and keys in
    HashMaps

--- core/src/main/java/feast/core/model/Job.java
+++ core/src/main/java/feast/core/model/Job.java
@@ -102,4 +102,39 @@ public class Job extends AbstractTimestampEntity {
   public String getSinkName() {
     return store.getName();
   }
+
+  // compute hash for a job
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
+
+  // comparing jobs - true equal, false otherwise
+
+  @Override
+  public boolean equals(Object obj) {
+    if(this == obj) {
+      return true;
+    }
+    if(obj == null) {
+      return false;
+    }
+    if(getClass() != obj.getClass()) {
+      return false;
+    }
+
+    Job other = (Job) obj;
+    if(id == null) {
+      if(other.id != null) {
+        return false;
+      }
+    } else if(!id.equals(other.id)) {
+      return false;
+    }
+    return true;
+  }
+  
 }
