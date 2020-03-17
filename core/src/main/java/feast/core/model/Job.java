06b84dfbb8a5ea83271cdfbd3b22c44aaada472c
Zhu Zhanyan <zhu.zhanyan@gojek.com>
2020-03-17 17:10:53 +0800

    Revert "Added hashCode() & equals() to Job model compare and hash jobs"
    This reverts commit ba995bb712cbd38f0f4bef47efbe433d5ec07521 as
    it caused core tests to fail

--- core/src/main/java/feast/core/model/Job.java
+++ core/src/main/java/feast/core/model/Job.java
@@ -156,37 +156,4 @@ public class Job extends AbstractTimestampEntity {
 
     return ingestJob;
   }
-
-  // compute hash for a job
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
-
-  // comparing jobs - true equal, false otherwise
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (getClass() != obj.getClass()) {
-      return false;
-    }
-
-    Job other = (Job) obj;
-    if (id == null) {
-      if (other.id != null) {
-        return false;
-      }
-    } else if (!id.equals(other.id)) {
-      return false;
-    }
-    return true;
-  }
 }
