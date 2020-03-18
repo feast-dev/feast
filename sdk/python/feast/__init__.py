225a048a7d2f7b70b35e90f8a57d7334eb8ab2cf
Willem Pienaar <6728866+woop@users.noreply.github.com>
2020-03-18 21:04:31 +0800

    Add __all__ to export

--- sdk/python/feast/__init__.py
+++ sdk/python/feast/__init__.py
@@ -12,3 +12,5 @@ from .feature_set import FeatureSet
 from .feature import Feature
 from .source import Source, KafkaSource
 from .value_type import ValueType
+
+__all__ = ['Client','Entity','FeatureSet','Feature','KafkaSource','ValueType',]
