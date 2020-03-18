fc5e54798014ee199d68f493da1300a68d0f776e
Willem Pienaar <6728866+woop@users.noreply.github.com>
2020-03-18 20:16:32 +0800

    Add exports back to __init__.py

---
+++ sdk/python/feast/__init__.py
@@ -0,0 +1,14 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+from .client import Client
+from .entity import Entity
+from .feature_set import FeatureSet
+from .feature import Feature
+from .source import Source, KafkaSource
+from .value_type import ValueType
