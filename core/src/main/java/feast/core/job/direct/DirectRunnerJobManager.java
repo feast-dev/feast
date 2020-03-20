f320e3898d3f0405d3212d26590eaf3220b0f414
Zhu Zhanyan <zhu.zhanyan@gojek.com>
2020-03-20 22:45:52 +0800

    Added restartJob() to JobManagers to restart ingestion/import jobs

--- core/src/main/java/feast/core/job/direct/DirectRunnerJobManager.java
+++ core/src/main/java/feast/core/job/direct/DirectRunnerJobManager.java
@@ -156,6 +156,27 @@ public class DirectRunnerJobManager implements JobManager {
   public PipelineResult runPipeline(ImportOptions pipelineOptions) throws IOException {
     return ImportJob.runPipeline(pipelineOptions);
   }
+  
+  /**
+   * Restart a direct runner job.
+   * Note that some data will be temporarily lost during when restarting running
+   * direct runner jobs. See {#link {@link #updateJob(Job)} for more info.
+   *
+   * @param job job to restart
+   * @return the restarted job
+  */
+  @Override
+  public Job restartJob(Job job) {
+    JobStatus status = job.getStatus();
+    if (JobStatus.getTerminalState().contains(status)) {
+      // job yet not running: just start job
+      return this.startJob(job);
+    } else { 
+      // job is running - updating the job without changing the job has 
+      // the effect of restarting the job.
+      return this.updateJob(job);
+    }
+  }
 
   /**
    * Gets the state of the direct runner job. Direct runner jobs only have 2 states: RUNNING and
