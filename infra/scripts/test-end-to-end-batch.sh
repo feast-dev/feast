d0e39f63edcd01dfcbb4f17b2b519cfb62106396
Khor Shu Heng <khor.heng@gojek.com>
2020-04-07 16:48:57 +0800

    Move ci scripts under infra from .prow

--- infra/scripts/test-end-to-end-batch.sh
+++ infra/scripts/test-end-to-end-batch.sh
@@ -90,7 +90,7 @@ if [[ ${SKIP_BUILD_JARS} != "true" ]]; then
   ============================================================
   "
 
-  .prow/scripts/download-maven-cache.sh \
+  infra/scripts/download-maven-cache.sh \
       --archive-uri gs://feast-templocation-kf-feast/.m2.2019-10-24.tar \
       --output-dir /root/
 
--- infra/scripts/test-serving.sh
+++ infra/scripts/test-serving.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-.prow/scripts/download-maven-cache.sh \
+infra/scripts/download-maven-cache.sh \
     --archive-uri gs://feast-templocation-kf-feast/.m2.2019-10-24.tar \
     --output-dir /root/
 
