15cc078598101279c6107d61bd5a0504a08b2ad6
Ches Martin <ches.martin@agoda.com>
2020-04-26 10:59:54 +0700

    core: Add unit tests for JobStatus

---
+++ core/src/test/java/feast/core/model/JobStatusTest.java
@@ -0,0 +1,45 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2018-2020 The Feast Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package feast.core.model;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class JobStatusTest {
+
+  @Test
+  public void isTerminalReturnsTrueForJobStatusWithTerminalState() {
+    JobStatus.getTerminalStates()
+        .forEach(
+            status -> {
+              assertThat(status.isTerminal(), is(true));
+              assertThat(status.isTransitional(), is(false));
+            });
+  }
+
+  @Test
+  public void isTransitionalReturnsTrueForJobStatusWithTransitionalState() {
+    JobStatus.getTransitionalStates()
+        .forEach(
+            status -> {
+              assertThat(status.isTransitional(), is(true));
+              assertThat(status.isTerminal(), is(false));
+            });
+  }
+}
