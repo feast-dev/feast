ff5c895abfb807153b27b20fdf6765d1a7f09670
Willem Pienaar <git@willem.co>
2020-05-01 12:28:11 +0800

    Get docker container address from docker

--- infra/scripts/test-docker-compose.sh
+++ infra/scripts/test-docker-compose.sh
@@ -9,7 +9,18 @@ Running Docker Compose tests with pytest at 'tests/e2e'
 "
 PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 cd ${PROJECT_ROOT_DIR}/infra/docker-compose/
+
+# Create Docker Compose configuration file
 cp .env.sample .env
+
+# Start all Docker Compose containers
 docker-compose -f docker-compose.yml -f docker-compose.online.yml up -d
-timeout 3m bash -c 'until [ $(curl -s localhost:8888/tree | grep notebook | wc -l) -gt 0 ]; do echo waiting for Jupyter to start...; sleep 10; done'
+
+# Get Docker Container IP address
+JUPYTER_DOCKER_CONTAINER_IP_ADDRESS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' feast_jupyter_1)
+
+# Wait for Jupyter Notebook Container to be online
+timeout 5m bash -c 'until [ $(curl -s ${JUPYTER_DOCKER_CONTAINER_IP_ADDRESS}:8888/tree | grep notebook | wc -l) -gt 0 ]; do echo waiting for Jupyter to start...; curl -s ${JUPYTER_DOCKER_CONTAINER_IP_ADDRESS}:8888/tree; sleep 10; done'
+
+# Run e2e tests for Redis
 docker exec -it feast_jupyter_1  bash -c 'cd feast/tests/e2e/ && pytest -s basic-ingest-redis-serving.py --core_url core:6565 --serving_url=online-serving:6566'
