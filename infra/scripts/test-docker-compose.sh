76132a77112d9301216e0bdc88719aa8fddb911d
Willem Pienaar <git@willem.co>
2020-05-01 12:21:11 +0800

    Fix typos and environment file

--- infra/scripts/test-docker-compose.sh
+++ infra/scripts/test-docker-compose.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 set -e
-set -o pipefail
 
 echo "
 ============================================================
-Running Basic Redis end-to-end tests with pytest at 'tests/e2e'
+Running Docker Compose tests with pytest at 'tests/e2e'
 ============================================================
 "
 PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 cd ${PROJECT_ROOT_DIR}/infra/docker-compose/
 cp .env.sample .env
 docker-compose -f docker-compose.yml -f docker-compose.online.yml up -d
-timeout 3m bash -c 'until [ $(curl -s localhost:8888/tree | grep notebook | wc -l) -gt 0 ]; do echo waiting for Jupyter to start..; sleep 10; done'
+timeout 3m bash -c 'until [ $(curl -s localhost:8888/tree | grep notebook | wc -l) -gt 0 ]; do echo waiting for Jupyter to start...; sleep 10; done'
 docker exec -it feast_jupyter_1  bash -c 'cd feast/tests/e2e/ && pytest -s basic-ingest-redis-serving.py --core_url core:6565 --serving_url=online-serving:6566'
