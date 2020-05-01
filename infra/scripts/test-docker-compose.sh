88c2a06ceb1c8fbc7f510be073b23e1e63843f91
Willem Pienaar <git@willem.co>
2020-05-01 14:27:34 +0800

    Externalize startup script for docker container

--- infra/scripts/test-docker-compose.sh
+++ infra/scripts/test-docker-compose.sh
@@ -7,20 +7,27 @@ echo "
 Running Docker Compose tests with pytest at 'tests/e2e'
 ============================================================
 "
-PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
-cd ${PROJECT_ROOT_DIR}/infra/docker-compose/
+
+export PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
+export COMPOSE_INTERACTIVE_NO_CLI=1
 
 # Create Docker Compose configuration file
+cd ${PROJECT_ROOT_DIR}/infra/docker-compose/
 cp .env.sample .env
 
 # Start all Docker Compose containers
 docker-compose -f docker-compose.yml -f docker-compose.online.yml up -d
 
 # Get Docker Container IP address
-JUPYTER_DOCKER_CONTAINER_IP_ADDRESS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' feast_jupyter_1)
+export JUPYTER_DOCKER_CONTAINER_IP_ADDRESS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' feast_jupyter_1)
 
-# Wait for Jupyter Notebook Container to be online
-timeout 5m bash -c 'until [ $(curl -s ${JUPYTER_DOCKER_CONTAINER_IP_ADDRESS}:8888/tree | grep notebook | wc -l) -gt 0 ]; do echo waiting for Jupyter to start...; curl -s ${JUPYTER_DOCKER_CONTAINER_IP_ADDRESS}:8888/tree; sleep 10; done'
+
+# Print Jupyter container information
+docker inspect feast_jupyter_1
+docker logs feast_jupyter_1
+
+# Wait for Jupyter Notebook Container to come online
+timeout 5m bash -c 'until [ $(curl -s ${JUPYTER_DOCKER_CONTAINER_IP_ADDRESS}:8888/tree | grep notebook | wc -l) -gt 0 ]; do echo waiting for Jupyter to start...; echo; sleep 5; done'
 
 # Run e2e tests for Redis
-docker exec -it feast_jupyter_1  bash -c 'cd feast/tests/e2e/ && pytest -s basic-ingest-redis-serving.py --core_url core:6565 --serving_url=online-serving:6566'
+docker exec feast_jupyter_1  bash -c 'cd feast/tests/e2e/ && pytest -s basic-ingest-redis-serving.py --core_url core:6565 --serving_url=online-serving:6566'
