d385868912e63eb5ff10eb160c8e38a429c80269
Willem Pienaar <git@willem.co>
2020-05-05 08:45:53 +0800

    Shut down docker compose when done

--- infra/scripts/test-docker-compose.sh
+++ infra/scripts/test-docker-compose.sh
@@ -30,3 +30,6 @@ ${PROJECT_ROOT_DIR}/infra/scripts/wait-for-it.sh ${JUPYTER_DOCKER_CONTAINER_IP_A
 
 # Run e2e tests for Redis
 docker exec feast_jupyter_1 bash -c 'cd feast/tests/e2e/ && pytest -s basic-ingest-redis-serving.py --core_url core:6565 --serving_url=online-serving:6566'
+
+# Shut down docker-compose images
+docker-compose -f docker-compose.yml -f docker-compose.online.yml down
