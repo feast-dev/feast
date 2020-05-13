ee48fbfb089e996b181cbad94c7d1509475da820
polatengin <polatengin@outlook.com>
2020-05-13 18:51:31 +0300

    curl commands silent now

--- infra/scripts/deploy-to-aks.sh
+++ infra/scripts/deploy-to-aks.sh
@@ -23,7 +23,7 @@ az aks get-credentials --name feast-temp --resource-group farfetch_rg
 if ! [ -x "$(command -v helm)" ]; then
   echo 'Error: helm is not installed.' >&2
 
-  curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+  curl -Ls https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | DESIRED_VERSION=v3.2.1 bash
 fi
 
 # update helm repos
@@ -39,7 +39,7 @@ helm plugin install https://github.com/databus23/helm-diff --version master
 if ! [ -x "$(command -v helmsman)" ]; then
   echo 'Error: helmsman is not installed.' >&2
 
-  curl -L https://github.com/Praqma/helmsman/releases/download/v3.2.0/helmsman_3.2.0_linux_amd64.tar.gz | tar zx
+  curl -Ls https://github.com/Praqma/helmsman/releases/download/v3.2.0/helmsman_3.2.0_linux_amd64.tar.gz | tar zx
 
   chmod +x helmsman
 fi
