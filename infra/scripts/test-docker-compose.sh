d6faa779d0c30756d6b119b9e40ca4aea754e2e2
Willem Pienaar <git@willem.co>
2020-05-21 23:36:10 +0800

    Update Docker Compose tests to Feast 0.5

--- infra/scripts/test-docker-compose.sh
+++ infra/scripts/test-docker-compose.sh
@@ -8,6 +8,20 @@ Running Docker Compose tests with pytest at 'tests/e2e'
 ============================================================
 "
 
+clean_up () {
+    ARG=$?
+
+    # Shut down docker-compose images
+    docker-compose -f docker-compose.yml -f docker-compose.online.yml down
+
+    # Remove configuration file
+    rm .env
+
+    exit $ARG
+}
+
+trap clean_up EXIT
+
 export PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 export COMPOSE_INTERACTIVE_NO_CLI=1
 
@@ -30,6 +44,3 @@ ${PROJECT_ROOT_DIR}/infra/scripts/wait-for-it.sh ${JUPYTER_DOCKER_CONTAINER_IP_A
 
 # Run e2e tests for Redis
 docker exec feast_jupyter_1 bash -c 'cd feast/tests/e2e/ && pytest -s basic-ingest-redis-serving.py --core_url core:6565 --serving_url=online-serving:6566'
-
-# Shut down docker-compose images
-docker-compose -f docker-compose.yml -f docker-compose.online.yml down
