d706acdf9e7fdb3b29790e2468c03835f0353c26
Oleksii Moskalenko <oleksiimo@wix.com>
2020-05-28 15:46:58 +0300

    redis-cluster is being merged into redis (#752)
    styling
    
    handle null case
    
    styling

--- storage/connectors/redis/src/main/java/feast/storage/connectors/redis/writer/RedisClusterIngestionClient.java
+++ storage/connectors/redis/src/main/java/feast/storage/connectors/redis/writer/RedisClusterIngestionClient.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package feast.storage.connectors.rediscluster.writer;
+package feast.storage.connectors.redis.writer;
 
 import com.google.common.collect.Lists;
 import feast.proto.core.StoreProto;
