c34f6e8165d0571481d95b7f5ee20a314e96245a
e10112844 <jayant.mj@vistaprint.com>
2020-06-11 09:22:52 -0400

    update to license

--- sdk/python/feast/staging/storage_client.py
+++ sdk/python/feast/staging/storage_client.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2020 Cimpress
+# Copyright 2020 The Feast Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
