faf10d5ee4141865e3b9f203df5a0d2d4dc7e143
Willem Pienaar <git@willem.co>
2020-06-20 14:00:12 +0800

    Fix Docker Compose test by waiting for service to come online

--- infra/scripts/test-docker-compose.sh
+++ infra/scripts/test-docker-compose.sh
@@ -40,7 +40,19 @@ docker inspect feast_jupyter_1
 docker logs feast_jupyter_1
 
 # Wait for Jupyter Notebook Container to come online
-${PROJECT_ROOT_DIR}/infra/scripts/wait-for-it.sh ${JUPYTER_DOCKER_CONTAINER_IP_ADDRESS}:8888 --timeout=300
+${PROJECT_ROOT_DIR}/infra/scripts/wait-for-it.sh ${JUPYTER_DOCKER_CONTAINER_IP_ADDRESS}:8888 --timeout=60
+
+# Get Feast Core container IP address
+export FEAST_CORE_CONTAINER_IP_ADDRESS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' feast_core_1)
+
+# Wait for Feast Core to be ready
+${PROJECT_ROOT_DIR}/infra/scripts/wait-for-it.sh ${FEAST_CORE_CONTAINER_IP_ADDRESS}:6565 --timeout=120
+
+# Get Feast Online Serving container IP address
+export FEAST_ONLINE_SERVING_CONTAINER_IP_ADDRESS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' feast_online-serving_1)
+
+# Wait for Feast Online Serving to be ready
+${PROJECT_ROOT_DIR}/infra/scripts/wait-for-it.sh ${FEAST_ONLINE_SERVING_CONTAINER_IP_ADDRESS}:6566 --timeout=120
 
 # Run e2e tests for Redis
 docker exec feast_jupyter_1 bash -c 'cd feast/tests/e2e/ && pytest -s basic-ingest-redis-serving.py --core_url core:6565 --serving_url=online-serving:6566'
