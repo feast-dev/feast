7231550315723fd418fe084d5b3bb3f761ecad61
Terence <terence.limxp@go-jek.com>
2020-06-22 11:16:36 +0800

    Add subscriptions blacklist functionality

--- common/src/main/java/feast/common/models/Store.java
+++ common/src/main/java/feast/common/models/Store.java
@@ -17,10 +17,34 @@
 package feast.common.models;
 
 import feast.proto.core.StoreProto.Store.Subscription;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Store {
+
+  /**
+   * Accepts a comma-delimited Subscriptions that is string-formatted and converts it to a list of
+   * Subscription class objects.
+   *
+   * @param subscriptions String formatted Subscriptions, comma delimited.
+   * @param exclude flag to determine if subscriptions with exclusion flag should be returned
+   * @return List of Subscription class objects
+   */
+  public static List<Subscription> parseSubscriptionFrom(String subscriptions, boolean exclude) {
+    List<Subscription> allSubscriptions =
+        Arrays.stream(subscriptions.split(","))
+            .map(subscriptionStr -> convertStringToSubscription(subscriptionStr))
+            .collect(Collectors.toList());
+
+    if (exclude) {
+      allSubscriptions =
+          allSubscriptions.stream().filter(sub -> !sub.getExclude()).collect(Collectors.toList());
+    }
+
+    return allSubscriptions;
+  }
 
   /**
    * Accepts a Subscription class object and returns it in string format
@@ -34,7 +58,8 @@ public class Store {
           String.format("Missing arguments in subscription string: %s", subscription.toString()));
     }
 
-    return String.format("%s:%s", subscription.getProject(), subscription.getName());
+    return String.format(
+        "%s:%s:%s", subscription.getProject(), subscription.getName(), subscription.getExclude());
   }
 
   /**
@@ -48,7 +73,15 @@ public class Store {
       return Subscription.newBuilder().build();
     }
     String[] split = subscription.split(":");
-    return Subscription.newBuilder().setProject(split[0]).setName(split[1]).build();
+    if (split.length == 2) {
+      // Backward compatibility check
+      return Subscription.newBuilder().setProject(split[0]).setName(split[1]).build();
+    }
+    return Subscription.newBuilder()
+        .setProject(split[0])
+        .setName(split[1])
+        .setExclude(Boolean.parseBoolean(split[2]))
+        .build();
   }
 
   /**
@@ -62,37 +95,57 @@ public class Store {
    */
   public static boolean isSubscribedToFeatureSet(
       List<Subscription> subscriptions, String projectName, String featureSetName) {
+    // Case 1: Highest priority check, to exclude all matching subscriptions with excluded flag =
+    // true
     for (Subscription sub : subscriptions) {
       // If configuration missing, fail
       if (sub.getProject().isEmpty() || sub.getName().isEmpty()) {
         throw new IllegalArgumentException(
             String.format("Subscription is missing arguments: %s", sub.toString()));
       }
+      // Match feature set name to pattern
+      Pattern patternName = getNamePattern(sub);
+      Pattern patternProject = getProjectPattern(sub);
+      // SubCase: Project name and feature set name matches and excluded flag is true
+      if (patternProject.matcher(projectName).matches()
+          && patternName.matcher(featureSetName).matches()
+          && sub.getExclude()) {
+        return false;
+      }
+    }
+    // Case 2: Featureset is not excluded, check if it is included in the current subscriptions
+    // filteredSubscriptions only contain subscriptions with excluded flag = false
+    List<Subscription> filteredSubscriptions =
+        subscriptions.stream().filter(sub -> !sub.getExclude()).collect(Collectors.toList());
 
-      // If all wildcards, subscribe to everything
-      if (sub.getProject().equals("*") || sub.getName().equals("*")) {
+    for (Subscription filteredSub : filteredSubscriptions) {
+      // Match feature set name to pattern
+      Pattern patternName = getNamePattern(filteredSub);
+      Pattern patternProject = getProjectPattern(filteredSub);
+      // SubCase: Project name and feature set name matches
+      if (patternProject.matcher(projectName).matches()
+          && patternName.matcher(featureSetName).matches()) {
         return true;
       }
+    }
+    return false;
+  }
 
-      // Match project name
-      if (!projectName.equals(sub.getProject())) {
-        continue;
-      }
-
-      // Convert wildcard to regex
-      String subName = sub.getName();
-      if (!sub.getName().contains(".*")) {
-        subName = subName.replace("*", ".*");
-      }
-
-      // Match feature set name to pattern
-      Pattern pattern = Pattern.compile(subName);
-      if (!pattern.matcher(featureSetName).matches()) {
-        continue;
-      }
-      return true;
+  private static Pattern getProjectPattern(Subscription subscription) {
+    String subProject = subscription.getProject();
+    if (!subscription.getProject().contains(".*")) {
+      subProject = subProject.replace("*", ".*");
     }
 
-    return false;
+    return Pattern.compile(subProject);
+  }
+
+  private static Pattern getNamePattern(Subscription subscription) {
+    String subName = subscription.getName();
+    if (!subscription.getProject().contains(".*")) {
+      subName = subName.replace("*", ".*");
+    }
+
+    return Pattern.compile(subName);
   }
 }
--- ingestion/src/test/java/feast/ingestion/transform/FeatureRowToStoreAllocatorTest.java
+++ ingestion/src/test/java/feast/ingestion/transform/FeatureRowToStoreAllocatorTest.java
@@ -19,7 +19,10 @@ package feast.ingestion.transform;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import feast.proto.core.StoreProto;
+import feast.proto.core.StoreProto.Store.Subscription;
 import feast.proto.types.FeatureRowProto;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import org.apache.beam.sdk.extensions.protobuf.ProtoCoder;
 import org.apache.beam.sdk.testing.PAssert;
@@ -39,6 +42,10 @@ public class FeatureRowToStoreAllocatorTest {
         .addSubscriptions(
             StoreProto.Store.Subscription.newBuilder().setProject("project").setName(s).build())
         .build();
+  }
+
+  private StoreProto.Store newStore(List<StoreProto.Store.Subscription> subscriptionList) {
+    return StoreProto.Store.newBuilder().addAllSubscriptions(subscriptionList).build();
   }
 
   @Test
@@ -93,6 +100,57 @@ public class FeatureRowToStoreAllocatorTest {
                 .setCoder(ProtoCoder.of(FeatureRowProto.FeatureRow.class))
                 .apply("CountAll", Count.globally()))
         .containsInAnyOrder(5L);
+
+    p.run();
+  }
+
+  @Test
+  public void featureRowShouldBeAllocatedToStoreTagsAccordingToSubscriptionBlacklist() {
+    Subscription subscription1 = Subscription.newBuilder().setProject("*").setName("*").build();
+    Subscription subscription2 =
+        Subscription.newBuilder().setProject("project1").setName("fs_2").build();
+    Subscription subscription3 =
+        Subscription.newBuilder().setProject("project1").setName("fs_1").setExclude(true).build();
+    Subscription subscription4 =
+        Subscription.newBuilder().setProject("project2").setName("*").setExclude(true).build();
+
+    List<Subscription> testStoreSubscriptions1 =
+        Arrays.asList(subscription1, subscription2, subscription3);
+    StoreProto.Store testStore1 = newStore(testStoreSubscriptions1);
+
+    List<Subscription> testStoreSubscriptions2 = Arrays.asList(subscription1, subscription4);
+    StoreProto.Store testStore2 = newStore(testStoreSubscriptions2);
+
+    Map<StoreProto.Store, TupleTag<FeatureRowProto.FeatureRow>> storeTags =
+        ImmutableMap.of(
+            testStore1, new TupleTag<>(),
+            testStore2, new TupleTag<>());
+
+    PCollectionTuple allocatedRows =
+        p.apply(
+                Create.of(
+                    FeatureRowProto.FeatureRow.newBuilder().setFeatureSet("project1/fs_1").build(),
+                    FeatureRowProto.FeatureRow.newBuilder().setFeatureSet("project2/fs_1").build(),
+                    FeatureRowProto.FeatureRow.newBuilder().setFeatureSet("project2/fs_2").build()))
+            .apply(
+                FeatureRowToStoreAllocator.newBuilder()
+                    .setStoreTags(storeTags)
+                    .setStores(ImmutableList.of(testStore1, testStore2))
+                    .build());
+
+    PAssert.that(
+            allocatedRows
+                .get(storeTags.get(testStore1))
+                .setCoder(ProtoCoder.of(FeatureRowProto.FeatureRow.class))
+                .apply("CountStore1", Count.globally()))
+        .containsInAnyOrder(2L);
+
+    PAssert.that(
+            allocatedRows
+                .get(storeTags.get(testStore2))
+                .setCoder(ProtoCoder.of(FeatureRowProto.FeatureRow.class))
+                .apply("CountStore2", Count.globally()))
+        .containsInAnyOrder(1L);
 
     p.run();
   }
