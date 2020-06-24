156fdbc793b181fde6a13d13eb7a505493b2020e
polatengin <polatengin@outlook.com>
2020-06-24 15:24:57 +0300

    project name and initial id fields added

--- tests/ds_scenarios/conftest.py
+++ tests/ds_scenarios/conftest.py
@@ -5,3 +5,5 @@ def pytest_addoption(parser):
     parser.addoption("--core_url", action="store", default="localhost:6565")
     parser.addoption("--serving_url", action="store", default="localhost:6566")
     parser.addoption("--allow_dirty", action="store", default="False")
+    parser.addoption("--project_name", action="store")
+    parser.addoption("--initial_entity_id", action="store", default="1")
