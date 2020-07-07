8fd5264cf531f2b2869f62f2bae50c31efb74e95
mirko.morandi <mirko.morandi@farfetch.com>
2020-07-07 09:20:04 +0200

    minor fixes for init script and its location

--- infra/scripts/init_pypi.sh
+++ infra/scripts/init_pypi.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-mkdir /.config && mkdir /.config/pip
+mkdir -p /.config && mkdir -p /.config/pip
 echo -e "[global]\nindex-url = https://$PYPI_USER:$PYPI_PWD@pypi.prod.konnekt.us/simple/\nextra-index-url =  https://pypi.org/simple/\n" > /.config/pip/pip.conf
 export PIP_CONFIG_FILE=/.config/pip/pip.conf
-pip install feast==0.5.1.dev471+g8c76c49d
