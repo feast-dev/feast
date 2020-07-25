5541ae11361cb1c0d561f8b370ef79d889ff988e
Willem Pienaar <git@willem.co>
2020-07-25 15:09:02 +0800

    Allow tests per commit sha

--- infra/scripts/test-load.sh
+++ infra/scripts/test-load.sh
@@ -31,14 +31,14 @@ export PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 export COMPOSE_INTERACTIVE_NO_CLI=1
 
 # Wait for docker images to be available
-"${PROJECT_ROOT_DIR}"/infra/scripts/wait-for-docker-images.sh latest
+"${PROJECT_ROOT_DIR}"/infra/scripts/wait-for-docker-images.sh "${CURRENT_SHA}"
 
 # Create Docker Compose configuration file
 cd "${PROJECT_ROOT_DIR}"/infra/docker-compose/
 cp .env.sample .env
 
 # Start Docker Compose containers
-FEAST_VERSION=latest docker-compose -f docker-compose.yml -f docker-compose.online.yml up -d
+FEAST_VERSION=${CURRENT_SHA} docker-compose -f docker-compose.yml -f docker-compose.online.yml up -d
 
 # Get Jupyter container IP address
 export JUPYTER_DOCKER_CONTAINER_IP_ADDRESS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' feast_jupyter_1)
