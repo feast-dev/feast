0411ced739f0ab975e6298abe699b1e32e37471c
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2020-08-25 15:58:17 +0800

    round event timestamp to seconds

--- tests/e2e/bq/bq-batch-retrieval.py
+++ tests/e2e/bq/bq-batch-retrieval.py
@@ -186,9 +186,6 @@ def test_batch_get_historical_features_with_file(client):
     client.ingest(file_fs1, features_1_df, timeout=480)
 
     # Rename column (datetime -> event_timestamp)
-    features_1_df["datetime"] += pd.Timedelta(
-        seconds=1
-    )  # adds buffer to avoid rounding errors
     features_1_df = features_1_df.rename(columns={"datetime": "event_timestamp"})
 
     to_avro(
@@ -234,9 +231,6 @@ def test_batch_get_historical_features_with_gs_path(client, gcs_path):
     client.ingest(gcs_fs1, features_1_df, timeout=360)
 
     # Rename column (datetime -> event_timestamp)
-    features_1_df["datetime"] += pd.Timedelta(
-        seconds=1
-    )  # adds buffer to avoid rounding errors
     features_1_df = features_1_df.rename(columns={"datetime": "event_timestamp"})
 
     # Output file to local
