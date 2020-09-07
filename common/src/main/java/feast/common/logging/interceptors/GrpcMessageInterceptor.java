e1f6ab71d20a709d791ca1dabc4f4a1fd492c7ca
Zhu Zhan Yan <program.nom@gmail.com>
2020-09-07 10:27:59 +0800

    Fix Audit Message Logging Interceptor Race Condition (#938)
    * Fix race condition in  GrpcMessageInterceptor to revert a empty message if message cannot be recorded.
    
    * Fix GrpcMessageInterceptor race condition by allow audit log message to be called from multiple async calls
    
    * Revert option change
    
    * Add CoreLoggingIT integration test to test message audit logging.
    
    * Fix GrpcMessageInterceptor race condition by moving allowing request to be unset.
    
    * Fix lint
    
    * Increase wait for logs in CoreLoggingIT
    
    * Fix to compare the correct lob JsonObject with the right response.
    
    * Debug response
    
    * Reduce load size to make test less flaky
    
    * Update test to only check request and response for one call.
    
    * Fix compile failure due to uncaught exception.
    
    * Add method name filter to prevent logs from tests from interfering with each other.
    
    * Add intergration test to check that message logs are produced correctly under load.
    
    * Fix imports and log4j2 not able to find config file
    
    * Fix issue with CoreLoggingIT TestLogAppender being null due to class not found by log4j2
    
    * Remove unused getters in MessageAuditLogEntry.
    
    * Update CoreLoggingIT test to check that expected contents of logs produced under load.

--- common/src/main/java/feast/common/logging/interceptors/GrpcMessageInterceptor.java
+++ common/src/main/java/feast/common/logging/interceptors/GrpcMessageInterceptor.java
@@ -61,7 +61,12 @@ public class GrpcMessageInterceptor implements ServerInterceptor {
   public <ReqT, RespT> Listener<ReqT> interceptCall(
       ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
     MessageAuditLogEntry.Builder entryBuilder = MessageAuditLogEntry.newBuilder();
-    // default response message to empty proto in log entry.
+    // default response/request message to empty proto in log entry.
+    // request could be empty when the client closes the connection before sending a request
+    // message.
+    // response could be unset when the service encounters an error when processsing the service
+    // call.
+    entryBuilder.setRequest(Empty.newBuilder().build());
     entryBuilder.setResponse(Empty.newBuilder().build());
 
     // Unpack service & method name from call
