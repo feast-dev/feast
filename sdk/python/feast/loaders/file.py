091eece1ccf736600ddf7f700d9adeb3d657e12e
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2020-10-13 17:43:01 +0800

    lint
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- sdk/python/feast/loaders/file.py
+++ sdk/python/feast/loaders/file.py
@@ -65,9 +65,7 @@ def export_source_to_staging_location(
     # Prepare Avro file to be exported to staging location
     if isinstance(source, pd.DataFrame):
         # Remote gs staging location provided by serving
-        dir_path, file_name, source_path = export_dataframe_to_local(
-            df=source
-        )
+        dir_path, file_name, source_path = export_dataframe_to_local(df=source)
     elif isinstance(source, str):
         source_uri = urlparse(source)
         if source_uri.scheme in ["", "file"]:
