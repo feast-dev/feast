adaa91cc83469b3d35aef4af8932b9c3081a849e
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2020-10-26 10:25:13 +0800

    test scopes
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- tests/e2e/fixtures/external_services.py
+++ tests/e2e/fixtures/external_services.py
@@ -4,25 +4,25 @@ from pytest_redis.executor import NoopRedis
 __all__ = ("feast_core", "feast_serving", "redis_server", "kafka_server")
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def redis_server(pytestconfig):
     host, port = pytestconfig.getoption("redis_url").split(":")
     return NoopRedis(host, port, None)
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def feast_core(pytestconfig):
     host, port = pytestconfig.getoption("core_url").split(":")
     return host, port
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def feast_serving(pytestconfig):
     host, port = pytestconfig.getoption("serving_url").split(":")
     return host, port
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def kafka_server(pytestconfig):
     host, port = pytestconfig.getoption("kafka_brokers").split(":")
     return host, port
