43e4624b86c5a2a71955e6604f2b2f5c7de1223f
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2020-10-27 17:21:38 +0800

    test commit
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- infra/scripts/test-end-to-end-gcp.sh
+++ infra/scripts/test-end-to-end-gcp.sh
@@ -9,6 +9,6 @@ make install-python
 python -m pip install -qr tests/requirements.txt
 
 su -p postgres -c "PATH=$PATH HOME=/tmp pytest tests/e2e/ \
-          --feast-version develop --env=gcloud --dataproc-cluster-name feast-e2e \
-          --dataproc-project kf-feast --dataproc-region us-central1 \
-          --redis-url 10.128.0.105:6379 --redis-cluster --kafka-brokers 10.128.0.103:9094"
+      --feast-version develop --env=gcloud --dataproc-cluster-name feast-e2e \
+      --dataproc-project kf-feast --dataproc-region us-central1 \
+      --redis-url 10.128.0.105:6379 --redis-cluster --kafka-brokers 10.128.0.103:9094"
