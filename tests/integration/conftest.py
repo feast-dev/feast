5e1899fa216f172cb3f4d72a34f9c7eb5890343e
Khor Shu Heng <khor.heng@gojek.com>
2020-10-29 16:08:57 +0800

    Scaffolding for integration tests
    Signed-off-by: Khor Shu Heng <khor.heng@gojek.com>

---
+++ tests/integration/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    pass
+
+
+def pytest_runtest_setup(item):
+    if "incremental" in item.keywords:
+        previousfailed = getattr(item.parent, "_previousfailed", None)
+        if previousfailed is not None:
+            pytest.xfail("previous test failed (%s)" % previousfailed.name)
