1976f1045a85a06cefa5b66c7fbdc100bac3b9dc
Khor Shu Heng <khor.heng@gojek.com>
2020-11-03 09:24:30 +0800

    Fix argument name to integration test script
    Signed-off-by: Khor Shu Heng <khor.heng@gojek.com>

--- infra/scripts/test-integration.sh
+++ infra/scripts/test-integration.sh
@@ -4,4 +4,4 @@ python -m pip install --upgrade pip setuptools wheel
 make install-python
 python -m pip install -qr tests/requirements.txt
 
-pytest tests/integration --dataproc-cluster-name feast-e2e --dataproc-project kf-feast --dataproc-region us-central1  --staging-location gs://feast-templocation-kf-feast
+pytest tests/integration --dataproc-cluster-name feast-e2e --dataproc-project kf-feast --dataproc-region us-central1  --dataproc-staging-location gs://feast-templocation-kf-feast
