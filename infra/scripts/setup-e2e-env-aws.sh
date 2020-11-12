8bcc1f9307dacc98103ebe2110f0a29bd725a220
Oleg Avdeev <oleg.v.avdeev@gmail.com>
2020-11-12 13:04:22 +0300

    make aws e2e tests to use correct feast_version (#1159)
    Signed-off-by: Oleg Avdeev <oleg.v.avdeev@gmail.com>

--- infra/scripts/setup-e2e-env-aws.sh
+++ infra/scripts/setup-e2e-env-aws.sh
@@ -8,7 +8,8 @@ python -m pip install -qr sdk/python/requirements-dev.txt
 python -m pip install -qr tests/requirements.txt
 
 # Using mvn -q to make it less verbose. This step happens after docker containers were
-# succesfully built so it should be unlikely to fail.
+# succesfully built so it should be unlikely to fail, therefore we likely won't need detailed logs.
 echo "########## Building ingestion jar"
 TIMEFORMAT='########## took %R seconds'
-time mvn -q --no-transfer-progress -Dmaven.javadoc.skip=true -Dgpg.skip -DskipUTs=true clean package
+
+time make build-java-no-tests REVISION=develop MAVEN_EXTRA_OPTS="-q --no-transfer-progress"
