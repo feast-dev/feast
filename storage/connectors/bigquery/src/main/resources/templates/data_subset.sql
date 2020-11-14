031a5be9bc767147f8e403432f33e49e22936f4e
Willem Pienaar <git@willem.co>
2020-11-14 22:35:47 +0800

    Remove job controller and ingestion java code
    Signed-off-by: Willem Pienaar <git@willem.co>

--- storage/connectors/bigquery/src/main/resources/templates/data_subset.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM `{{ table }}`
-{% if ingestionId is not empty %}
-WHERE ingestion_id='{{ ingestionId }}'
-{% endif %}
-{% if date is not empty %}
-WHERE DATE(event_timestamp)='{{ date }}'
-{% endif %}
