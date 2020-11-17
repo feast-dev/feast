b92331488bb2d4d91abf5838bb15c43ad5bbb769
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2020-11-17 15:02:17 +0800

    drop auth=true completely
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- tests/e2e/fixtures/feast_services.py
+++ tests/e2e/fixtures/feast_services.py
@@ -48,7 +48,7 @@ def _wait_port_open(port, max_wait=60):
 
 
 @pytest.fixture(
-    scope="session", params=[pytest.param(True, marks=pytest.mark.skip), False],
+    scope="session", params=[False],
 )
 def enable_auth(request):
     return request.param
