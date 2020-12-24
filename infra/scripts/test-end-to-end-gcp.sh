b35790771924e5dc072864cc0cce7d3531d79373
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2020-12-24 15:32:25 +0800

    mvn retry in e2e tests
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- infra/scripts/test-end-to-end-gcp.sh
+++ infra/scripts/test-end-to-end-gcp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export DISABLE_SERVICE_FIXTURES=1
-export MAVEN_OPTS="-Dmaven.repo.local=/tmp/.m2/repository -DdependencyLocationsEnabled=false"
+export MAVEN_OPTS="-Dmaven.repo.local=/tmp/.m2/repository -DdependencyLocationsEnabled=false -Dmaven.wagon.httpconnectionManager.ttlSeconds=25 -Dmaven.wagon.http.retryHandler.count=3 -Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false"
 export MAVEN_CACHE="gs://feast-templocation-kf-feast/.m2.2020-11-17.tar"
 
 infra/scripts/download-maven-cache.sh --archive-uri ${MAVEN_CACHE} --output-dir /tmp
