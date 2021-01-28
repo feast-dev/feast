7f764b27f4c4abbb8d97f725df05c14d9e72a929
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2021-01-28 21:10:25 +0800

    update helm chart repo
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- infra/scripts/install-helm.sh
+++ infra/scripts/install-helm.sh
@@ -2,8 +2,8 @@
 set -e
 readonly HELM_URL=https://storage.googleapis.com/kubernetes-helm
 readonly HELM_TARBALL="helm-${HELM_VERSION}-linux-amd64.tar.gz"
-readonly STABLE_REPO_URL=https://kubernetes-charts.storage.googleapis.com/
-readonly INCUBATOR_REPO_URL=https://kubernetes-charts-incubator.storage.googleapis.com/
+readonly STABLE_REPO_URL=https://charts.helm.sh/stable
+readonly INCUBATOR_REPO_URL=https://charts.helm.sh/incubator
 curl -s "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" | tar -C /tmp -xz
 sudo mv /tmp/linux-amd64/helm /usr/bin/helm
 helm init --client-only
