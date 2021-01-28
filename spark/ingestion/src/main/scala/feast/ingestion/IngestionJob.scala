05a594965daef0be5a91aca80208c5004e67930c
Oleksii Moskalenko <moskalenko.alexey@gmail.com>
2021-01-28 22:05:31 +0800

    Bump version to 0.10.0-SNAPSHOT (#1296)
    Signed-off-by: Oleksii Moskalenko <moskalenko.alexey@gmail.com>

--- spark/ingestion/src/main/scala/feast/ingestion/IngestionJob.scala
+++ spark/ingestion/src/main/scala/feast/ingestion/IngestionJob.scala
@@ -31,7 +31,7 @@ object IngestionJob {
 
   val parser = new scopt.OptionParser[IngestionJobConfig]("IngestionJob") {
     // ToDo: read version from Manifest
-    head("feast.ingestion.IngestionJob", "0.9.0")
+    head("feast.ingestion.IngestionJob", "0.10.0-SNAPSHOT")
 
     opt[Modes]("mode")
       .action((x, c) => c.copy(mode = x))
