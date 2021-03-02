4f281b2ade61aa663603fa35882fefba8c9acb33
Jacob Klegar <jacob@tecton.ai>
2021-03-02 16:50:48 -0500

    FeatureView and FeatureStore classes
    Signed-off-by: Jacob Klegar <jacob@tecton.ai>

---
+++ sdk/python/feast/feature_view.py
@@ -0,0 +1,42 @@
+# Copyright 2019 The Feast Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from datetime import datetime
+from typing import Dict, List
+
+from feast.big_query_source import BigQuerySource
+from feast.entity import Entity
+from feast.feature import Feature
+
+
+class FeatureView:
+    def __init__(
+        self,
+        name: str,
+        entities: List[Entity],
+        features: List[Feature],
+        tags: Dict[str, str],
+        ttl: str,
+        online: bool,
+        inputs: BigQuerySource,
+        feature_start_time: datetime,
+    ):
+        self.name = name
+        self.entities = entities
+        self.features = features
+        self.tags = tags
+        self.ttl = ttl
+        self.online = online
+        self.inputs = inputs
+        self.feature_start_time = feature_start_time
+        return
