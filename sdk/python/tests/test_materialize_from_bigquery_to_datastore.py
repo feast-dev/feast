796805c8fd0b982499307aabfb9a9d21b3312241
Jacob Klegar <jacob@tecton.ai>
2021-03-23 18:31:52 -0400

    Use read API instead of internal method
    Signed-off-by: Jacob Klegar <jacob@tecton.ai>

--- sdk/python/tests/test_materialize_from_bigquery_to_datastore.py
+++ sdk/python/tests/test_materialize_from_bigquery_to_datastore.py
@@ -10,8 +10,6 @@ from feast.data_source import BigQuerySource
 from feast.feature import Feature
 from feast.feature_store import FeatureStore
 from feast.feature_view import FeatureView
-from feast.protos.feast.types.EntityKey_pb2 import EntityKey as EntityKeyProto
-from feast.protos.feast.types.Value_pb2 import Value as ValueProto
 from feast.repo_config import LocalOnlineStoreConfig, OnlineStoreConfig, RepoConfig
 from feast.value_type import ValueType
 
@@ -82,11 +80,10 @@ class TestMaterializeFromBigQueryToDatastore:
         )
 
         # check result of materialize()
-        entity_key = EntityKeyProto(
-            entity_names=["driver_id"], entity_values=[ValueProto(int64_val=1)]
-        )
-        t, val = fs._get_provider().online_read("default", fv, entity_key)
-        assert abs(val["value"].double_val - checked_value) < 1e-6
+        response_dict = fs.get_online_features(
+            [f"{fv.name}:value"], [{"driver_id": 1}]
+        ).to_dict()
+        assert abs(response_dict[f"{fv.name}:value"][0] - checked_value) < 1e-6
 
     def test_bigquery_query_to_datastore_correctness(self):
         # create dataset
@@ -142,10 +139,7 @@ class TestMaterializeFromBigQueryToDatastore:
         )
 
         # check result of materialize()
-        entity_key = EntityKeyProto(
-            entity_names=["driver_id"], entity_values=[ValueProto(int64_val=1)]
-        )
-        read_rows = fs._get_provider().online_read("default", fv, [entity_key])
-        assert len(read_rows) == 1
-        _, val = read_rows[0]
-        assert abs(val["value"].double_val - checked_value) < 1e-6
+        response_dict = fs.get_online_features(
+            [f"{fv.name}:value"], [{"driver_id": 1}]
+        ).to_dict()
+        assert abs(response_dict[f"{fv.name}:value"][0] - checked_value) < 1e-6
