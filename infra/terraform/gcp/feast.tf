4068ca630a00d28e7ed00929623313917e85a0dc
Jose Pinero <gregorio.pinero07@gmail.com>
2021-03-30 23:08:47 +0200

    Rollback docs and update feast chart location

--- infra/terraform/gcp/feast.tf
+++ infra/terraform/gcp/feast.tf
@@ -113,7 +113,7 @@ resource "helm_release" "feast" {
   depends_on = [kubernetes_secret.feast-postgres-secret, kubernetes_secret.feast_sa_secret]
 
   name  = var.name_prefix
-  chart = "feast-charts/feast"
+  chart = "https://feast-helm-charts.storage.googleapis.com/feast-0.100.4.tgz"
 
   values = [
     yamlencode(local.feast_helm_values)
