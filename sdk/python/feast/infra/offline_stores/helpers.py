7cf3545115b7429b4981cfc93366277a569b08a3
Willem Pienaar <git@willem.co>
2021-05-14 17:41:06 -0700

    Ensure that GCP dependencies aren't loaded unnecessarily
    Signed-off-by: Willem Pienaar <git@willem.co>

--- sdk/python/feast/infra/offline_stores/helpers.py
+++ sdk/python/feast/infra/offline_stores/helpers.py
@@ -1,8 +1,6 @@
 from typing import List
 
 from feast.data_source import BigQuerySource, DataSource, FileSource
-from feast.infra.offline_stores.bigquery import BigQueryOfflineStore
-from feast.infra.offline_stores.file import FileOfflineStore
 from feast.infra.offline_stores.offline_store import OfflineStore
 
 
@@ -13,10 +11,12 @@ def get_offline_store_from_sources(sources: List[DataSource]) -> OfflineStore:
 
     # Retrieve features from ParquetOfflineStore
     if all(source == FileSource for source in source_types):
+        from feast.infra.offline_stores.file import FileOfflineStore
         return FileOfflineStore()
 
     # Retrieve features from BigQueryOfflineStore
     if all(source == BigQuerySource for source in source_types):
+        from feast.infra.offline_stores.bigquery import BigQueryOfflineStore
         return BigQueryOfflineStore()
 
     # Could not map inputs to an OfflineStore implementation
