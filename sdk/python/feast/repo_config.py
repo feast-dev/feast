b359bee227265c03ee00829c64aa48cdc9e02247
Tsotne Tabidze <tsotne@tecton.ai>
2021-05-19 11:18:39 -0700

    Add offline_store config (#1552)
    * Add offline_store config
    
    Signed-off-by: Tsotne Tabidze <tsotne@tecton.ai>
    
    * Enforce offline_store during feast apply, rename entity_dataset_name to dataset
    
    Signed-off-by: Tsotne Tabidze <tsotne@tecton.ai>
    
    * Remove ugly getattr since it's unnecessary anymore
    
    Signed-off-by: Tsotne Tabidze <tsotne@tecton.ai>
    
    * Rename Bigquery to BigQuery
    
    Signed-off-by: Tsotne Tabidze <tsotne@tecton.ai>

--- sdk/python/feast/repo_config.py
+++ sdk/python/feast/repo_config.py
@@ -39,6 +39,26 @@ class DatastoreOnlineStoreConfig(FeastBaseModel):
 OnlineStoreConfig = Union[DatastoreOnlineStoreConfig, SqliteOnlineStoreConfig]
 
 
+class FileOfflineStoreConfig(FeastBaseModel):
+    """ Offline store config for local (file-based) store """
+
+    type: Literal["file"] = "file"
+    """ Offline store type selector"""
+
+
+class BigQueryOfflineStoreConfig(FeastBaseModel):
+    """ Offline store config for GCP BigQuery """
+
+    type: Literal["bigquery"] = "bigquery"
+    """ Offline store type selector"""
+
+    dataset: Optional[StrictStr] = "feast"
+    """ (optional) BigQuery Dataset name for temporary tables """
+
+
+OfflineStoreConfig = Union[FileOfflineStoreConfig, BigQueryOfflineStoreConfig]
+
+
 class RegistryConfig(FeastBaseModel):
     """ Metadata Store Configuration. Configuration that relates to reading from and writing to the Feast registry."""
 
@@ -70,6 +90,9 @@ class RepoConfig(FeastBaseModel):
     online_store: OnlineStoreConfig = SqliteOnlineStoreConfig()
     """ OnlineStoreConfig: Online store configuration (optional depending on provider) """
 
+    offline_store: OfflineStoreConfig = FileOfflineStoreConfig()
+    """ OfflineStoreConfig: Offline store configuration (optional depending on provider) """
+
     def get_registry_config(self):
         if isinstance(self.registry, str):
             return RegistryConfig(path=self.registry)
@@ -85,45 +108,84 @@ class RepoConfig(FeastBaseModel):
         # considered tech debt until we can implement https://github.com/samuelcolvin/pydantic/issues/619 or a more
         # granular configuration system
 
-        # Skip if online store isn't set explicitly
+        # Set empty online_store config if it isn't set explicitly
         if "online_store" not in values:
             values["online_store"] = dict()
 
-        # Skip if we arent creating the configuration from a dict
+        # Skip if we aren't creating the configuration from a dict
         if not isinstance(values["online_store"], Dict):
             return values
 
         # Make sure that the provider configuration is set. We need it to set the defaults
         assert "provider" in values
 
-        if "online_store" in values:
-            # Set the default type
-            if "type" not in values["online_store"]:
-                if values["provider"] == "local":
-                    values["online_store"]["type"] = "sqlite"
-                elif values["provider"] == "gcp":
-                    values["online_store"]["type"] = "datastore"
+        # Set the default type
+        if "type" not in values["online_store"]:
+            if values["provider"] == "local":
+                values["online_store"]["type"] = "sqlite"
+            elif values["provider"] == "gcp":
+                values["online_store"]["type"] = "datastore"
 
-            online_store_type = values["online_store"]["type"]
+        online_store_type = values["online_store"]["type"]
 
-            # Make sure the user hasn't provided the wrong type
-            assert online_store_type in ["datastore", "sqlite"]
+        # Make sure the user hasn't provided the wrong type
+        assert online_store_type in ["datastore", "sqlite"]
 
-            # Validate the dict to ensure one of the union types match
-            try:
-                if online_store_type == "sqlite":
-                    SqliteOnlineStoreConfig(**values["online_store"])
-                elif values["online_store"]["type"] == "datastore":
-                    DatastoreOnlineStoreConfig(**values["online_store"])
-                else:
-                    raise ValidationError(
-                        f"Invalid online store type {online_store_type}"
-                    )
-            except ValidationError as e:
+        # Validate the dict to ensure one of the union types match
+        try:
+            if online_store_type == "sqlite":
+                SqliteOnlineStoreConfig(**values["online_store"])
+            elif online_store_type == "datastore":
+                DatastoreOnlineStoreConfig(**values["online_store"])
+            else:
+                raise ValidationError(f"Invalid online store type {online_store_type}")
+        except ValidationError as e:
+            raise ValidationError(
+                [ErrorWrapper(e, loc="online_store")], model=SqliteOnlineStoreConfig,
+            )
+
+        return values
+
+    @root_validator(pre=True)
+    def _validate_offline_store_config(cls, values):
+        # Set empty offline_store config if it isn't set explicitly
+        if "offline_store" not in values:
+            values["offline_store"] = dict()
+
+        # Skip if we aren't creating the configuration from a dict
+        if not isinstance(values["offline_store"], Dict):
+            return values
+
+        # Make sure that the provider configuration is set. We need it to set the defaults
+        assert "provider" in values
+
+        # Set the default type
+        if "type" not in values["offline_store"]:
+            if values["provider"] == "local":
+                values["offline_store"]["type"] = "file"
+            elif values["provider"] == "gcp":
+                values["offline_store"]["type"] = "bigquery"
+
+        offline_store_type = values["offline_store"]["type"]
+
+        # Make sure the user hasn't provided the wrong type
+        assert offline_store_type in ["file", "bigquery"]
+
+        # Validate the dict to ensure one of the union types match
+        try:
+            if offline_store_type == "file":
+                FileOfflineStoreConfig(**values["offline_store"])
+            elif offline_store_type == "bigquery":
+                BigQueryOfflineStoreConfig(**values["offline_store"])
+            else:
                 raise ValidationError(
-                    [ErrorWrapper(e, loc="online_store")],
-                    model=SqliteOnlineStoreConfig,
+                    f"Invalid offline store type {offline_store_type}"
                 )
+        except ValidationError as e:
+            raise ValidationError(
+                [ErrorWrapper(e, loc="offline_store")], model=FileOfflineStoreConfig,
+            )
+
         return values
 
 
--- sdk/python/feast/repo_operations.py
+++ sdk/python/feast/repo_operations.py
@@ -13,6 +13,7 @@ from click.exceptions import BadParameter
 
 from feast import Entity, FeatureTable
 from feast.feature_view import FeatureView
+from feast.infra.offline_stores.helpers import assert_offline_store_supports_data_source
 from feast.infra.provider import get_provider
 from feast.names import adjectives, animals
 from feast.registry import Registry
@@ -139,6 +140,14 @@ def apply_total(repo_config: RepoConfig, repo_path: Path):
 
     for t in repo.feature_views:
         repo_table_names.add(t.name)
+
+    data_sources = [t.input for t in repo.feature_views]
+
+    # Make sure the data source used by this feature view is supported by
+    for data_source in data_sources:
+        assert_offline_store_supports_data_source(
+            repo_config.offline_store, data_source
+        )
 
     tables_to_delete = []
     for registry_table in registry.list_feature_tables(project=project):
