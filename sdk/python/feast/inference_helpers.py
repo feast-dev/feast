25dae145baa6655f929e47d685498c3b9d750f3a
David Y Liu <davidyliuliu@gmail.com>
2021-05-25 21:42:03 -0700

    Made error messages more descriptive and improved its structuring
    Signed-off-by: David Y Liu <davidyliuliu@gmail.com>

--- sdk/python/feast/inference_helpers.py
+++ sdk/python/feast/inference_helpers.py
@@ -24,10 +24,13 @@ def infer_entity_value_type_from_feature_views(
                 entity_col = list(
                     filter(lambda tup: tup[0] == entity_name, col_names_and_types)
                 )
-                if len(entity_col) > 1:
-                    raise ValueError("More than one table column matches Entity name.")
                 if len(entity_col) == 0:
-                    raise ValueError("No column in the table matches Entity name.")
+                    # Doesn't mention inference error because would also be an error without inferencing
+                    raise ValueError(
+                        f"""No column in the input source for the {view.name} FeatureView matches
+                        its Entity's name."""
+                    )
+
                 entity = incomplete_entities[entity_name]
                 inferred_value_type = view.input.source_datatype_to_feast_value_type()(
                     entity_col[0][1]
@@ -35,10 +38,13 @@ def infer_entity_value_type_from_feature_views(
                 if (
                     entity.value_type != ValueType.UNKNOWN
                     and entity.value_type != inferred_value_type
-                ):
+                ) or (len(entity_col) > 1):
                     raise ValueError(
-                        "Entity value_type inference failed. Multiple viable matches. Please explicitly specify Entity value_type for this Entity."
+                        f"""Entity value_type inference failed for {entity_name} Entity.
+                        Multiple viable matches. Please explicitly specify the Entity value_type
+                        for this Entity."""
                     )
+
                 entity.value_type = inferred_value_type
 
     return entities
