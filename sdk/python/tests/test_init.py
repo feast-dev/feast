4e31485f4157eb75c57f11b34c8ec2de87a9b932
ted chang <htchang@us.ibm.com>
2021-06-07 20:20:34 -0700

    Test invalid project name
    Signed-off-by: ted chang <htchang@us.ibm.com>

--- sdk/python/tests/test_init.py
+++ sdk/python/tests/test_init.py
@@ -1,6 +1,7 @@
 import tempfile
 from datetime import datetime, timedelta
 from pathlib import Path
+from textwrap import dedent
 
 from tests.cli_utils import CliRunner
 
@@ -25,3 +26,43 @@ def test_repo_init() -> None:
             ["materialize", start_date.isoformat(), end_date.isoformat()], cwd=repo_path
         )
         assert result.returncode == 0
+
+
+def test_repo_init_with_underscore_in_project_name() -> None:
+    """
+    Test `feast init` with underscore in the project name
+    """
+    with tempfile.TemporaryDirectory() as temp_dir:
+        temp_path = Path(temp_dir)
+        runner = CliRunner()
+
+        # `feast init` should fail with repo names start with underscore
+        invalid_repo_names = ["_test", "_test_1"]
+        for repo_name in invalid_repo_names:
+            result = runner.run(["init", repo_name], cwd=temp_path)
+            assert result.returncode != 0
+
+        # `feast init` should succeed with underscore in repo name
+        valid_repo_names = ["test_1"]
+        for repo_name in valid_repo_names:
+            result = runner.run(["init", repo_name], cwd=temp_path)
+            assert result.returncode == 0
+
+        # `feast apply` should fail with underscore in project name
+        project_name = "test_1"
+        repo_dir = temp_path / project_name
+        data_dir = repo_dir / "data"
+        repo_config = repo_dir / "feature_store.yaml"
+        repo_config.write_text(
+            dedent(
+                f"""
+        project: __foo
+        registry: {data_dir / "registry.db"}
+        provider: local
+        online_store:
+            path: {data_dir / "online_store.db"}
+        """
+            )
+        )
+        result = runner.run(["apply"], cwd=repo_dir)
+        assert result.returncode != 0
