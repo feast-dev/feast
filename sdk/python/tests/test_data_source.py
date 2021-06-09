d5eb8af667a779a48572cc0aa5f17494ce289219
Cody Lin <codyl@twitter.com>
2021-06-09 11:19:40 -0700

    fix lint errors and rebase
    Signed-off-by: Cody Lin <codyl@twitter.com>

--- sdk/python/tests/test_data_source.py
+++ sdk/python/tests/test_data_source.py
@@ -1,18 +1,17 @@
 import pytest
-from google.cloud import bigquery
 from utils.data_source_utils import simple_bq_source_using_table_ref_arg
+
+from feast.data_source import BigQuerySource
 
 
 @pytest.mark.integration
 def test_existent_bq_source(simple_dataset_1):
-    existent_bq = simple_bq_source_using_table_ref_arg(simple_dataset_1)
+    simple_bq_source_using_table_ref_arg(simple_dataset_1)
 
 
 @pytest.mark.integration
 def test_nonexistent_bq_source():
-    client = bigquery.Client()
-
     with pytest.raises(NameError):
-        nonexistent_bq = BigQuerySource(
+        BigQuerySource(
             table_ref="project.dataset.nonexistent_table", event_timestamp_column=""
         )
