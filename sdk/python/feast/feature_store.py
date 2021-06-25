3cb303f020300c1a9f1ed1f2d424ecaa90b5a657
David Y Liu <7172604+mavysavydav@users.noreply.github.com>
2021-06-25 15:19:23 -0700

    Grouped inferencing statements together in apply methods for easier readability (#1667)
    * grouped inferencing statements together
    
    Signed-off-by: David Y Liu <davidyliuliu@gmail.com>
    
    * update in testing
    
    Signed-off-by: David Y Liu <davidyliuliu@gmail.com>

--- sdk/python/feast/feature_store.py
+++ sdk/python/feast/feature_store.py
@@ -27,8 +27,8 @@ from feast.entity import Entity
 from feast.errors import FeastProviderLoginError, FeatureViewNotFoundException
 from feast.feature_view import FeatureView
 from feast.inference import (
-    infer_entity_value_type_from_feature_views,
     update_data_sources_with_inferred_event_timestamp_col,
+    update_entities_with_inferred_types_from_feature_views,
 )
 from feast.infra.provider import Provider, RetrievalJob, get_provider
 from feast.online_response import OnlineResponse, _infer_online_entity_rows
@@ -224,8 +224,11 @@ class FeatureStore:
         assert isinstance(objects, list)
 
         views_to_update = [ob for ob in objects if isinstance(ob, FeatureView)]
-        entities_to_update = infer_entity_value_type_from_feature_views(
-            [ob for ob in objects if isinstance(ob, Entity)], views_to_update
+        entities_to_update = [ob for ob in objects if isinstance(ob, Entity)]
+
+        # Make inferences
+        update_entities_with_inferred_types_from_feature_views(
+            entities_to_update, views_to_update
         )
         update_data_sources_with_inferred_event_timestamp_col(
             [view.input for view in views_to_update]
