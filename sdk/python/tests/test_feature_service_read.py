ea2210ddadc6c81c524060a44c02439dc0841227
Achal Shah <achals@gmail.com>
2021-07-19 12:21:32 -0700

    Remove dupe
    Signed-off-by: Achal Shah <achals@gmail.com>

--- sdk/python/tests/test_feature_service_read.py
+++ sdk/python/tests/test_feature_service_read.py
@@ -1,7 +1,7 @@
 import pytest
 
 from tests.cli_utils import CliRunner, get_example_repo
-from tests.online_read_write_test import basic_feature_service_rw_test
+from tests.online_read_write_test import basic_rw_test
 
 
 @pytest.mark.integration
@@ -15,7 +15,7 @@ def test_feature_service_read() -> None:
         get_example_repo("example_feature_repo_1.py"), "bigquery"
     ) as store:
 
-        basic_feature_service_rw_test(
+        basic_rw_test(
             store,
             view_name="driver_locations",
             feature_service_name="driver_locations_service",
