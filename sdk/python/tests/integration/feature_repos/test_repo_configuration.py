4529733f406170e30fea3cdbeb24d2d765e6f9f6
Achal Shah <achals@gmail.com>
2021-08-17 09:51:19 -0700

    Static config
    Signed-off-by: Achal Shah <achals@gmail.com>

--- sdk/python/tests/integration/feature_repos/test_repo_configuration.py
+++ sdk/python/tests/integration/feature_repos/test_repo_configuration.py
@@ -36,17 +36,37 @@ class TestRepoConfig:
     infer_event_timestamp_col: bool = True
 
 
+def ds_creator_path(cls: str):
+    return f"tests.integration.feature_repos.universal.data_sources.{cls}"
+
+
+DYNAMO_CONFIG = {"type": "dynamodb", "region": "us-west-2"}
+REDIS_CONFIG = {"type": "redis", "connection_string": "localhost:6379,db=0"}
 FULL_REPO_CONFIGS: List[TestRepoConfig] = [
-    TestRepoConfig(),  # Local
+    # Local configurations
+    TestRepoConfig(),
+    TestRepoConfig(online_store=REDIS_CONFIG),
+    # GCP configurations
     TestRepoConfig(
         provider="gcp",
-        offline_store_creator="tests.integration.feature_repos.universal.data_sources.bigquery.BigQueryDataSourceCreator",
+        offline_store_creator=ds_creator_path("bigquery.BigQueryDataSourceCreator"),
         online_store="datastore",
     ),
     TestRepoConfig(
+        provider="gcp",
+        offline_store_creator=ds_creator_path("bigquery.BigQueryDataSourceCreator"),
+        online_store=REDIS_CONFIG,
+    ),
+    # AWS configurations
+    TestRepoConfig(
         provider="aws",
-        offline_store_creator="tests.integration.feature_repos.universal.data_sources.redshift.RedshiftDataSourceCreator",
-        online_store={"type": "dynamodb", "region": "us-west-2"},
+        offline_store_creator=ds_creator_path("redshift.RedshiftDataSourceCreator"),
+        online_store=DYNAMO_CONFIG,
+    ),
+    TestRepoConfig(
+        provider="aws",
+        offline_store_creator=ds_creator_path("redshift.RedshiftDataSourceCreator"),
+        online_store=REDIS_CONFIG,
     ),
 ]
 
@@ -158,32 +178,6 @@ def vary_infer_event_timestamp_col(
     return new_configs
 
 
-def vary_redis_online_store(configs: List[TestRepoConfig],) -> List[TestRepoConfig]:
-    new_configs = []
-    redis_config = {"type": "redis", "connection_string": "localhost:6379,db=0"}
-    for c in configs:
-        if (
-            "FileDataSourceCreator" in c.offline_store_creator
-            and "sqlite" in c.online_store
-        ):
-            new_configs.append(c)
-            new_configs.append(replace(c, online_store=redis_config))
-        elif (
-            "RedshiftDataSourceCreator" in c.offline_store_creator
-            and isinstance(c.online_store, Dict)
-            and c.online_store["type"] == "dynamodb"
-        ):
-            new_configs.append(c)
-            new_configs.append(replace(c, online_store=redis_config))
-        elif (
-            "BigQueryDataSourceCreator" in c.offline_store_creator
-            and c.online_store == "datastore"
-        ):
-            new_configs.append(c)
-            new_configs.append(replace(c, online_store=redis_config))
-    return new_configs
-
-
 def vary_providers_for_offline_stores(
     configs: List[TestRepoConfig],
 ) -> List[TestRepoConfig]:
@@ -282,9 +276,7 @@ def parametrize_e2e_test(e2e_test):
     """
 
     @pytest.mark.integration
-    @pytest.mark.parametrize(
-        "config", vary_redis_online_store(FULL_REPO_CONFIGS), ids=lambda v: str(v)
-    )
+    @pytest.mark.parametrize("config", FULL_REPO_CONFIGS, ids=lambda v: str(v))
     def inner_test(config):
         with construct_test_environment(config) as environment:
             e2e_test(environment)
@@ -305,8 +297,7 @@ def parametrize_offline_retrieval_test(offline_retrieval_test):
     The decorator takes care of tearing down the feature store, as well as the sample data.
     """
 
-    configs = vary_redis_online_store(FULL_REPO_CONFIGS)
-    configs = vary_providers_for_offline_stores(configs)
+    configs = vary_providers_for_offline_stores(FULL_REPO_CONFIGS)
     configs = vary_full_feature_names(configs)
     configs = vary_infer_event_timestamp_col(configs)
 
