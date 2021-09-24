a309fbb7ebc68d8dd37b392b765a73cd0015adc4
David Y Liu <davidyliuliu@gmail.com>
2021-09-24 10:11:07 -0700

    Modified FeatureNameCollisionError msg
    Signed-off-by: David Y Liu <davidyliuliu@gmail.com>

--- sdk/python/feast/errors.py
+++ sdk/python/feast/errors.py
@@ -146,8 +146,9 @@ class FeatureNameCollisionError(Exception):
         if full_feature_names:
             collisions = [ref.replace(":", "__") for ref in feature_refs_collisions]
             error_message = (
-                "To resolve this collision, please ensure that the features in question "
-                "have different names."
+                "To resolve this collision, please ensure that the feature views or their own features "
+                "have different names. If you're intentionally joining the same feature view twice on "
+                "different sets of entities, please rename one of the feature views with '.with_name'."
             )
         else:
             collisions = [ref.split(":")[1] for ref in feature_refs_collisions]
