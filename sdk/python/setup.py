fd6b582c7299c7c7272a8eb0d818d251cf495aa7
Judah Rand <17158624+judahrand@users.noreply.github.com>
2021-10-13 16:06:08 +0100

    Ensure `pyarrow` supports `use_compliant_nested_type`
    Signed-off-by: Judah Rand <17158624+judahrand@users.noreply.github.com>

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -54,7 +54,7 @@ REQUIRED = [
     "pandas>=1.0.0",
     "pandavro==1.5.*",
     "protobuf>=3.10",
-    "pyarrow>=2.0.0",
+    "pyarrow>=4.0.0",
     "pydantic>=1.0.0",
     "PyYAML>=5.4.*",
     "tabulate==0.8.*",
