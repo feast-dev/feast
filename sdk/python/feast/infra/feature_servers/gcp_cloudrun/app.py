0892133b057f1ca2ffa2904f2d1d847543e57b9d
Judah Rand <17158624+judahrand@users.noreply.github.com>
2021-10-15 09:17:39 +0100

    Sync CloudRun `app.py` with AWS Lambda
    Signed-off-by: Judah Rand <17158624+judahrand@users.noreply.github.com>

--- sdk/python/feast/infra/feature_servers/gcp_cloudrun/app.py
+++ sdk/python/feast/infra/feature_servers/gcp_cloudrun/app.py
@@ -3,32 +3,19 @@ import os
 import tempfile
 from pathlib import Path
 
-import yaml
-
 from feast import FeatureStore
+from feast.constants import FEATURE_STORE_YAML_ENV_NAME
 from feast.feature_server import get_app
 
 # Load RepoConfig
-config_base64 = os.environ["FEAST_CONFIG_BASE64"]
+config_base64 = os.environ[FEATURE_STORE_YAML_ENV_NAME]
 config_bytes = base64.b64decode(config_base64)
 
-# Override the registry path
-config_yaml = yaml.safe_load(config_bytes)
-config_yaml["registry"] = "registry.db"
-config_bytes = yaml.safe_dump(config_yaml).encode()
-
-# Load Registry
-registry_base64 = os.environ["FEAST_REGISTRY_BASE64"]
-registry_bytes = base64.b64decode(registry_base64)
-
-# Create a new unique directory for writing feature_store.yaml and registry.db files
+# Create a new unique directory for writing feature_store.yaml
 repo_path = Path(tempfile.mkdtemp())
 
 with open(repo_path / "feature_store.yaml", "wb") as f:
     f.write(config_bytes)
-
-with open(repo_path / "registry.db", "wb") as f:
-    f.write(registry_bytes)
 
 # Initialize the feature store
 store = FeatureStore(repo_path=str(repo_path.resolve()))
