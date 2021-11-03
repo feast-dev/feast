049999e7ee5bb3c0f5221e28a6e45f3460896cf3
Yusuke Nishioka <yusuke.nishioka.0713@gmail.com>
2021-11-03 11:28:16 +0900

    Fix protobuf version conflict
    Signed-off-by: Yusuke Nishioka <yusuke.nishioka.0713@gmail.com>

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -66,6 +66,9 @@ REQUIRED = [
 ]
 
 GCP_REQUIRED = [
+    # proto-plus>=1.19.7 requires protobuf>=3.19.0
+    # which conflicts with the version required by google-api-core[grpc]
+    "proto-plus<1.19.7",
     "google-cloud-bigquery>=2.28.1",
     "google-cloud-bigquery-storage >= 2.0.0",
     "google-cloud-datastore>=2.1.*",
@@ -112,6 +115,9 @@ CI_REQUIRED = [
     "firebase-admin==4.5.2",
     "pre-commit",
     "assertpy==1.1",
+    # proto-plus>=1.19.7 requires protobuf>=3.19.0
+    # which conflicts with the version required by google-api-core[grpc]
+    "proto-plus<1.19.7",
     "google-cloud-bigquery>=2.28.1",
     "google-cloud-bigquery-storage >= 2.0.0",
     "google-cloud-datastore>=2.1.*",
@@ -231,7 +237,7 @@ setup(
     ],
     entry_points={"console_scripts": ["feast=feast.cli:cli"]},
     use_scm_version=use_scm_version,
-    setup_requires=["setuptools_scm", "grpcio", "grpcio-tools==1.34.0", "mypy-protobuf", "sphinx!=4.0.0"],
+    setup_requires=["setuptools_scm", "grpcio", "grpcio-tools==1.34.0", "mypy-protobuf==1.*", "sphinx!=4.0.0"],
     package_data={
         "": [
             "protos/feast/**/*.proto",
