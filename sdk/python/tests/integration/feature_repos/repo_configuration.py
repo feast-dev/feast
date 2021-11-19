27ac25e4cb617c996c5dcb53f43202a39bfd5fb2
Achal Shah <achals@gmail.com>
2021-11-18 22:45:32 -0800

    Add github run id into the integration test projects for debugging
    Signed-off-by: Achal Shah <achals@gmail.com>

--- sdk/python/tests/integration/feature_repos/repo_configuration.py
+++ sdk/python/tests/integration/feature_repos/repo_configuration.py
@@ -240,7 +240,14 @@ def construct_test_environment(
     test_repo_config: IntegrationTestRepoConfig,
     test_suite_name: str = "integration_test",
 ) -> Environment:
-    project = f"{test_suite_name}_{str(uuid.uuid4()).replace('-', '')[:8]}"
+
+    _uuid = str(uuid.uuid4()).replace("-", "")[:8]
+
+    run_id = os.getenv("GITHUB_RUN_ID", default=None)
+    run_id = f"{run_id}_{_uuid}" if run_id else _uuid
+    run_num = os.getenv("GITHUB_RUN_NUMBER", default=1)
+
+    project = f"{test_suite_name}_{run_id}_{run_num}"
 
     offline_creator: DataSourceCreator = test_repo_config.offline_store_creator(project)
 
