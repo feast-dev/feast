0e4eac827066473396cccedfd5e52a3d175b6c83
Achal Shah <achals@gmail.com>
2021-12-16 15:51:17 -0800

    Fix inference
    Signed-off-by: Achal Shah <achals@gmail.com>

--- sdk/python/feast/diff/FcoDiff.py
+++ sdk/python/feast/diff/FcoDiff.py
@@ -83,15 +83,13 @@ FIELDS_TO_IGNORE = {"project"}
 def diff_between(current: U, new: U, object_type: str) -> FcoDiff:
     assert current.DESCRIPTOR.full_name == new.DESCRIPTOR.full_name
     property_diffs = []
-    transition: TransitionType
-    if current.spec == new.spec:
-        transition = TransitionType.UNCHANGED
-    else:
-        transition = TransitionType.UPDATE
+    transition: TransitionType = TransitionType.UNCHANGED
+    if current.spec != new.spec:
         for _field in current.spec.DESCRIPTOR.fields:
             if _field.name in FIELDS_TO_IGNORE:
                 continue
             if getattr(current.spec, _field.name) != getattr(new.spec, _field.name):
+                transition = TransitionType.UPDATE
                 property_diffs.append(
                     PropertyDiff(
                         _field.name,
