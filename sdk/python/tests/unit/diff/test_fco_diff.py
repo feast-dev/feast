b29c4cbbb977ba211282b3336b85e0d600e863a8
Felix Wang <wangfelix98@gmail.com>
2022-01-21 11:54:42 -0800

    Add FCO test
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>

--- sdk/python/tests/unit/diff/test_fco_diff.py
+++ sdk/python/tests/unit/diff/test_fco_diff.py
@@ -1,4 +1,8 @@
-from feast.diff.FcoDiff import diff_between, tag_proto_objects_for_keep_delete_add
+from feast.diff.FcoDiff import (
+    diff_between,
+    tag_objects_for_keep_delete_update_add,
+    tag_proto_objects_for_keep_delete_add,
+)
 from feast.feature_view import FeatureView
 from tests.utils.data_source_utils import prep_file_source
 
@@ -41,6 +45,52 @@ def test_tag_proto_objects_for_keep_delete_add(simple_dataset_1):
         assert pre_changed not in keep
         assert len(list(delete)) == 1
         assert to_delete in delete
+        assert len(list(add)) == 1
+        assert to_add in add
+
+
+def test_tag_objects_for_keep_delete_update_add(simple_dataset_1):
+    with prep_file_source(
+        df=simple_dataset_1, event_timestamp_column="ts_1"
+    ) as file_source:
+        to_delete = FeatureView(
+            name="to_delete", entities=["id"], batch_source=file_source, ttl=None,
+        )
+        unchanged_fv = FeatureView(
+            name="fv1", entities=["id"], batch_source=file_source, ttl=None,
+        )
+        pre_changed = FeatureView(
+            name="fv2",
+            entities=["id"],
+            batch_source=file_source,
+            ttl=None,
+            tags={"when": "before"},
+        )
+        post_changed = FeatureView(
+            name="fv2",
+            entities=["id"],
+            batch_source=file_source,
+            ttl=None,
+            tags={"when": "after"},
+        )
+        to_add = FeatureView(
+            name="to_add", entities=["id"], batch_source=file_source, ttl=None,
+        )
+
+        keep, delete, update, add = tag_objects_for_keep_delete_update_add(
+            [unchanged_fv, pre_changed, to_delete], [unchanged_fv, post_changed, to_add]
+        )
+
+        assert len(list(keep)) == 2
+        assert unchanged_fv in keep
+        assert pre_changed in keep
+        assert post_changed not in keep
+        assert len(list(delete)) == 1
+        assert to_delete in delete
+        assert len(list(update)) == 2
+        assert unchanged_fv in update
+        assert post_changed in update
+        assert pre_changed not in update
         assert len(list(add)) == 1
         assert to_add in add
 
