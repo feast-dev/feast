592af75e63766a380673b2dc8a879e70d2df5818
Michelle Rascati <44408275+michelle-rascati-sp@users.noreply.github.com>
2022-01-28 01:57:54 -0600

    historical_field_mappings2 merge for one sign off commit (#2252)
    Signed-off-by: Michelle Rascati <michelle.rascati@sailpoint.com>

--- sdk/python/tests/integration/feature_repos/universal/feature_views.py
+++ sdk/python/tests/integration/feature_repos/universal/feature_views.py
@@ -217,3 +217,13 @@ def create_location_stats_feature_view(source, infer_features: bool = False):
         ttl=timedelta(days=2),
     )
     return location_stats_feature_view
+
+
+def create_field_mapping_feature_view(source):
+    return FeatureView(
+        name="field_mapping",
+        entities=[],
+        features=[Feature(name="feature_name", dtype=ValueType.INT32)],
+        batch_source=source,
+        ttl=timedelta(days=2),
+    )
