e617a0698cbb7ae782eac1c4b6672333d29b5ab2
Felix Wang <wangfelix98@gmail.com>
2022-01-30 09:02:52 -0800

    Feast plan clean up (#2256)
    * Run validation and inference on views and entities during plan
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Do not log objects that are unchanged
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Rename Fco to FeastObject
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Remove useless method
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Lint
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Always initialize registry during feature store initialization
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Fix usage test
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Remove print statements
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    Signed-off-by: sfc-gh-madkins <miles.adkins@snowflake.com>

--- sdk/python/tests/unit/diff/test_registry_diff.py
+++ sdk/python/tests/unit/diff/test_registry_diff.py
@@ -1,52 +1,9 @@
-from feast.diff.FcoDiff import (
+from feast.diff.registry_diff import (
     diff_registry_objects,
     tag_objects_for_keep_delete_update_add,
-    tag_proto_objects_for_keep_delete_add,
 )
 from feast.feature_view import FeatureView
 from tests.utils.data_source_utils import prep_file_source
-
-
-def test_tag_proto_objects_for_keep_delete_add(simple_dataset_1):
-    with prep_file_source(
-        df=simple_dataset_1, event_timestamp_column="ts_1"
-    ) as file_source:
-        to_delete = FeatureView(
-            name="to_delete", entities=["id"], batch_source=file_source, ttl=None,
-        ).to_proto()
-        unchanged_fv = FeatureView(
-            name="fv1", entities=["id"], batch_source=file_source, ttl=None,
-        ).to_proto()
-        pre_changed = FeatureView(
-            name="fv2",
-            entities=["id"],
-            batch_source=file_source,
-            ttl=None,
-            tags={"when": "before"},
-        ).to_proto()
-        post_changed = FeatureView(
-            name="fv2",
-            entities=["id"],
-            batch_source=file_source,
-            ttl=None,
-            tags={"when": "after"},
-        ).to_proto()
-        to_add = FeatureView(
-            name="to_add", entities=["id"], batch_source=file_source, ttl=None,
-        ).to_proto()
-
-        keep, delete, add = tag_proto_objects_for_keep_delete_add(
-            [unchanged_fv, pre_changed, to_delete], [unchanged_fv, post_changed, to_add]
-        )
-
-        assert len(list(keep)) == 2
-        assert unchanged_fv in keep
-        assert post_changed in keep
-        assert pre_changed not in keep
-        assert len(list(delete)) == 1
-        assert to_delete in delete
-        assert len(list(add)) == 1
-        assert to_add in add
 
 
 def test_tag_objects_for_keep_delete_update_add(simple_dataset_1):
@@ -114,12 +71,20 @@ def test_diff_registry_objects_feature_views(simple_dataset_1):
             tags={"when": "after"},
         )
 
-        fco_diffs = diff_registry_objects(pre_changed, pre_changed, "feature view")
-        assert len(fco_diffs.fco_property_diffs) == 0
+        feast_object_diffs = diff_registry_objects(
+            pre_changed, pre_changed, "feature view"
+        )
+        assert len(feast_object_diffs.feast_object_property_diffs) == 0
 
-        fco_diffs = diff_registry_objects(pre_changed, post_changed, "feature view")
-        assert len(fco_diffs.fco_property_diffs) == 1
+        feast_object_diffs = diff_registry_objects(
+            pre_changed, post_changed, "feature view"
+        )
+        assert len(feast_object_diffs.feast_object_property_diffs) == 1
 
-        assert fco_diffs.fco_property_diffs[0].property_name == "tags"
-        assert fco_diffs.fco_property_diffs[0].val_existing == {"when": "before"}
-        assert fco_diffs.fco_property_diffs[0].val_declared == {"when": "after"}
+        assert feast_object_diffs.feast_object_property_diffs[0].property_name == "tags"
+        assert feast_object_diffs.feast_object_property_diffs[0].val_existing == {
+            "when": "before"
+        }
+        assert feast_object_diffs.feast_object_property_diffs[0].val_declared == {
+            "when": "after"
+        }
