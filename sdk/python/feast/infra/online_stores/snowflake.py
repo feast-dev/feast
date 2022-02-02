6bc03bb8d8831877c5ccbc299e204e78ecea8375
sfc-gh-madkins <miles.adkins@snowflake.com>
2022-02-02 11:20:51 -0600

    initial hybrid code
    Signed-off-by: sfc-gh-madkins <miles.adkins@snowflake.com>

---
+++ sdk/python/feast/infra/online_stores/snowflake.py
@@ -0,0 +1,242 @@
+import itertools
+import os
+from binascii import hexlify
+from datetime import datetime
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple
+
+import pandas as pd
+import pytz
+from pydantic import Field
+from pydantic.schema import Literal
+
+from feast import Entity, FeatureView
+from feast.infra.key_encoding_utils import serialize_entity_key
+from feast.infra.online_stores.online_store import OnlineStore
+from feast.infra.utils.snowflake_utils import get_snowflake_conn, write_pandas_binary
+from feast.protos.feast.types.EntityKey_pb2 import EntityKey as EntityKeyProto
+from feast.protos.feast.types.Value_pb2 import Value as ValueProto
+from feast.repo_config import FeastConfigBaseModel, RepoConfig
+from feast.usage import log_exceptions_and_usage
+
+
+class SnowflakeOnlineStoreConfig(FeastConfigBaseModel):
+    """ Online store config for Snowflake """
+
+    type: Literal["snowflake.online"] = "snowflake.online"
+    """ Online store type selector"""
+
+    config_path: Optional[str] = (
+        Path(os.environ["HOME"]) / ".snowsql/config"
+    ).__str__()
+    """ Snowflake config path -- absolute path required (Can't use ~)"""
+
+    account: Optional[str] = None
+    """ Snowflake deployment identifier -- drop .snowflakecomputing.com"""
+
+    user: Optional[str] = None
+    """ Snowflake user name """
+
+    password: Optional[str] = None
+    """ Snowflake password """
+
+    role: Optional[str] = None
+    """ Snowflake role name"""
+
+    warehouse: Optional[str] = None
+    """ Snowflake warehouse name """
+
+    database: Optional[str] = None
+    """ Snowflake database name """
+
+    schema_: Optional[str] = Field("PUBLIC", alias="schema")
+    """ Snowflake schema name """
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class SnowflakeOnlineStore(OnlineStore):
+    """
+    OnlineStore is an object used for all interaction between Feast and the service used for offline storage of
+    features.
+    """
+
+    @log_exceptions_and_usage(online_store="snowflake")
+    def online_write_batch(
+        self,
+        config: RepoConfig,
+        table: FeatureView,
+        data: List[
+            Tuple[EntityKeyProto, Dict[str, ValueProto], datetime, Optional[datetime]]
+        ],
+        progress: Optional[Callable[[int], Any]],
+    ) -> None:
+        assert isinstance(config.online_store, SnowflakeOnlineStoreConfig)
+
+        snowflake_conn = get_snowflake_conn(config.online_store, autocommit=False)
+
+        dfs = [None] * len(data)
+        for i, (entity_key, values, timestamp, created_ts) in enumerate(data):
+
+            df = pd.DataFrame(
+                columns=[
+                    "entity_key",
+                    "feature_name",
+                    "value",
+                    "event_ts",
+                    "created_ts",
+                ],
+                index=range(0, len(values)),
+            )
+
+            timestamp = _to_naive_utc(timestamp)
+            if created_ts is not None:
+                created_ts = _to_naive_utc(created_ts)
+
+            for j, (feature_name, val) in enumerate(values.items()):
+                df.loc[j, "entity_key"] = serialize_entity_key(entity_key)
+                df.loc[j, "feature_name"] = feature_name
+                df.loc[j, "value"] = val.SerializeToString()
+                df.loc[j, "event_ts"] = timestamp
+                df.loc[j, "created_ts"] = created_ts
+
+            dfs[i] = df
+            if progress:
+                progress(1)
+
+        if dfs:
+            agg_df = pd.concat(dfs)
+
+            with snowflake_conn as conn:
+
+                write_pandas_binary(conn, agg_df, f"{config.project}_{table.name}")
+
+                query2 = f"""
+                    INSERT OVERWRITE INTO "{config.online_store.database}"."{config.online_store.schema_}"."{config.project}_{table.name}"
+                        SELECT
+                            "entity_key",
+                            "feature_name",
+                            "value",
+                            "event_ts",
+                            "created_ts"
+                        FROM
+                          (SELECT
+                              *,
+                              ROW_NUMBER() OVER(PARTITION BY "entity_key","feature_name" ORDER BY "event_ts" DESC, "created_ts" DESC) AS "_feast_row"
+                          FROM
+                              "{config.online_store.database}"."{config.online_store.schema_}"."{config.project}_{table.name}")
+                        WHERE
+                            "_feast_row" = 1;
+                """
+
+                conn.cursor().execute(query2)
+        return None
+
+    @log_exceptions_and_usage(online_store="snowflake")
+    def online_read(
+        self,
+        config: RepoConfig,
+        table: FeatureView,
+        entity_keys: List[EntityKeyProto],
+        requested_features: Optional[List[str]] = None,
+    ) -> List[Tuple[Optional[datetime], Optional[Dict[str, ValueProto]]]]:
+        assert isinstance(config.online_store, SnowflakeOnlineStoreConfig)
+
+        snowflake_conn = get_snowflake_conn(config.online_store)
+
+        result: List[Tuple[Optional[datetime], Optional[Dict[str, ValueProto]]]] = []
+
+        with snowflake_conn as conn:
+
+            data = list(
+                conn.cursor()
+                .execute(
+                    f"""
+                SELECT
+                    "entity_key", "feature_name", "value", "event_ts"
+                FROM
+                    "{config.online_store.database}"."{config.online_store.schema_}"."{config.project}_{table.name}"
+                WHERE
+                    "entity_key" IN ({','.join([('TO_BINARY('+hexlify(serialize_entity_key(entity_key)).__str__()[1:]+")") for entity_key in entity_keys])})
+                ORDER BY
+                    "entity_key"
+            """,
+                )
+                .fetch_pandas_all()
+                .to_records(index=False)
+            )
+
+        rows = {
+            k: list(group) for k, group in itertools.groupby(data, key=lambda r: r[0])
+        }
+        for entity_key in entity_keys:
+            entity_key_bin = serialize_entity_key(entity_key)
+            res = {}
+            res_ts = None
+            for _, feature_name, val_bin, ts in rows.get(entity_key_bin, []):
+                val = ValueProto()
+                val.ParseFromString(val_bin)
+                res[feature_name] = val
+                res_ts = ts
+
+            if not res:
+                result.append((None, None))
+            else:
+                result.append((res_ts, res))
+        return result
+
+    @log_exceptions_and_usage(online_store="snowflake")
+    def update(
+        self,
+        config: RepoConfig,
+        tables_to_delete: Sequence[FeatureView],
+        tables_to_keep: Sequence[FeatureView],
+        entities_to_delete: Sequence[Entity],
+        entities_to_keep: Sequence[Entity],
+        partial: bool,
+    ):
+        assert isinstance(config.online_store, SnowflakeOnlineStoreConfig)
+
+        snowflake_conn = get_snowflake_conn(config.online_store)
+
+        with snowflake_conn as conn:
+
+            for table in tables_to_keep:
+                conn.cursor().execute(
+                    f"""CREATE TABLE IF NOT EXISTS "{config.online_store.database}"."{config.online_store.schema_}"."{config.project}_{table.name}" (
+                        "entity_key" BINARY,
+                        "feature_name" VARCHAR,
+                        "value" BINARY,
+                        "event_ts" TIMESTAMP,
+                        "created_ts" TIMESTAMP
+                        )"""
+                )
+
+            for table in tables_to_delete:
+                conn.cursor().execute(
+                    f'DROP TABLE IF EXISTS "{config.online_store.database}"."{config.online_store.schema_}"."{config.project}_{table.name}"'
+                )
+
+    def teardown(
+        self,
+        config: RepoConfig,
+        tables: Sequence[FeatureView],
+        entities: Sequence[Entity],
+    ):
+        assert isinstance(config.online_store, SnowflakeOnlineStoreConfig)
+
+        snowflake_conn = get_snowflake_conn(config.online_store)
+
+        with snowflake_conn as conn:
+
+            for table in tables:
+                query = f'DROP TABLE IF EXISTS "{config.online_store.database}"."{config.online_store.schema_}"."{config.project}_{table.name}"'
+                conn.cursor().execute(query)
+
+
+def _to_naive_utc(ts: datetime):
+    if ts.tzinfo is None:
+        return ts
+    else:
+        return ts.astimezone(pytz.utc).replace(tzinfo=None)
--- sdk/python/feast/infra/utils/snowflake_utils.py
+++ sdk/python/feast/infra/utils/snowflake_utils.py
@@ -35,6 +35,8 @@ def execute_snowflake_statement(conn: SnowflakeConnection, query) -> SnowflakeCu
 def get_snowflake_conn(config, autocommit=True) -> SnowflakeConnection:
     if config.type == "snowflake.offline":
         config_header = "connections.feast_offline_store"
+    elif config.type == "snowflake.online":
+        config_header = "connections.feast_online_store"
 
     config = dict(config)
 
@@ -271,6 +273,180 @@ def create_temporary_sfc_stage(cursor: SnowflakeCursor) -> str:
         raise SnowflakeQueryUnknownError(create_stage_sql)
     result_cursor.fetchall()
     return stage_name
+
+
+def write_pandas_binary(
+    conn: "SnowflakeConnection",
+    df: pd.DataFrame,
+    table_name: str,
+    database: Optional[str] = None,
+    schema: Optional[str] = None,
+    chunk_size: Optional[int] = None,
+    compression: str = "gzip",
+    on_error: str = "abort_statement",
+    parallel: int = 4,
+    quote_identifiers: bool = True,
+) -> Tuple[
+    bool,
+    int,
+    int,
+    Sequence[
+        Tuple[
+            str,
+            str,
+            int,
+            int,
+            int,
+            int,
+            Optional[str],
+            Optional[int],
+            Optional[int],
+            Optional[str],
+        ]
+    ],
+]:
+    """Allows users to most efficiently write back a pandas DataFrame to Snowflake.
+
+    It works by dumping the DataFrame into Parquet files, uploading them and finally copying their data into the table.
+
+    Returns whether all files were ingested correctly, number of chunks uploaded, and number of rows ingested
+    with all of the COPY INTO command's output for debugging purposes.
+
+        Example usage:
+            import pandas
+            from snowflake.connector.pandas_tools import write_pandas
+
+            df = pandas.DataFrame([('Mark', 10), ('Luke', 20)], columns=['name', 'balance'])
+            success, nchunks, nrows, _ = write_pandas(cnx, df, 'customers')
+
+    Args:
+        conn: Connection to be used to communicate with Snowflake.
+        df: Dataframe we'd like to write back.
+        table_name: Table name where we want to insert into.
+        database: Database schema and table is in, if not provided the default one will be used (Default value = None).
+        schema: Schema table is in, if not provided the default one will be used (Default value = None).
+        chunk_size: Number of elements to be inserted once, if not provided all elements will be dumped once
+            (Default value = None).
+        compression: The compression used on the Parquet files, can only be gzip, or snappy. Gzip gives supposedly a
+            better compression, while snappy is faster. Use whichever is more appropriate (Default value = 'gzip').
+        on_error: Action to take when COPY INTO statements fail, default follows documentation at:
+            https://docs.snowflake.com/en/sql-reference/sql/copy-into-table.html#copy-options-copyoptions
+            (Default value = 'abort_statement').
+        parallel: Number of threads to be used when uploading chunks, default follows documentation at:
+            https://docs.snowflake.com/en/sql-reference/sql/put.html#optional-parameters (Default value = 4).
+        quote_identifiers: By default, identifiers, specifically database, schema, table and column names
+            (from df.columns) will be quoted. If set to False, identifiers are passed on to Snowflake without quoting.
+            I.e. identifiers will be coerced to uppercase by Snowflake.  (Default value = True)
+
+    Returns:
+        Returns the COPY INTO command's results to verify ingestion in the form of a tuple of whether all chunks were
+        ingested correctly, # of chunks, # of ingested rows, and ingest's output.
+    """
+    if database is not None and schema is None:
+        raise ProgrammingError(
+            "Schema has to be provided to write_pandas when a database is provided"
+        )
+    # This dictionary maps the compression algorithm to Snowflake put copy into command type
+    # https://docs.snowflake.com/en/sql-reference/sql/copy-into-table.html#type-parquet
+    compression_map = {"gzip": "auto", "snappy": "snappy"}
+    if compression not in compression_map.keys():
+        raise ProgrammingError(
+            "Invalid compression '{}', only acceptable values are: {}".format(
+                compression, compression_map.keys()
+            )
+        )
+    if quote_identifiers:
+        location = (
+            (('"' + database + '".') if database else "")
+            + (('"' + schema + '".') if schema else "")
+            + ('"' + table_name + '"')
+        )
+    else:
+        location = (
+            (database + "." if database else "")
+            + (schema + "." if schema else "")
+            + (table_name)
+        )
+    if chunk_size is None:
+        chunk_size = len(df)
+    cursor = conn.cursor()
+    stage_name = None  # Forward declaration
+    while True:
+        try:
+            stage_name = "".join(
+                random.choice(string.ascii_lowercase) for _ in range(5)
+            )
+            create_stage_sql = (
+                "create temporary stage /* Python:snowflake.connector.pandas_tools.write_pandas() */ "
+                '"{stage_name}"'
+            ).format(stage_name=stage_name)
+            logger.debug("creating stage with '{}'".format(create_stage_sql))
+            cursor.execute(create_stage_sql, _is_internal=True).fetchall()
+            break
+        except ProgrammingError as pe:
+            if pe.msg.endswith("already exists."):
+                continue
+            raise
+
+    with TemporaryDirectory() as tmp_folder:
+        for i, chunk in chunk_helper(df, chunk_size):
+            chunk_path = os.path.join(tmp_folder, "file{}.txt".format(i))
+            # Dump chunk into parquet file
+            chunk.to_parquet(chunk_path, compression=compression)
+            # Upload parquet file
+            upload_sql = (
+                "PUT /* Python:snowflake.connector.pandas_tools.write_pandas() */ "
+                "'file://{path}' @\"{stage_name}\" PARALLEL={parallel}"
+            ).format(
+                path=chunk_path.replace("\\", "\\\\").replace("'", "\\'"),
+                stage_name=stage_name,
+                parallel=parallel,
+            )
+            logger.debug("uploading files with '{}'".format(upload_sql))
+            cursor.execute(upload_sql, _is_internal=True)
+            # Remove chunk file
+            os.remove(chunk_path)
+    if quote_identifiers:
+        columns = '"' + '","'.join(list(df.columns)) + '"'
+    else:
+        columns = ",".join(list(df.columns))
+
+    # in Snowflake, all parquet data is stored in a single column, $1, so we must select columns explicitly
+    # see (https://docs.snowflake.com/en/user-guide/script-data-load-transform-parquet.html)
+    if quote_identifiers:
+        parquet_columns = ",".join(
+            f'TO_BINARY($1:"{c}")' if c in ["entity_key", "value"] else f'$1:"{c}"'
+            for c in df.columns
+        )
+    else:
+        parquet_columns = ",".join(
+            f"TO_BINARY($1:{c})" if c in ["entity_key", "value"] else f"$1:{c}"
+            for c in df.columns
+        )
+
+    copy_into_sql = (
+        "COPY INTO {location} /* Python:snowflake.connector.pandas_tools.write_pandas() */ "
+        "({columns}) "
+        'FROM (SELECT {parquet_columns} FROM @"{stage_name}") '
+        "FILE_FORMAT=(TYPE=PARQUET COMPRESSION={compression} BINARY_AS_TEXT = FALSE) "
+        "PURGE=TRUE ON_ERROR={on_error}"
+    ).format(
+        location=location,
+        columns=columns,
+        parquet_columns=parquet_columns,
+        stage_name=stage_name,
+        compression=compression_map[compression],
+        on_error=on_error,
+    )
+    logger.debug("copying into with '{}'".format(copy_into_sql))
+    copy_results = cursor.execute(copy_into_sql, _is_internal=True).fetchall()
+    cursor.close()
+    return (
+        all(e[1] == "LOADED" for e in copy_results),
+        len(copy_results),
+        sum(int(e[3]) for e in copy_results),
+        copy_results,
+    )
 
 
 def chunk_helper(lst: pd.DataFrame, n: int) -> Iterator[Tuple[int, pd.DataFrame]]:
