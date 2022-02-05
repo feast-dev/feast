03fc5106a1713e0c47e358d14849ab09bb7a8bda
Tsotne Tabidze <tsotne@tecton.ai>
2022-02-04 18:03:35 -0800

    remove helm init (v3 migration)
    Signed-off-by: Tsotne Tabidze <tsotne@tecton.ai>

--- infra/scripts/helm/install-helm.sh
+++ infra/scripts/helm/install-helm.sh
@@ -6,5 +6,4 @@ readonly STABLE_REPO_URL=https://charts.helm.sh/stable
 readonly INCUBATOR_REPO_URL=https://charts.helm.sh/incubator
 curl -s "https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz" | tar -C /tmp -xz
 sudo mv /tmp/linux-amd64/helm /usr/bin/helm
-helm init --client-only
 helm repo add incubator "$INCUBATOR_REPO_URL"
