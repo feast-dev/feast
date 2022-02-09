6dc165ec59e833a5cce27804e0a3dbe80936f5d1
Danny Chiao <danny@tecton.ai>
2022-02-09 16:40:24 -0500

    address linter
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- sdk/python/feast/registry.py
+++ sdk/python/feast/registry.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import logging
-import pickle
 from collections import defaultdict
 from datetime import datetime, timedelta
 from enum import Enum
