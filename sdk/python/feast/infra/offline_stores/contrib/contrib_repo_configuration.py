ad0bad058d1126fd78fbf8fd310f197453f3ff7b
Kevin Zhang <kzhang@tecton.ai>
2022-03-04 16:03:11 -0800

    Clean up
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/feast/infra/offline_stores/contrib/contrib_repo_configuration.py
+++ sdk/python/feast/infra/offline_stores/contrib/contrib_repo_configuration.py
@@ -1,12 +1,10 @@
 from tests.integration.feature_repos.integration_test_repo_config import (
     IntegrationTestRepoConfig,
 )
+from tests.integration.feature_repos.universal.data_sources.spark_data_source_creator import (
+    SparkDataSourceCreator,
+)
 
-from tests.integration.feature_repos.universal.data_sources.spark_data_source_creator import SparkDataSourceCreator
-
-print("Importing new repo_configs")
 FULL_REPO_CONFIGS = [
-    IntegrationTestRepoConfig(
-        offline_store_creator=SparkDataSourceCreator,
-    )
+    IntegrationTestRepoConfig(offline_store_creator=SparkDataSourceCreator,)
 ]
