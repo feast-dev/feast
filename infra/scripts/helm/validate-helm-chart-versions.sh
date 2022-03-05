f6129b069083a2b12142fb728e56263b8bd3416a
Willem Pienaar <git@willem.co>
2022-03-04 21:05:24 -0800

    ci: Add version bump script
    Signed-off-by: Willem Pienaar <git@willem.co>

--- infra/scripts/helm/validate-helm-chart-versions.sh
+++ infra/scripts/helm/validate-helm-chart-versions.sh
@@ -21,7 +21,7 @@ echo "Found ${CHANGED_VERSIONS_COUNT} versions that have been changed"
 echo "This repository should contain ${UNIQUE_VERSIONS_COUNT} changed versions"
 
 if [ $UNIQUE_VERSIONS_COUNT -ne "${CHANGED_VERSIONS_COUNT}" ]; then
-    echo "We expected $UNIQUE_VERSIONS_COUNT to have been updated to the latest version, but online ${CHANGED_VERSIONS_COUNT} have. This number is statically defined based on a simple grep"
+    echo "We expected $UNIQUE_VERSIONS_COUNT to have been updated to the latest version, but only ${CHANGED_VERSIONS_COUNT} have. This number is statically defined based on a simple grep"
     echo "Please confirm that all versions in all charts and requirements files have been bumped to the tagged release version. If you have successfully bumped all versions and there is still a mismatch in the expected and actual counts, then rerun the following command"
     echo "grep -R 'insert_your_semver_version_here' . | wc -l"
     echo "and update the script scripts/validate-helm-chart-versions.sh"
