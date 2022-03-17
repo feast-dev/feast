1996fd958830b23fb04ecd8f2352c361295b0688
Kevin Zhang <kzhang@tecton.ai>
2022-03-17 13:21:55 -0700

    revert feature store
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- go/cmd/server/main.go
+++ go/cmd/server/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
+	"time"
 
 	"github.com/feast-dev/feast/go/internal/feast"
 	"github.com/feast-dev/feast/go/internal/feast/registry"
@@ -13,6 +13,7 @@ import (
 
 	"github.com/feast-dev/feast/go/internal/feast"
 	"github.com/feast-dev/feast/go/protos/feast/serving"
+	"github.com/kelseyhightower/envconfig"
 	"google.golang.org/grpc"
 )
 
@@ -23,12 +24,32 @@ const (
 	feastServerVersion  = "0.18.0"
 )
 
+type FeastEnvConfig struct {
+	RepoPath   string `envconfig:"FEAST_REPO_PATH"`
+	RepoConfig string `envconfig:"FEAST_REPO_CONFIG"`
+	SockFile   string `envconfig:"FEAST_GRPC_SOCK_FILE"`
+}
+
+type MemoryBuffer struct {
+	logs []Log
+}
+
 // TODO: Add a proper logging library such as https://github.com/Sirupsen/logrus
 func main() {
-	repoPath := os.Getenv(flagFeastRepoPath)
-	repoConfigJSON := os.Getenv(flagFeastRepoConfig)
-	sockFile := os.Getenv(flagFeastSockFile)
-	if repoPath == "" && repoConfigJSON == "" {
+	// TODO(kevjumba) Figure out how large this log channel should be.
+	logChannel := make(chan Log, 1000)
+	defer close(logChannel)
+	var logBuffer = MemoryBuffer{
+		logs: make([]Log, 0),
+	}
+
+	var feastEnvConfig FeastEnvConfig
+	var err error
+	err = envconfig.Process("feast", &feastEnvConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if feastEnvConfig.RepoPath == "" && feastEnvConfig.RepoConfig == "" {
 		log.Fatalln(fmt.Sprintf("One of %s of %s environment variables must be set", flagFeastRepoPath, flagFeastRepoConfig))
 	}
 
@@ -52,11 +73,12 @@ func main() {
 		log.Fatalln(err)
 	}
 	defer fs.DestructOnlineStore()
-	startGrpcServer(fs, sockFile)
+	startGrpcServer(fs, logChannel, &logBuffer, feastEnvConfig.SockFile)
 }
 
-func startGrpcServer(fs *feast.FeatureStore, sockFile string) {
-	server := newServingServiceServer(fs)
+func startGrpcServer(fs *feast.FeatureStore, logChannel chan Log, logBuffer *MemoryBuffer, sockFile string) {
+	go processLogs(logChannel, logBuffer)
+	server := newServingServiceServer(fs, logChannel)
 	log.Printf("Starting a gRPC server listening on %s\n", sockFile)
 	lis, err := net.Listen("unix", sockFile)
 	if err != nil {
@@ -68,5 +90,21 @@ func startGrpcServer(fs *feast.FeatureStore, sockFile string) {
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		log.Fatalln(err)
+	}
+}
+
+func processLogs(log_channel chan Log, logBuffer *MemoryBuffer) {
+	// start a periodic flush
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+	for {
+		select {
+		case t := <-ticker.C:
+			log.Printf("time is %d", t)
+			log.Printf("Flushing buffer to offline storage with channel length: %d\n", len(logBuffer.logs))
+		case new_log := <-log_channel:
+			log.Printf("Pushing %s to memory.\n", new_log.featureValues)
+			logBuffer.logs = append(logBuffer.logs, new_log)
+		}
 	}
 }
--- go/cmd/server/server.go
+++ go/cmd/server/server.go
@@ -9,12 +9,28 @@ import (
 )
 
 type servingServiceServer struct {
-	fs *feast.FeatureStore
+	fs         *feast.FeatureStore
+	logChannel chan Log
 	serving.UnimplementedServingServiceServer
 }
 
-func newServingServiceServer(fs *feast.FeatureStore) *servingServiceServer {
-	return &servingServiceServer{fs: fs}
+type Log struct {
+	// Example: driver_id, customer_id
+	entityNames []string
+	// Example: val{int64_val: 5017}, val{int64_val: 1003}
+	entityValues []*prototypes.Value
+
+	// Feature names is 1:1 correspondence with featureValue, featureStatus, and timestamp
+	featureNames []string
+
+	featureValues   []*prototypes.Value
+	featureStatuses []serving.FieldStatus
+	eventTimestamps []*timestamp.Timestamp
+	RequestContext  map[string]*prototypes.RepeatedValue
+}
+
+func newServingServiceServer(fs *feast.FeatureStore, logChannel chan Log) *servingServiceServer {
+	return &servingServiceServer{fs: fs, logChannel: logChannel}
 }
 
 func (s *servingServiceServer) GetFeastServingInfo(ctx context.Context, request *serving.GetFeastServingInfoRequest) (*serving.GetFeastServingInfoResponse, error) {
@@ -66,4 +82,50 @@ func (s *servingServiceServer) GetOnlineFeatures(ctx context.Context, request *s
 	}
 
 	return resp, nil
+}
+
+func flushToChannel(s *servingServiceServer, onlineResponse *serving.GetOnlineFeaturesResponse, request *serving.GetOnlineFeaturesRequest) {
+	for _, featureVector := range onlineResponse.Results {
+		featureValues := make([]*prototypes.Value, len(featureVector.Values))
+		eventTimestamps := make([]*timestamp.Timestamp, len(featureVector.EventTimestamps))
+		for idx, featureValue := range featureVector.Values {
+			if featureVector.Statuses[idx] != serving.FieldStatus_PRESENT {
+				continue
+			}
+			featureValues[idx] = &prototypes.Value{Val: featureValue.Val}
+		}
+		for idx, ts := range featureVector.EventTimestamps {
+			if featureVector.Statuses[idx] != serving.FieldStatus_PRESENT {
+				continue
+			}
+			eventTimestamps[idx] = &timestamp.Timestamp{Seconds: ts.Seconds, Nanos: ts.Nanos}
+		}
+		// log.Println(request.Entities)
+		// for idx, featureName := range onlineResponse.Metadata.FeatureNames.Val {
+		// 	if _, ok := request.Entities[featureName]; ok {
+		// 		entityNames = append(entityNames, featureName)
+		// 		entityValues = append(entityValues, featureVector.Values[idx])
+		// 	} else {
+		// 		featureNames = append(featureNames, onlineResponse.Metadata.FeatureNames.Val[idx:]...)
+		// 		featureValues = append(featureValues, featureVector.Values[idx:]...)
+		// 		featureStatuses = append(featureStatuses, featureVector.Statuses[idx:]...)
+		// 		eventTimestamps = append(eventTimestamps, featureVector.EventTimestamps[idx:]...)
+		// 		break
+		// 	}
+		// }
+		// featureNames = append(featureNames, onlineResponse.Metadata.FeatureNames.Val[:]...)
+		// featureValues = append(featureValues, featureVector.Values[:]...)
+		// featureStatuses = append(featureStatuses, featureVector.Statuses[:]...)
+		// eventTimestamps = append(eventTimestamps, featureVector.EventTimestamps[:]...)
+
+		newLog := Log{
+			featureNames:    onlineResponse.Metadata.FeatureNames.Val,
+			featureValues:   featureValues,
+			featureStatuses: featureVector.Statuses,
+			eventTimestamps: eventTimestamps,
+			// TODO(kevjumba): figure out if this is required
+			RequestContext: request.RequestContext,
+		}
+		s.logChannel <- newLog
+	}
 }
