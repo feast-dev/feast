ad3816548b4ccb19f7c4972b86719dd08e1f6b11
Achal Shah <achals@gmail.com>
2022-03-22 10:49:54 -0700

    cr
    Signed-off-by: Achal Shah <achals@gmail.com>

--- sdk/python/tests/unit/test_data_sources.py
+++ sdk/python/tests/unit/test_data_sources.py
@@ -1,32 +1,30 @@
-import unittest
-
 from feast.data_source import PushSource
 from feast.infra.offline_stores.bigquery_source import BigQuerySource
 from feast.protos.feast.types.Value_pb2 import ValueType
 
 
-class DataSourceTestCases(unittest.TestCase):
-    def test_push_no_batch(self):
-        push_source = PushSource(
-            name="test", schema={"user_id": ValueType.INT64, "ltv": ValueType.INT64}
-        )
-        push_source_proto = push_source.to_proto()
-        self.assertIsNotNone(push_source_proto.push_options)
-        self.assertFalse(push_source_proto.push_options.HasField("batch_source"))
-        push_source_unproto = PushSource.from_proto(push_source_proto)
+def test_push_no_batch():
+    push_source = PushSource(
+        name="test", schema={"user_id": ValueType.INT64, "ltv": ValueType.INT64}
+    )
+    push_source_proto = push_source.to_proto()
+    assert push_source_proto.push_options is not None
+    assert not push_source_proto.push_options.HasField("batch_source")
+    push_source_unproto = PushSource.from_proto(push_source_proto)
 
-        self.assertEqual(push_source, push_source_unproto)
+    assert push_source == push_source_unproto
 
-    def test_push_with_batch(self):
-        push_source = PushSource(
-            name="test",
-            schema={"user_id": ValueType.INT64, "ltv": ValueType.INT64},
-            batch_source=BigQuerySource(table="test.test"),
-        )
-        push_source_proto = push_source.to_proto()
-        self.assertIsNotNone(push_source_proto.push_options)
-        self.assertTrue(push_source_proto.push_options.HasField("batch_source"))
 
-        push_source_unproto = PushSource.from_proto(push_source_proto)
+def test_push_with_batch():
+    push_source = PushSource(
+        name="test",
+        schema={"user_id": ValueType.INT64, "ltv": ValueType.INT64},
+        batch_source=BigQuerySource(table="test.test"),
+    )
+    push_source_proto = push_source.to_proto()
+    assert push_source_proto.push_options is not None
+    assert push_source_proto.push_options.HasField("batch_source")
 
-        self.assertEqual(push_source, push_source_unproto)
+    push_source_unproto = PushSource.from_proto(push_source_proto)
+
+    assert push_source == push_source_unproto
