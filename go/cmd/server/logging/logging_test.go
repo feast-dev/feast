31e0de84b43c148467ba4f53b0655908aa5b31a3
Kevin Zhang <kzhang@tecton.ai>
2022-03-31 14:48:39 -0700

    Fix
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- go/cmd/server/logging/logging_test.go
+++ go/cmd/server/logging/logging_test.go
@@ -9,30 +9,7 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// func TestLoggingChannelToMemoryBuffer(t *testing.T) {
-// 	//Feature store is still not checked in so we can't create one.
-// 	loggingService, err := NewLoggingService(nil, 10, false)
-// 	assert.Nil(t, err)
-// 	assert.Empty(t, loggingService.memoryBuffer.logs)
-// 	ts := timestamppb.New(time.Now())
-// 	newLog := Log{
-// 		FeatureNames:    []string{"feature1", "feature2"},
-// 		FeatureStatuses: []serving.FieldStatus{serving.FieldStatus_PRESENT},
-// 		EventTimestamps: []*timestamppb.Timestamp{ts},
-// 	}
-// 	err = loggingService.EmitLog(&newLog)
-// 	assert.Nil(t, err)
-// 	// Wait for memory buffer flush
-// 	time.Sleep(20 * time.Millisecond)
-// 	assert.Len(t, loggingService.memoryBuffer.logs, 1)
-// 	assert.Len(t, loggingService.logChannel, 0)
-// 	assert.True(t, reflect.DeepEqual(loggingService.memoryBuffer.logs[0].FeatureNames, []string{"feature1", "feature2"}))
-// 	assert.True(t, reflect.DeepEqual(loggingService.memoryBuffer.logs[0].FeatureStatuses, []serving.FieldStatus{serving.FieldStatus_PRESENT}))
-// 	assert.Nil(t, err)
-// }
-
 func TestLoggingChannelTimeout(t *testing.T) {
-	//Feature store is still not checked in so we can't create one.
 	loggingService, err := NewLoggingService(nil, 1, false)
 	assert.Nil(t, err)
 	assert.Empty(t, loggingService.memoryBuffer.logs)
@@ -53,6 +30,7 @@ func TestLoggingChannelTimeout(t *testing.T) {
 		EventTimestamps: []*timestamppb.Timestamp{newTs, newTs},
 	}
 	err = loggingService.EmitLog(&newLog2)
+	// The channel times out and doesn't hang.
 	time.Sleep(20 * time.Millisecond)
 	assert.NotNil(t, err)
 }
