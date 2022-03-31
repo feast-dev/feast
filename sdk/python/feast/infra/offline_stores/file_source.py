4f6ffefef85d84cbccdc502ee2da588437ccccc8
Kevin Zhang <kzhang@tecton.ai>
2022-03-31 11:18:13 -0700

    Keep file_url param
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/feast/infra/offline_stores/file_source.py
+++ sdk/python/feast/infra/offline_stores/file_source.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import Callable, Dict, Iterable, Optional, Tuple
 
 from pyarrow._fs import FileSystem
@@ -60,6 +61,7 @@ class FileSource(DataSource):
             )
         self.file_options = FileOptions(
             file_format=file_format,
+            file_url=path,
             uri=path,
             s3_endpoint_override=s3_endpoint_override,
         )
@@ -179,6 +181,7 @@ class FileOptions:
     def __init__(
         self,
         file_format: Optional[FileFormat],
+        file_url: Optional[str],
         s3_endpoint_override: Optional[str],
         uri: Optional[str],
     ):
@@ -187,11 +190,21 @@ class FileOptions:
 
         Args:
             file_format (FileFormat, optional): file source format eg. parquet
+            file_url (str, optional): [DEPRECATED] file source url eg. s3:// or local file
             s3_endpoint_override (str, optional): custom s3 endpoint (used only with s3 uri)
             uri (str, optional): file source url eg. s3:// or local file
 
         """
         self._file_format = file_format
+        if file_url:
+            warnings.warn(
+                (
+                    "The option to pass a file_url parameter to FileOptions is being deprecated. "
+                    "Please pass the file url to the uri parameter instead. The parameter will be deprecated in Feast 0.23"
+                ),
+                DeprecationWarning,
+            )
+        self._file_url = file_url
         self._uri = uri
         self._s3_endpoint_override = s3_endpoint_override
 
@@ -208,6 +221,20 @@ class FileOptions:
         Sets the file format of this file
         """
         self._file_format = file_format
+
+    @property
+    def file_url(self):
+        """
+        Returns the file url of this file
+        """
+        return self._file_url
+
+    @file_url.setter
+    def file_url(self, file_url):
+        """
+        Sets the file url of this file
+        """
+        self._file_url = file_url
 
     @property
     def uri(self):
