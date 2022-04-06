aefda92b41a849a1a0b29d1791c10098639fbb05
Kevin Zhang <kzhang@tecton.ai>
2022-04-06 15:55:16 -0700

    Update structs to make fields public
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- go/internal/feast/basefeatureview.go
+++ go/internal/feast/basefeatureview.go
@@ -7,37 +7,37 @@ import (
 )
 
 type BaseFeatureView struct {
-	name       string
-	features   []*Feature
-	projection *FeatureViewProjection
+	Name       string
+	Features   []*Feature
+	Projection *FeatureViewProjection
 }
 
 func NewBaseFeatureView(name string, featureProtos []*core.FeatureSpecV2) *BaseFeatureView {
-	base := &BaseFeatureView{name: name}
+	base := &BaseFeatureView{Name: name}
 	features := make([]*Feature, len(featureProtos))
 	for index, featureSpecV2 := range featureProtos {
 		features[index] = NewFeatureFromProto(featureSpecV2)
 	}
-	base.features = features
-	base.projection = NewFeatureViewProjectionFromDefinition(base)
+	base.Features = features
+	base.Projection = NewFeatureViewProjectionFromDefinition(base)
 	return base
 }
 
 func (fv *BaseFeatureView) withProjection(projection *FeatureViewProjection) (*BaseFeatureView, error) {
-	if projection.name != fv.name {
+	if projection.Name != fv.Name {
 		return nil, fmt.Errorf("the projection for the %s FeatureView cannot be applied because it differs "+
 			"in name; the projection is named %s and the name indicates which "+
-			"FeatureView the projection is for", fv.name, projection.name)
+			"FeatureView the projection is for", fv.Name, projection.Name)
 	}
 	features := make(map[string]bool)
-	for _, feature := range fv.features {
-		features[feature.name] = true
+	for _, feature := range fv.Features {
+		features[feature.Name] = true
 	}
 	for _, feature := range projection.Features {
-		if _, ok := features[feature.name]; !ok {
+		if _, ok := features[feature.Name]; !ok {
 			return nil, fmt.Errorf("the projection for %s cannot be applied because it contains %s which the "+
-				"FeatureView doesn't have", projection.name, feature.name)
+				"FeatureView doesn't have", projection.Name, feature.Name)
 		}
 	}
-	return &BaseFeatureView{name: fv.name, features: fv.features, projection: projection}, nil
+	return &BaseFeatureView{Name: fv.Name, Features: fv.Features, Projection: projection}, nil
 }
