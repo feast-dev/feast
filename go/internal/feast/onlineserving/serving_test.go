c233a24d4bf7817ad905be510f88ad0f694210dc
pyalex <moskalenko.alexey@gmail.com>
2022-04-07 16:07:47 -0700

    test unpacking request
    Signed-off-by: pyalex <moskalenko.alexey@gmail.com>

--- go/internal/feast/onlineserving/serving_test.go
+++ go/internal/feast/onlineserving/serving_test.go
@@ -2,8 +2,11 @@ package onlineserving
 
 import (
 	"github.com/feast-dev/feast/go/internal/feast/model"
+	"github.com/feast-dev/feast/go/protos/feast/core"
 	"github.com/feast-dev/feast/go/protos/feast/types"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"testing"
 )
 
@@ -177,4 +180,153 @@ func TestGroupingFeatureRefsWithMissingKey(t *testing.T) {
 		true,
 	)
 	assert.Errorf(t, err, "key destination_id is missing in provided entity rows")
+}
+
+func createFeature(name string, valueType types.ValueType_Enum) *core.FeatureSpecV2 {
+	return &core.FeatureSpecV2{
+		Name:      name,
+		ValueType: valueType,
+	}
+}
+
+func createFeatureView(name string, entities []string, features ...*core.FeatureSpecV2) *model.FeatureView {
+	viewProto := core.FeatureView{
+		Spec: &core.FeatureViewSpec{
+			Name:     name,
+			Entities: entities,
+			Features: features,
+			Ttl:      &durationpb.Duration{},
+		},
+	}
+	return model.NewFeatureViewFromProto(&viewProto)
+}
+
+func createFeatureService(viewProjections map[string][]*core.FeatureSpecV2) *model.FeatureService {
+	projections := make([]*core.FeatureViewProjection, 0)
+	for name, features := range viewProjections {
+		projections = append(projections, &core.FeatureViewProjection{
+			FeatureViewName: name,
+			FeatureColumns:  features,
+			JoinKeyMap:      map[string]string{},
+		})
+	}
+
+	fsProto := core.FeatureService{
+		Spec: &core.FeatureServiceSpec{
+			Features: projections,
+		},
+		Meta: &core.FeatureServiceMeta{
+			LastUpdatedTimestamp: timestamppb.Now(),
+			CreatedTimestamp:     timestamppb.Now(),
+		},
+	}
+
+	return model.NewFeatureServiceFromProto(&fsProto)
+}
+
+func createOnDemandFeatureView(name string, featureSources map[string][]*core.FeatureSpecV2, features ...*core.FeatureSpecV2) *model.OnDemandFeatureView {
+	sources := make(map[string]*core.OnDemandSource)
+	for viewName, features := range featureSources {
+		sources[viewName] = &core.OnDemandSource{
+			Source: &core.OnDemandSource_FeatureViewProjection{
+				FeatureViewProjection: &core.FeatureViewProjection{
+					FeatureViewName: viewName,
+					FeatureColumns:  features,
+					JoinKeyMap:      map[string]string{},
+				},
+			},
+		}
+	}
+
+	proto := &core.OnDemandFeatureView{
+		Spec: &core.OnDemandFeatureViewSpec{
+			Name:     name,
+			Sources:  sources,
+			Features: features,
+		},
+	}
+	return model.NewOnDemandFeatureViewFromProto(proto)
+}
+
+func TestUnpackFeatureService(t *testing.T) {
+	featASpec := createFeature("featA", types.ValueType_INT32)
+	featBSpec := createFeature("featB", types.ValueType_INT32)
+	featCSpec := createFeature("featC", types.ValueType_INT32)
+	featDSpec := createFeature("featD", types.ValueType_INT32)
+	featESpec := createFeature("featE", types.ValueType_FLOAT)
+	onDemandFeature1 := createFeature("featF", types.ValueType_FLOAT)
+	onDemandFeature2 := createFeature("featG", types.ValueType_FLOAT)
+
+	viewA := createFeatureView("viewA", []string{"entity"}, featASpec, featBSpec)
+	viewB := createFeatureView("viewB", []string{"entity"}, featCSpec, featDSpec)
+	viewC := createFeatureView("viewC", []string{"entity"}, featESpec)
+	onDemandView := createOnDemandFeatureView(
+		"odfv",
+		map[string][]*core.FeatureSpecV2{"viewB": {featCSpec}, "viewC": {featESpec}},
+		onDemandFeature1, onDemandFeature2)
+
+	fs := createFeatureService(map[string][]*core.FeatureSpecV2{
+		"viewA": {featASpec, featBSpec},
+		"viewB": {featCSpec},
+		"odfv":  {onDemandFeature2},
+	})
+
+	fvs, odfvs, err := GetFeatureViewsToUseByService(
+		fs,
+		map[string]*model.FeatureView{"viewA": viewA, "viewB": viewB, "viewC": viewC},
+		map[string]*model.OnDemandFeatureView{"odfv": onDemandView})
+
+	assertCorrectUnpacking(t, fvs, odfvs, err)
+}
+
+func assertCorrectUnpacking(t *testing.T, fvs []*FeatureViewAndRefs, odfvs []*model.OnDemandFeatureView, err error) {
+	assert.Nil(t, err)
+	assert.Len(t, fvs, 3)
+	assert.Len(t, odfvs, 1)
+
+	fvsByName := make(map[string]*FeatureViewAndRefs)
+	for _, fv := range fvs {
+		fvsByName[fv.View.Base.Name] = fv
+	}
+
+	// feature views and features as declared in service
+	assert.Equal(t, []string{"featA", "featB"}, fvsByName["viewA"].FeatureRefs)
+	assert.Equal(t, []string{"featC"}, fvsByName["viewB"].FeatureRefs)
+
+	// dependency of the on demand feature view
+	assert.Equal(t, []string{"featE"}, fvsByName["viewC"].FeatureRefs)
+
+	// only requested features projected
+	assert.Len(t, odfvs[0].Base.Projection.Features, 1)
+	assert.Equal(t, "featG", odfvs[0].Base.Projection.Features[0].Name)
+}
+
+func TestUnpackFeatureViewsByReferences(t *testing.T) {
+	featASpec := createFeature("featA", types.ValueType_INT32)
+	featBSpec := createFeature("featB", types.ValueType_INT32)
+	featCSpec := createFeature("featC", types.ValueType_INT32)
+	featDSpec := createFeature("featD", types.ValueType_INT32)
+	featESpec := createFeature("featE", types.ValueType_FLOAT)
+	onDemandFeature1 := createFeature("featF", types.ValueType_FLOAT)
+	onDemandFeature2 := createFeature("featG", types.ValueType_FLOAT)
+
+	viewA := createFeatureView("viewA", []string{"entity"}, featASpec, featBSpec)
+	viewB := createFeatureView("viewB", []string{"entity"}, featCSpec, featDSpec)
+	viewC := createFeatureView("viewC", []string{"entity"}, featESpec)
+	onDemandView := createOnDemandFeatureView(
+		"odfv",
+		map[string][]*core.FeatureSpecV2{"viewB": {featCSpec}, "viewC": {featESpec}},
+		onDemandFeature1, onDemandFeature2)
+
+	fvs, odfvs, err := GetFeatureViewsToUseByFeatureRefs(
+		[]string{
+			"viewA:featA",
+			"viewA:featB",
+			"viewB:featC",
+			"odfv:featG",
+		},
+		map[string]*model.FeatureView{"viewA": viewA, "viewB": viewB, "viewC": viewC},
+		map[string]*model.OnDemandFeatureView{"odfv": onDemandView})
+
+	assertCorrectUnpacking(t, fvs, odfvs, err)
 }
