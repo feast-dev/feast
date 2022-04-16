63ab5757063023b1391f88083a4a318d96d57d85
Achal Shah <achals@gmail.com>
2022-04-15 21:48:08 -0700

    feat: Create stream and batch feature view abstractions
    Signed-off-by: Achal Shah <achals@gmail.com>

---
+++ sdk/python/feast/batch_feature_view.py
@@ -0,0 +1,61 @@
+from datetime import timedelta
+from typing import Dict, List, Optional, Union
+
+from google.protobuf.duration_pb2 import Duration
+
+from feast import Entity, Feature, Field
+from feast.data_source import DataSource
+from feast.feature_view import FeatureView
+from feast.protos.feast.core.DataSource_pb2 import DataSource as DataSourceProto
+
+SUPPORTED_BATCH_SOURCES = {
+    "BigQuerySource",
+    "FileSource",
+    "RedshiftSource",
+    "SnowflakeSource",
+    "SparkSource",
+    "TrinoSource",
+}
+
+
+class BatchFeatureView(FeatureView):
+    def __init__(
+        self,
+        *,
+        name: Optional[str] = None,
+        entities: Optional[Union[List[Entity], List[str]]] = None,
+        ttl: Optional[Union[Duration, timedelta]] = None,
+        features: Optional[List[Feature]] = None,
+        tags: Optional[Dict[str, str]] = None,
+        online: bool = True,
+        description: str = "",
+        owner: str = "",
+        schema: Optional[List[Field]] = None,
+        source: Optional[DataSource] = None,
+    ):
+
+        if source is None:
+            raise ValueError("Feature views need a source specified")
+        if (
+            type(source).__name__ not in SUPPORTED_BATCH_SOURCES
+            and source.to_proto().type != DataSourceProto.SourceType.CUSTOM_SOURCE
+        ):
+            raise ValueError(
+                f"Batch feature views need a batch source, expected one of {SUPPORTED_BATCH_SOURCES} "
+                f"or CUSTOM_SOURCE, got {type(source).__name__}: {source.name} instead "
+            )
+
+        super().__init__(
+            name=name,
+            entities=entities,
+            ttl=ttl,
+            batch_source=None,
+            stream_source=None,
+            features=features,
+            tags=tags,
+            online=online,
+            description=description,
+            owner=owner,
+            schema=schema,
+            source=source,
+        )
