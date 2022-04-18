5b37bb6658e2e8aeabb25dbf7aa0978bf61eb672
Danny Chiao <danny@tecton.ai>
2022-04-18 00:06:32 -0400

    fix generator
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- sdk/python/tests/integration/e2e/test_python_feature_server.py
+++ sdk/python/tests/integration/e2e/test_python_feature_server.py
@@ -1,3 +1,4 @@
+import contextlib
 import json
 from typing import List
 
@@ -24,58 +25,60 @@ from tests.integration.feature_repos.universal.entities import (
 @pytest.mark.integration
 @pytest.mark.universal
 def test_get_online_features():
-    client = setup_python_fs_client()
-    request_data_dict = {
-        "features": [
-            "driver_stats:conv_rate",
-            "driver_stats:acc_rate",
-            "driver_stats:avg_daily_trips",
-        ],
-        "entities": {"driver_id": [5001, 5002]},
-    }
-    response = client.post("/get-online-features", data=json.dumps(request_data_dict))
-    parsed_response = json.loads(response.text)
-    assert "metadata" in parsed_response
-    metadata = parsed_response["metadata"]
-    expected_features = ["driver_id", "conv_rate", "acc_rate", "avg_daily_trips"]
-    response_feature_names = metadata["feature_names"]
-    assert len(response_feature_names) == len(expected_features)
-    for expected_feature in expected_features:
-        assert expected_feature in response_feature_names
-    assert "results" in parsed_response
-    results = parsed_response["results"]
-    for result in results:
-        # Same order as in metadata
-        assert len(result["statuses"]) == 2  # Requested two entities
-        for status in result["statuses"]:
-            assert status == "PRESENT"
+    with setup_python_fs_client() as client:
+        request_data_dict = {
+            "features": [
+                "driver_stats:conv_rate",
+                "driver_stats:acc_rate",
+                "driver_stats:avg_daily_trips",
+            ],
+            "entities": {"driver_id": [5001, 5002]},
+        }
+        response = client.post(
+            "/get-online-features", data=json.dumps(request_data_dict)
+        )
+        parsed_response = json.loads(response.text)
+        assert "metadata" in parsed_response
+        metadata = parsed_response["metadata"]
+        expected_features = ["driver_id", "conv_rate", "acc_rate", "avg_daily_trips"]
+        response_feature_names = metadata["feature_names"]
+        assert len(response_feature_names) == len(expected_features)
+        for expected_feature in expected_features:
+            assert expected_feature in response_feature_names
+        assert "results" in parsed_response
+        results = parsed_response["results"]
+        for result in results:
+            # Same order as in metadata
+            assert len(result["statuses"]) == 2  # Requested two entities
+            for status in result["statuses"]:
+                assert status == "PRESENT"
 
-    results_driver_id_index = response_feature_names.index("driver_id")
-    assert (
-        results[results_driver_id_index]["values"]
-        == request_data_dict["entities"]["driver_id"]
-    )
+        results_driver_id_index = response_feature_names.index("driver_id")
+        assert (
+            results[results_driver_id_index]["values"]
+            == request_data_dict["entities"]["driver_id"]
+        )
 
 
 @pytest.mark.integration
 @pytest.mark.universal
 def test_push():
-    client = setup_python_fs_client()
-    initial_temp = get_temperatures(client, location_ids=[1])[0]
-    json_data = json.dumps(
-        {
-            "push_source_name": "location_stats_push_source",
-            "df": {
-                "location_id": [1],
-                "temperature": [initial_temp * 100],
-                "event_timestamp": ["2022-05-13 10:59:42"],
-                "created": ["2022-05-13 10:59:42"],
-            },
-        }
-    )
-    response = client.post("/push", data=json_data,)
-    assert response.status_code == 200
-    assert get_temperatures(client, location_ids=[1]) == [initial_temp * 100]
+    with setup_python_fs_client() as client:
+        initial_temp = get_temperatures(client, location_ids=[1])[0]
+        json_data = json.dumps(
+            {
+                "push_source_name": "location_stats_push_source",
+                "df": {
+                    "location_id": [1],
+                    "temperature": [initial_temp * 100],
+                    "event_timestamp": ["2022-05-13 10:59:42"],
+                    "created": ["2022-05-13 10:59:42"],
+                },
+            }
+        )
+        response = client.post("/push", data=json_data,)
+        assert response.status_code == 200
+        assert get_temperatures(client, location_ids=[1]) == [initial_temp * 100]
 
 
 def get_temperatures(client, location_ids: List[int]):
@@ -94,16 +97,20 @@ def get_temperatures(client, location_ids: List[int]):
     return results[results_temperature_index]["values"]
 
 
+@contextlib.contextmanager
 def setup_python_fs_client():
     config = IntegrationTestRepoConfig()
     environment = construct_test_environment(config)
     fs = environment.feature_store
-    entities, datasets, data_sources = construct_universal_test_data(environment)
-    feature_views = construct_universal_feature_views(data_sources)
-    feast_objects: List[FeastObject] = []
-    feast_objects.extend(feature_views.values())
-    feast_objects.extend([driver(), customer(), location()])
-    fs.apply(feast_objects)
-    fs.materialize(environment.start_date, environment.end_date)
-    client = TestClient(get_app(fs))
-    return client
+    try:
+        entities, datasets, data_sources = construct_universal_test_data(environment)
+        feature_views = construct_universal_feature_views(data_sources)
+        feast_objects: List[FeastObject] = []
+        feast_objects.extend(feature_views.values())
+        feast_objects.extend([driver(), customer(), location()])
+        fs.apply(feast_objects)
+        fs.materialize(environment.start_date, environment.end_date)
+        client = TestClient(get_app(fs))
+        yield client
+    finally:
+        fs.teardown()
