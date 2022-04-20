4dce254dc8c4f7de0c6005907ceba53b44f264ce
Kevin Zhang <kzhang@tecton.ai>
2022-04-19 18:13:38 -0700

    fix: Enforce kw args featureservice (#2575)
    * Fix
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix lint
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix lint
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Add tests
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix lint
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix lint
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/tests/unit/test_feature_service.py
+++ sdk/python/tests/unit/test_feature_service.py
@@ -1,3 +1,5 @@
+import pytest
+
 from feast.feature_service import FeatureService
 from feast.feature_view import FeatureView
 from feast.field import Field
@@ -55,3 +57,49 @@ def test_hash():
 
     s4 = {feature_service_1, feature_service_2, feature_service_3, feature_service_4}
     assert len(s4) == 3
+
+
+def test_feature_view_kw_args_warning():
+    with pytest.warns(DeprecationWarning):
+        service = FeatureService("name", [], tags={"tag_1": "tag"}, description="desc")
+        assert service.name == "name"
+        assert service.tags == {"tag_1": "tag"}
+        assert service.description == "desc"
+
+    # More positional args than name and features
+    with pytest.raises(ValueError):
+        service = FeatureService("name", [], {"tag_1": "tag"}, "desc")
+
+    # No name defined.
+    with pytest.raises(ValueError):
+        service = FeatureService(features=[], tags={"tag_1": "tag"}, description="desc")
+
+
+def no_warnings(func):
+    def wrapper_no_warnings(*args, **kwargs):
+        with pytest.warns(None) as warnings:
+            func(*args, **kwargs)
+
+        if len(warnings) > 0:
+            raise AssertionError(
+                "Warnings were raised: " + ", ".join([str(w) for w in warnings])
+            )
+
+    return wrapper_no_warnings
+
+
+@no_warnings
+def test_feature_view_kw_args_normal():
+    file_source = FileSource(name="my-file-source", path="test.parquet")
+    feature_view = FeatureView(
+        name="my-feature-view",
+        entities=[],
+        schema=[
+            Field(name="feature1", dtype=Float32),
+            Field(name="feature2", dtype=Float32),
+        ],
+        source=file_source,
+    )
+    _ = FeatureService(
+        name="my-feature-service", features=[feature_view[["feature1", "feature2"]]]
+    )
