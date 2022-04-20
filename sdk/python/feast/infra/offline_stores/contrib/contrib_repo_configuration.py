d0b50812827cf38ee9798a591ea77d8782ba070d
Kevin Zhang <kzhang@tecton.ai>
2022-04-19 19:03:42 -0700

    Fix repo_config for rest of sources
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/feast/infra/offline_stores/contrib/contrib_repo_configuration.py
+++ sdk/python/feast/infra/offline_stores/contrib/contrib_repo_configuration.py
@@ -4,7 +4,11 @@ from tests.integration.feature_repos.integration_test_repo_config import (
 from tests.integration.feature_repos.universal.data_sources.spark_data_source_creator import (
     SparkDataSourceCreator,
 )
+from tests.integration.feature_repos.universal.data_sources.trino import (
+    TrinoSourceCreator,
+)
 
 FULL_REPO_CONFIGS = [
     IntegrationTestRepoConfig(offline_store_creator=SparkDataSourceCreator),
+    IntegrationTestRepoConfig(offline_store_creator=TrinoSourceCreator),
 ]
