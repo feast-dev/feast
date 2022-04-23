a2fd043940349246fbfebe1b4f7ebd891d380235
Felix Wang <wangfelix98@gmail.com>
2022-04-22 17:14:58 -0700

    Fix Redshift proto conversion and add test
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>

--- sdk/python/tests/unit/test_data_sources.py
+++ sdk/python/tests/unit/test_data_sources.py
@@ -12,6 +12,7 @@ from feast.data_source import (
 from feast.field import Field
 from feast.infra.offline_stores.bigquery_source import BigQuerySource
 from feast.infra.offline_stores.file_source import FileSource
+from feast.infra.offline_stores.redshift_source import RedshiftSource
 from feast.infra.offline_stores.snowflake_source import SnowflakeSource
 from feast.types import Bool, Float32, Int64
 
@@ -159,6 +160,18 @@ def test_proto_conversion():
         owner="test@gmail.com",
     )
 
+    redshift_source = RedshiftSource(
+        name="test_source",
+        database="test_database",
+        schema="test_schema",
+        table="test_table",
+        timestamp_field="event_timestamp",
+        created_timestamp_column="created_timestamp",
+        field_mapping={"foo": "bar"},
+        description="test description",
+        owner="test@gmail.com",
+    )
+
     snowflake_source = SnowflakeSource(
         name="test_source",
         database="test_database",
@@ -173,4 +186,5 @@ def test_proto_conversion():
     )
 
     assert FileSource.from_proto(file_source.to_proto()) == file_source
+    assert RedshiftSource.from_proto(redshift_source.to_proto()) == redshift_source
     assert SnowflakeSource.from_proto(snowflake_source.to_proto()) == snowflake_source
