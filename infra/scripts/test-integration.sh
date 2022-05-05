1758be39354dfd6362d78e06d76adaa35c42a85d
Achal Shah <achals@gmail.com>
2022-05-05 11:23:24 -0700

    chore: Remove unused scripts and docker-compose env (#2642)
    Signed-off-by: Achal Shah <achals@gmail.com>

--- infra/scripts/test-integration.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-python -m pip install --upgrade pip setuptools wheel pip-tools
-make install-python
-python -m pip install -qr tests/requirements.txt
-
-export FEAST_USAGE="False"
-pytest tests/integration --dataproc-cluster-name feast-e2e --dataproc-project kf-feast --dataproc-region us-central1  --dataproc-staging-location gs://feast-templocation-kf-feast
