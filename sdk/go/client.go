0a86692e4d8ad2728e24226cc6ae29128c42da77
KeshavSharma <keshav.sharma@gojek.com>
2022-05-11 11:18:38 +0530

    new protobuf for online features response
    Signed-off-by: KeshavSharma <keshav.sharma@gojek.com>

--- sdk/go/client.go
+++ sdk/go/client.go
@@ -110,9 +110,9 @@ func (fc *GrpcClient) GetOnlineFeatures(ctx context.Context, req *OnlineFeatures
 	if err != nil {
 		return nil, err
 	}
-	resp, err := fc.cli.GetOnlineFeaturesV2(ctx, featuresRequest)
+	resp, err := fc.cli.GetOnlineFeatures(ctx, featuresRequest)
 
-	// collect unqiue entity refs from entity rows
+	// collect unique entity refs from entity rows
 	entityRefs := make(map[string]struct{})
 	for _, entityRows := range req.Entities {
 		for ref := range entityRows {
