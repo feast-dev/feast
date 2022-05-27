f69b3817e66ce81ca82963a2b86bf3a4d73f5388
Achal Shah <achals@gmail.com>
2022-05-27 16:33:43 -0700

    chore: Respect environment variables when calling check_call (#2741)
    Signed-off-by: Achal Shah <achals@gmail.com>

--- setup.py
+++ setup.py
@@ -248,6 +248,7 @@ class BuildPythonProtosCommand(Command):
                 self.python_folder,
             ]
             + proto_files,
+            env=os.environ
         )
 
     def run(self):
@@ -333,6 +334,8 @@ class BuildGoProtosCommand(Command):
         proto_files = glob.glob(os.path.join(self.proto_folder, path))
 
         try:
+            e = os.environ.copy()
+            e["PATH"] = self.path_val
             subprocess.check_call(
                 self.go_protoc
                 + [
@@ -346,7 +349,7 @@ class BuildGoProtosCommand(Command):
                     "--go-grpc_opt=module=github.com/feast-dev/feast/go/protos",
                 ]
                 + proto_files,
-                env={"PATH": self.path_val},
+                env=e,
             )
         except CalledProcessError as e:
             print(f"Stderr: {e.stderr}")
