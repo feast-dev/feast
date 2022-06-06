97b0a0bd692b1d84e4827c9f6c38f073aae4ebd6
Kevin Zhang <kzhang@tecton.ai>
2022-06-06 14:43:08 -0700

    Add sliding window to aggregations
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/feast/aggregation.py
+++ sdk/python/feast/aggregation.py
@@ -14,21 +14,28 @@ class Aggregation:
         column: str  # Column name of the feature we are aggregating.
         function: str  # Provided built in aggregations sum, max, min, count mean
         time_window: timedelta  # The time window for this aggregation.
+        sliding_window: timedelta # The sliding window for these aggregations
     """
 
     column: str
     function: str
     time_window: Optional[timedelta]
+    sliding_window: timedelta
 
     def __init__(
         self,
         column: Optional[str] = "",
         function: Optional[str] = "",
         time_window: Optional[timedelta] = None,
+        sliding_window: Optional[timedelta] = None,
     ):
         self.column = column or ""
         self.function = function or ""
         self.time_window = time_window
+        if not self.sliding_window:
+            self.sliding_window = self.time_window
+        else:
+            self.sliding_window = self.time_window
 
     def to_proto(self) -> AggregationProto:
         window_duration = None
@@ -36,8 +43,13 @@ class Aggregation:
             window_duration = Duration()
             window_duration.FromTimedelta(self.time_window)
 
+        sliding_window_duration = None
+        if self.sliding_window is not None:
+            sliding_window_duration = Duration()
+            sliding_window_duration.FromTimedelta(self.sliding_window)
+
         return AggregationProto(
-            column=self.column, function=self.function, time_window=window_duration
+            column=self.column, function=self.function, time_window=window_duration, sliding_window=sliding_window_duration,
         )
 
     @classmethod
@@ -48,10 +60,17 @@ class Aggregation:
             else agg_proto.time_window.ToTimedelta()
         )
 
+        sliding_window = (
+            timedelta(days=0)
+            if agg_proto.sliding_window.ToNanoseconds() == 0
+            else agg_proto.sliding_window.ToTimedelta()
+        )
         aggregation = cls(
             column=agg_proto.column,
             function=agg_proto.function,
             time_window=time_window,
+            sliding_window=sliding_window,
+
         )
         return aggregation
 
@@ -63,6 +82,7 @@ class Aggregation:
             self.column != other.column
             or self.function != other.function
             or self.time_window != other.time_window
+            or self.sliding_window != other.sliding_window
         ):
             return False
 
