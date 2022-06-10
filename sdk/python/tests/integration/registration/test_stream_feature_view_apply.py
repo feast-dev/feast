331a214b04dc8b3f9347e79819130fa7bdf9e4c3
Kevin Zhang <kzhang@tecton.ai>
2022-06-10 16:37:41 -0700

    fix: Update udf tests and add base functions to streaming fcos and fix some nonetype errors (#2776)
    * Fix lint and add comments
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix lint
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/tests/integration/registration/test_stream_feature_view_apply.py
+++ sdk/python/tests/integration/registration/test_stream_feature_view_apply.py
@@ -70,3 +70,71 @@ def test_apply_stream_feature_view(environment) -> None:
     assert features["test_key"] == [1001]
     assert "dummy_field" in features
     assert features["dummy_field"] == [None]
+
+
+@pytest.mark.integration
+def test_stream_feature_view_udf(environment) -> None:
+    """
+    Test apply of StreamFeatureView udfs are serialized correctly and usable.
+    """
+    fs = environment.feature_store
+
+    # Create Feature Views
+    entity = Entity(name="driver_entity", join_keys=["test_key"])
+
+    stream_source = KafkaSource(
+        name="kafka",
+        timestamp_field="event_timestamp",
+        bootstrap_servers="",
+        message_format=AvroFormat(""),
+        topic="topic",
+        batch_source=FileSource(path="test_path", timestamp_field="event_timestamp"),
+        watermark=timedelta(days=1),
+    )
+
+    @stream_feature_view(
+        entities=[entity],
+        ttl=timedelta(days=30),
+        owner="test@example.com",
+        online=True,
+        schema=[Field(name="dummy_field", dtype=Float32)],
+        description="desc",
+        aggregations=[
+            Aggregation(
+                column="dummy_field", function="max", time_window=timedelta(days=1),
+            ),
+            Aggregation(
+                column="dummy_field2", function="count", time_window=timedelta(days=24),
+            ),
+        ],
+        timestamp_field="event_timestamp",
+        mode="spark",
+        source=stream_source,
+        tags={},
+    )
+    def pandas_view(pandas_df):
+        import pandas as pd
+
+        assert type(pandas_df) == pd.DataFrame
+        df = pandas_df.transform(lambda x: x + 10, axis=1)
+        df.insert(2, "C", [20.2, 230.0, 34.0], True)
+        return df
+
+    import pandas as pd
+
+    df = pd.DataFrame({"A": [1, 2, 3], "B": [10, 20, 30]})
+
+    fs.apply([entity, pandas_view])
+    stream_feature_views = fs.list_stream_feature_views()
+    assert len(stream_feature_views) == 1
+    assert stream_feature_views[0].name == "pandas_view"
+    assert stream_feature_views[0] == pandas_view
+
+    sfv = stream_feature_views[0]
+
+    new_df = sfv.udf(df)
+
+    expected_df = pd.DataFrame(
+        {"A": [11, 12, 13], "B": [20, 30, 40], "C": [20.2, 230.0, 34.0]}
+    )
+    assert new_df.equals(expected_df)
