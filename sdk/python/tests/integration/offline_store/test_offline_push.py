42133bdb53a4b60689bdc769f9d229697170c896
Kevin Zhang <kzhang@tecton.ai>
2022-06-16 15:38:26 -0700

    Fix
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/tests/integration/offline_store/test_offline_push.py
+++ sdk/python/tests/integration/offline_store/test_offline_push.py
@@ -5,8 +5,7 @@ from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 import pytest
-import tempfile
-import uuid
+import random
 
 from feast.data_format import ParquetFormat
 
@@ -74,8 +73,66 @@ def test_writing_incorrect_order_fails(environment, universal_data_sources):
                 ts-timedelta(hours=3),
                 ts,
             ],
-            "conv_rate": [0.1, 0.2],
-            "avg_daily_trips": [1, 2],
+            "conv_rate": [random.random(), random.random()],
+            "avg_daily_trips": [random.randint(0, 10), random.randint(0, 10)],
+            "created": [ts, ts]
+        },
+    )
+    with pytest.raises(ValueError):
+        store.write_to_offline_store(driver_stats.name, expected_df, allow_registry_cache=False)
+
+
+@pytest.mark.integration
+@pytest.mark.universal_online_stores(only=["sqlite"])
+def test_writing_incorrect_schema_fails(environment, universal_data_sources):
+    # TODO(kevjumba) handle incorrect order later, for now schema must be in the order that the filesource is in
+    store = environment.feature_store
+    _, _, data_sources = universal_data_sources
+    driver_stats = FeatureView(
+        name="driver_stats",
+        entities=["driver"],
+        schema=[
+            Field(name="avg_daily_trips", dtype=Int32),
+            Field(name="conv_rate", dtype=Float32),
+        ],
+        source=data_sources.driver,
+    )
+
+    now = datetime.utcnow()
+    ts = pd.Timestamp(now).round("ms")
+
+    entity_df = pd.DataFrame.from_dict(
+        {
+            "driver_id": [1001, 1002],
+            "event_timestamp": [
+                ts-timedelta(hours=3),
+                ts,
+            ],
+        }
+    )
+
+    store.apply([driver(), driver_stats])
+    df = store.get_historical_features(
+        entity_df=entity_df,
+        features=[
+            "driver_stats:conv_rate",
+            "driver_stats:avg_daily_trips"
+        ],
+        full_feature_names=False,
+    ).to_df()
+
+    assert df["conv_rate"].isnull().all()
+    assert df["avg_daily_trips"].isnull().all()
+
+    expected_df = pd.DataFrame.from_dict(
+        {
+            "event_timestamp": [
+                ts-timedelta(hours=3),
+                ts,
+            ],
+            "driver_id": [1001, 1002],
+            "conv_rate": [random.random(), random.random()],
+            "incorrect_schema": [random.randint(0, 10), random.randint(0, 10)],
             "created": [ts, ts]
         },
     )
@@ -103,7 +160,7 @@ def test_writing_consecutively_to_offline_store(environment, universal_data_sour
 
     entity_df = pd.DataFrame.from_dict(
         {
-            "driver_id": [1001, 1002],
+            "driver_id": [1001, 1001],
             "event_timestamp": [
                 ts-timedelta(hours=4),
                 ts-timedelta(hours=3),
@@ -131,9 +188,9 @@ def test_writing_consecutively_to_offline_store(environment, universal_data_sour
                 ts-timedelta(hours=3),
             ],
             "driver_id": [1001, 1001],
-            "conv_rate": [0.1, 0.2],
-            "acc_rate": [0.5, 0.6],
-            "avg_daily_trips": [1, 2],
+            "conv_rate": [random.random(), random.random()],
+            "acc_rate": [random.random(), random.random()],
+            "avg_daily_trips": [random.randint(0, 10), random.randint(0, 10)],
             "created": [ts, ts]
         },
     )
@@ -159,9 +216,9 @@ def test_writing_consecutively_to_offline_store(environment, universal_data_sour
                 ts,
             ],
             "driver_id": [1001, 1001],
-            "conv_rate": [0.3, 0.4],
-            "acc_rate": [0.8, 0.9],
-            "avg_daily_trips": [3, 4],
+            "conv_rate": [random.random(), random.random()],
+            "acc_rate": [random.random(), random.random()],
+            "avg_daily_trips": [random.randint(0, 10), random.randint(0, 10)],
             "created": [ts, ts]
         },
     )
