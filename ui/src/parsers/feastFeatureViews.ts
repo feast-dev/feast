c084f9cd4661f91ec57314fab2b7ffa1a2e5f41e
Daniel Kim <danielk@twitter.com>
2022-06-17 16:16:20 -0700

    Add metadata tab functionality
    Signed-off-by: Daniel Kim <danielk@twitter.com>

--- ui/src/parsers/feastFeatureViews.ts
+++ ui/src/parsers/feastFeatureViews.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 import { FEAST_FEATURE_VALUE_TYPES } from "./types";
+import { jsonSchema } from "./jsonType"
+
 
 const FeastFeatureColumnSchema = z.object({
   name: z.string(),
@@ -54,6 +56,7 @@ const FeastFeatureViewSchema = z.object({
         })
       )
       .optional(),
+    metadata: jsonSchema.optional(),
   }),
 });
 
