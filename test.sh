b7b2c11c14bfc2c8b718007e7fc78fafc180235d
Kevin Zhang <kzhang@tecton.ai>
2022-06-28 18:37:00 -0700

    Fix workflow
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
-REGEX='^Feast SDK Version: "feast [0-9]+\.[0-9]+\.[0-9]"$'
-OUT=$(feast version)
-echo $OUT
-if echo $OUT | grep -E "$REGEX" &>/dev/null ; then
-  echo "gay"
-else 
-  echo "not gay"
+VERSION_REGEX='[0-9]+\.[0-9]+\.[0-9]+'
+OUTPUT_REGEX='^Feast SDK Version: "$VERSION_REGEX"$'
+VERSION_OUTPUT=$(feast version)
+VERSION=$(echo $VERSION_OUTPUT | grep -oE "$VERSION_REGEX")
+VERSION_WITHOUT_PREFIX='0.19.3'
+OUTPUT=$(echo $VERSION_OUTPUT | grep -E "$REGEX")
+if  [ -n "$OUTPUT" ] &&[ "$VERSION" = "$VERSION_WITHOUT_PREFIX" ]; then  
+  echo "Correct Feast Version Installed"
+else
+  echo "$VERSION_OUTPUT is not in the correct format"
+  exit 1
 fi
-
-
-echo 'Feast SDK Version: "feast 0.21.3"' | grep -e "$REGEX"
