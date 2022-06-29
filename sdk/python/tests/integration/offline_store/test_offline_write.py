838363a774fe96dd70acbb8710b374a6734074ce
Felix Wang <wangfelix98@gmail.com>
2022-06-28 17:58:24 -0700

    Clean up `test_offline_write.py`
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>

--- sdk/python/tests/integration/offline_store/test_offline_write.py
+++ sdk/python/tests/integration/offline_store/test_offline_write.py
@@ -9,69 +9,25 @@ from feast import FeatureView, Field
 from feast.types import Float32, Int32
 from tests.integration.feature_repos.universal.entities import driver
 
-
-@pytest.mark.integration
-@pytest.mark.universal_offline_stores(only=["file", "redshift"])
-@pytest.mark.universal_online_stores(only=["sqlite"])
-def test_writing_columns_in_incorrect_order_fails(environment, universal_data_sources):
-    # TODO(kevjumba) handle incorrect order later, for now schema must be in the order that the filesource is in
-    store = environment.feature_store
-    _, _, data_sources = universal_data_sources
-    driver_stats = FeatureView(
-        name="driver_stats",
-        entities=["driver"],
-        schema=[
-            Field(name="avg_daily_trips", dtype=Int32),
-            Field(name="conv_rate", dtype=Float32),
-        ],
-        source=data_sources.driver,
-    )
-
-    now = datetime.utcnow()
-    ts = pd.Timestamp(now).round("ms")
-
-    entity_df = pd.DataFrame.from_dict(
-        {"driver_id": [1001, 1002], "event_timestamp": [ts - timedelta(hours=3), ts]}
-    )
-
-    store.apply([driver(), driver_stats])
-    df = store.get_historical_features(
-        entity_df=entity_df,
-        features=["driver_stats:conv_rate", "driver_stats:avg_daily_trips"],
-        full_feature_names=False,
-    ).to_df()
-
-    assert df["conv_rate"].isnull().all()
-    assert df["avg_daily_trips"].isnull().all()
-
-    expected_df = pd.DataFrame.from_dict(
-        {
-            "driver_id": [1001, 1002],
-            "event_timestamp": [ts - timedelta(hours=3), ts],
-            "conv_rate": [random.random(), random.random()],
-            "avg_daily_trips": [random.randint(0, 10), random.randint(0, 10)],
-            "created": [ts, ts],
-        },
-    )
-    with pytest.raises(ValueError):
-        store._write_to_offline_store(
-            driver_stats.name, expected_df, allow_registry_cache=False
-        )
+# TODO(felixwang9817): Add a unit test that checks that write_to_offline_store can reorder columns.
+# This should only happen after https://github.com/feast-dev/feast/issues/2797 is fixed.
 
 
 @pytest.mark.integration
-@pytest.mark.universal_offline_stores(only=["file", "redshift"])
+@pytest.mark.universal_offline_stores
 @pytest.mark.universal_online_stores(only=["sqlite"])
 def test_writing_incorrect_schema_fails(environment, universal_data_sources):
-    # TODO(kevjumba) handle incorrect order later, for now schema must be in the order that the filesource is in
+    """Tests that writing a dataframe with an incorrect schema fails."""
     store = environment.feature_store
     _, _, data_sources = universal_data_sources
+    driver_entity = driver()
     driver_stats = FeatureView(
         name="driver_stats",
-        entities=["driver"],
+        entities=[driver_entity],
         schema=[
             Field(name="avg_daily_trips", dtype=Int32),
             Field(name="conv_rate", dtype=Float32),
+            Field(name="acc_rate", dtype=Float32),
         ],
         source=data_sources.driver,
     )
@@ -83,14 +39,19 @@ def test_writing_incorrect_schema_fails(environment, universal_data_sources):
         {"driver_id": [1001, 1002], "event_timestamp": [ts - timedelta(hours=3), ts]}
     )
 
-    store.apply([driver(), driver_stats])
+    store.apply([driver_entity, driver_stats])
     df = store.get_historical_features(
         entity_df=entity_df,
-        features=["driver_stats:conv_rate", "driver_stats:avg_daily_trips"],
+        features=[
+            "driver_stats:conv_rate",
+            "driver_stats:acc_rate",
+            "driver_stats:avg_daily_trips",
+        ],
         full_feature_names=False,
     ).to_df()
 
     assert df["conv_rate"].isnull().all()
+    assert df["acc_rate"].isnull().all()
     assert df["avg_daily_trips"].isnull().all()
 
     expected_df = pd.DataFrame.from_dict(
@@ -103,7 +64,7 @@ def test_writing_incorrect_schema_fails(environment, universal_data_sources):
         },
     )
     with pytest.raises(ValueError):
-        store._write_to_offline_store(
+        store.write_to_offline_store(
             driver_stats.name, expected_df, allow_registry_cache=False
         )
 
@@ -114,9 +75,10 @@ def test_writing_incorrect_schema_fails(environment, universal_data_sources):
 def test_writing_consecutively_to_offline_store(environment, universal_data_sources):
     store = environment.feature_store
     _, _, data_sources = universal_data_sources
+    driver_entity = driver()
     driver_stats = FeatureView(
         name="driver_stats",
-        entities=["driver"],
+        entities=[driver_entity],
         schema=[
             Field(name="avg_daily_trips", dtype=Int32),
             Field(name="conv_rate", dtype=Float32),
@@ -138,14 +100,19 @@ def test_writing_consecutively_to_offline_store(environment, universal_data_sour
         }
     )
 
-    store.apply([driver(), driver_stats])
+    store.apply([driver_entity, driver_stats])
     df = store.get_historical_features(
         entity_df=entity_df,
-        features=["driver_stats:conv_rate", "driver_stats:avg_daily_trips"],
+        features=[
+            "driver_stats:conv_rate",
+            "driver_stats:acc_rate",
+            "driver_stats:avg_daily_trips",
+        ],
         full_feature_names=False,
     ).to_df()
 
     assert df["conv_rate"].isnull().all()
+    assert df["acc_rate"].isnull().all()
     assert df["avg_daily_trips"].isnull().all()
 
     first_df = pd.DataFrame.from_dict(
@@ -158,13 +125,17 @@ def test_writing_consecutively_to_offline_store(environment, universal_data_sour
             "created": [ts, ts],
         },
     )
-    store._write_to_offline_store(
+    store.write_to_offline_store(
         driver_stats.name, first_df, allow_registry_cache=False
     )
 
     after_write_df = store.get_historical_features(
         entity_df=entity_df,
-        features=["driver_stats:conv_rate", "driver_stats:avg_daily_trips"],
+        features=[
+            "driver_stats:conv_rate",
+            "driver_stats:acc_rate",
+            "driver_stats:avg_daily_trips",
+        ],
         full_feature_names=False,
     ).to_df()
 
@@ -172,6 +143,10 @@ def test_writing_consecutively_to_offline_store(environment, universal_data_sour
     assert np.where(
         after_write_df["conv_rate"].reset_index(drop=True)
         == first_df["conv_rate"].reset_index(drop=True)
+    )
+    assert np.where(
+        after_write_df["acc_rate"].reset_index(drop=True)
+        == first_df["acc_rate"].reset_index(drop=True)
     )
     assert np.where(
         after_write_df["avg_daily_trips"].reset_index(drop=True)
@@ -189,7 +164,7 @@ def test_writing_consecutively_to_offline_store(environment, universal_data_sour
         },
     )
 
-    store._write_to_offline_store(
+    store.write_to_offline_store(
         driver_stats.name, second_df, allow_registry_cache=False
     )
 
