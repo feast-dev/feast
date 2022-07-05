f78922e31a3ad78286993c7bbeca17eec23137ec
Felix Wang <wangfelix98@gmail.com>
2022-07-05 10:05:10 -0700

    chore: Change pytest fixtures to be function-scoped instead of session-scoped (#2899)
    * Test to trigger registry conflicts
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Switch environment and associated fixtures to being function scoped
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Format
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Switch type tests to function-scoped fixtures
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Format
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>

---
+++ sdk/python/tests/unit/test_registry_conflict.py
@@ -0,0 +1,23 @@
+import pytest
+
+from feast.entity import Entity
+
+
+@pytest.mark.integration
+def test_apply_first_entity(environment):
+    entity = Entity(name="first")
+    fs = environment.feature_store
+    fs.apply([entity])
+
+    entities = fs.list_entities()
+    assert len(entities) == 1
+
+
+@pytest.mark.integration
+def test_apply_second_entity(environment):
+    entity = Entity(name="second")
+    fs = environment.feature_store
+    fs.apply([entity])
+
+    entities = fs.list_entities()
+    assert len(entities) == 1
