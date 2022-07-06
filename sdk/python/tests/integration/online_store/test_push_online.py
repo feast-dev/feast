c75ae414cd4ebdb914f4587fcbecaad6e9706128
Felix Wang <wangfelix98@gmail.com>
2022-07-05 23:38:25 -0700

    Clean up push source online retrieval test
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>

--- sdk/python/tests/integration/online_store/test_push_online.py
+++ sdk/python/tests/integration/online_store/test_push_online.py
@@ -6,29 +6,20 @@ import pytest
 from tests.integration.feature_repos.repo_configuration import (
     construct_universal_feature_views,
 )
-from tests.integration.feature_repos.universal.entities import (
-    customer,
-    driver,
-    location,
-)
+from tests.integration.feature_repos.universal.entities import location
 
 
 @pytest.mark.integration
 @pytest.mark.universal_online_stores
 def test_push_features_and_read(environment, universal_data_sources):
     store = environment.feature_store
-
-    (_, datasets, data_sources) = universal_data_sources
+    _, _, data_sources = universal_data_sources
     feature_views = construct_universal_feature_views(data_sources)
+    location_fv = feature_views.pushed_locations
+    store.apply([location(), location_fv])
 
-    store.apply([driver(), customer(), location(), *feature_views.values()])
-
-    # TODO(felixwang9817): Note that we choose an entity value of 101 here since it is not included
-    # in the existing range of entity values (1-49). This allows us to push data for this test
-    # without affecting other tests. This decision is tech debt, and should be resolved by finding a
-    # better way to isolate data sources across tests.
     data = {
-        "location_id": [101],
+        "location_id": [1],
         "temperature": [4],
         "event_timestamp": [pd.Timestamp(datetime.datetime.utcnow()).round("ms")],
         "created": [pd.Timestamp(datetime.datetime.utcnow()).round("ms")],
@@ -39,8 +30,8 @@ def test_push_features_and_read(environment, universal_data_sources):
 
     online_resp = store.get_online_features(
         features=["pushable_location_stats:temperature"],
-        entity_rows=[{"location_id": 101}],
+        entity_rows=[{"location_id": 1}],
     )
     online_resp_dict = online_resp.to_dict()
-    assert online_resp_dict["location_id"] == [101]
+    assert online_resp_dict["location_id"] == [1]
     assert online_resp_dict["temperature"] == [4]
