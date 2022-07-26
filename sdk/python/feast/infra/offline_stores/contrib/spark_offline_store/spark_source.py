3ce51391e0b2ebdec68c81d93b54f5d06bb427a6
kfiring <kfiring@163.com>
2022-07-27 03:05:15 +0800

    fix: Spark source support table with pattern "db.table" (#2606)
    * 1. fix: spark source support table with pattern "db.table" (#2605 https://github.com/feast-dev/feast/issues/2605)
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix lint
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    Co-authored-by: leonpeng <kfiring>
    Co-authored-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/feast/infra/offline_stores/contrib/spark_offline_store/spark_source.py
+++ sdk/python/feast/infra/offline_stores/contrib/spark_offline_store/spark_source.py
@@ -177,7 +177,8 @@ class SparkSource(DataSource):
         """Returns a string that can directly be used to reference this table in SQL"""
         if self.table:
             # Backticks make sure that spark sql knows this a table reference.
-            return f"`{self.table}`"
+            table = ".".join([f"`{x}`" for x in self.table.split(".")])
+            return table
         if self.query:
             return f"({self.query})"
 
