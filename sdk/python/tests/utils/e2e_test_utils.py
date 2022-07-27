8fe04e57d138ddf9a563e8b5f70668bee21753ef
Kevin Zhang <kzhang@tecton.ai>
2022-07-27 15:15:02 -0700

    Address review
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/tests/utils/e2e_test_utils.py
+++ sdk/python/tests/utils/e2e_test_utils.py
@@ -33,7 +33,70 @@ from tests.integration.feature_repos.universal.data_sources.redshift import (
 )
 
 
-def check_offline_and_online_features(
+def validate_offline_online_store_consistency(
+    fs: FeatureStore, fv: FeatureView, split_dt: datetime
+) -> None:
+    now = datetime.utcnow()
+
+    full_feature_names = True
+    check_offline_store: bool = True
+
+    # Run materialize()
+    # use both tz-naive & tz-aware timestamps to test that they're both correctly handled
+    start_date = (now - timedelta(hours=5)).replace(tzinfo=utc)
+    end_date = split_dt
+    fs.materialize(feature_views=[fv.name], start_date=start_date, end_date=end_date)
+
+    time.sleep(10)
+
+    # check result of materialize()
+    _check_offline_and_online_features(
+        fs=fs,
+        fv=fv,
+        driver_id=1,
+        event_timestamp=end_date,
+        expected_value=0.3,
+        full_feature_names=full_feature_names,
+        check_offline_store=check_offline_store,
+    )
+
+    _check_offline_and_online_features(
+        fs=fs,
+        fv=fv,
+        driver_id=2,
+        event_timestamp=end_date,
+        expected_value=None,
+        full_feature_names=full_feature_names,
+        check_offline_store=check_offline_store,
+    )
+
+    # check prior value for materialize_incremental()
+    _check_offline_and_online_features(
+        fs=fs,
+        fv=fv,
+        driver_id=3,
+        event_timestamp=end_date,
+        expected_value=4,
+        full_feature_names=full_feature_names,
+        check_offline_store=check_offline_store,
+    )
+
+    # run materialize_incremental()
+    fs.materialize_incremental(feature_views=[fv.name], end_date=now)
+
+    # check result of materialize_incremental()
+    _check_offline_and_online_features(
+        fs=fs,
+        fv=fv,
+        driver_id=3,
+        event_timestamp=now,
+        expected_value=5,
+        full_feature_names=full_feature_names,
+        check_offline_store=check_offline_store,
+    )
+
+
+def _check_offline_and_online_features(
     fs: FeatureStore,
     fv: FeatureView,
     driver_id: int,
@@ -99,69 +162,6 @@ def check_offline_and_online_features(
                 assert not df.to_dict(orient="list")["value"] or math.isnan(
                     df.to_dict(orient="list")["value"][0]
                 )
-
-
-def validate_offline_online_store_consistency(
-    fs: FeatureStore, fv: FeatureView, split_dt: datetime
-) -> None:
-    now = datetime.utcnow()
-
-    full_feature_names = True
-    check_offline_store: bool = True
-
-    # Run materialize()
-    # use both tz-naive & tz-aware timestamps to test that they're both correctly handled
-    start_date = (now - timedelta(hours=5)).replace(tzinfo=utc)
-    end_date = split_dt
-    fs.materialize(feature_views=[fv.name], start_date=start_date, end_date=end_date)
-
-    time.sleep(10)
-
-    # check result of materialize()
-    check_offline_and_online_features(
-        fs=fs,
-        fv=fv,
-        driver_id=1,
-        event_timestamp=end_date,
-        expected_value=0.3,
-        full_feature_names=full_feature_names,
-        check_offline_store=check_offline_store,
-    )
-
-    check_offline_and_online_features(
-        fs=fs,
-        fv=fv,
-        driver_id=2,
-        event_timestamp=end_date,
-        expected_value=None,
-        full_feature_names=full_feature_names,
-        check_offline_store=check_offline_store,
-    )
-
-    # check prior value for materialize_incremental()
-    check_offline_and_online_features(
-        fs=fs,
-        fv=fv,
-        driver_id=3,
-        event_timestamp=end_date,
-        expected_value=4,
-        full_feature_names=full_feature_names,
-        check_offline_store=check_offline_store,
-    )
-
-    # run materialize_incremental()
-    fs.materialize_incremental(feature_views=[fv.name], end_date=now)
-
-    # check result of materialize_incremental()
-    check_offline_and_online_features(
-        fs=fs,
-        fv=fv,
-        driver_id=3,
-        event_timestamp=now,
-        expected_value=5,
-        full_feature_names=full_feature_names,
-        check_offline_store=check_offline_store,
-    )
 
 
 def make_feature_store_yaml(project, test_repo_config, repo_dir_name: Path):
