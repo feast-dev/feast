375d65d6de7381d48f251a726dbf909a11ada4f5
Kevin Zhang <kzhang@tecton.ai>
2022-07-28 11:32:23 -0700

    Fix
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/tests/integration/e2e/test_go_feature_server.py
+++ sdk/python/tests/integration/e2e/test_go_feature_server.py
@@ -33,7 +33,7 @@ from tests.integration.feature_repos.universal.entities import (
     location,
 )
 from tests.utils.feature_test_utils import generate_expected_logs, get_latest_rows
-from tests.utils.http_utils import check_port_open, free_port
+from tests.utils.http_server_test_utils import check_port_open, free_port
 
 
 @pytest.mark.integration
