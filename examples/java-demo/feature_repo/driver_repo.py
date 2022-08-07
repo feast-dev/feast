f340aeb020e1e6c51d8d4a8978924f51c585306c
Danny Chiao <danny@tecton.ai>
2022-08-07 12:05:06 -0500

    fix: Fix Feast Java inconsistency with int64 serialization vs python (#3031)
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- examples/java-demo/feature_repo/driver_repo.py
+++ examples/java-demo/feature_repo/driver_repo.py
@@ -1,13 +1,11 @@
 import pandas as pd
 from feast.data_source import RequestSource
-from feast.field import Field
 from feast.on_demand_feature_view import on_demand_feature_view
-from feast.request_feature_view import RequestFeatureView
 from feast.types import Float32, Float64, Int64, String
 from google.protobuf.duration_pb2 import Duration
 from feast.field import Field
 
-from feast import Entity, Feature, BatchFeatureView, FileSource
+from feast import Entity, FileSource, FeatureView
 
 driver_hourly_stats = FileSource(
     path="data/driver_stats_with_string.parquet",
@@ -15,10 +13,10 @@ driver_hourly_stats = FileSource(
     created_timestamp_column="created",
 )
 driver = Entity(name="driver_id", description="driver id",)
-driver_hourly_stats_view = BatchFeatureView(
+driver_hourly_stats_view = FeatureView(
     name="driver_hourly_stats",
-    entities=["driver_id"],
-    ttl=Duration(seconds=86400000),
+    entities=[driver],
+    ttl=timedelta(days=365),
     schema=[
         Field(name="conv_rate", dtype=Float32),
         Field(name="acc_rate", dtype=Float32),
@@ -40,6 +38,7 @@ input_request = RequestSource(
     ],
 )
 
+
 # Define an on demand feature view which can generate new features based on
 # existing feature views and RequestSource features
 @on_demand_feature_view(
@@ -58,14 +57,3 @@ def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
     df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
     return df
 
-
-# Define request feature view
-driver_age_request_fv = RequestFeatureView(
-    name="driver_age",
-    request_data_source=RequestSource(
-        name="driver_age",
-        schema=[
-            Field(name="driver_age", dtype=Int64),
-        ],
-    ),
-)
