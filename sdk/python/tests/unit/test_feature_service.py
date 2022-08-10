e169729b89e4b8b73ead6c6efb218163f670bad3
Felix Wang <wangfelix98@gmail.com>
2022-08-10 13:13:59 -0700

    chore!: Deprecate positional arguments for feature services (#3051)
    * Clean up feature service
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Delete unnecessary feature service test
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Fix docstring
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>

--- sdk/python/tests/unit/test_feature_service.py
+++ sdk/python/tests/unit/test_feature_service.py
@@ -1,5 +1,3 @@
-import pytest
-
 from feast.feature_service import FeatureService
 from feast.feature_view import FeatureView
 from feast.field import Field
@@ -57,22 +55,6 @@ def test_hash():
 
     s4 = {feature_service_1, feature_service_2, feature_service_3, feature_service_4}
     assert len(s4) == 3
-
-
-def test_feature_view_kw_args_warning():
-    with pytest.warns(DeprecationWarning):
-        service = FeatureService("name", [], tags={"tag_1": "tag"}, description="desc")
-        assert service.name == "name"
-        assert service.tags == {"tag_1": "tag"}
-        assert service.description == "desc"
-
-    # More positional args than name and features
-    with pytest.raises(ValueError):
-        service = FeatureService("name", [], {"tag_1": "tag"}, "desc")
-
-    # No name defined.
-    with pytest.raises(ValueError):
-        service = FeatureService(features=[], tags={"tag_1": "tag"}, description="desc")
 
 
 @no_warnings
