5de70c15343275549271da2b498b8688a4f3cfd1
hao-affirm <104030690+hao-affirm@users.noreply.github.com>
2022-08-12 15:33:22 -0700

    update dependency

--- sdk/python/feast/driver_test_data.py
+++ sdk/python/feast/driver_test_data.py
@@ -103,7 +103,7 @@ def create_driver_hourly_stats_df(drivers, start_date, end_date) -> pd.DataFrame
             "event_timestamp": [
                 pd.Timestamp(dt, unit="ms", tz="UTC").round("ms")
                 for dt in pd.date_range(
-                    start=start_date, end=end_date, freq="1H", inclusive="left"
+                    start=start_date, end=end_date, freq="1H", closed="left"
                 )
             ]
             # include a fixed timestamp for get_historical_features in the quickstart
@@ -256,7 +256,7 @@ def create_global_daily_stats_df(start_date, end_date) -> pd.DataFrame:
             "event_timestamp": [
                 pd.Timestamp(dt, unit="ms", tz="UTC").round("ms")
                 for dt in pd.date_range(
-                    start=start_date, end=end_date, freq="1D", inclusive="left"
+                    start=start_date, end=end_date, freq="1D", closed="left"
                 )
             ]
         }
