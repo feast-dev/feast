4d858d6464665bc01528b86943bc4b9213f2aac2
Danny Chiao <danny@tecton.ai>
2022-08-15 10:35:29 -0400

    add tqdm
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- infra/scripts/cleanup_dynamo_ci.py
+++ infra/scripts/cleanup_dynamo_ci.py
@@ -1,15 +1,21 @@
 import boto3
+from tqdm import tqdm
 
 
 def main() -> None:
     db = boto3.resource("dynamodb")
 
-    num_deleted = 0
-    for table in db.tables.all():
+    num_to_delete = 0
+    all_tables = db.tables.all()
+    for table in all_tables:
         if "integration_test" in table.name:
-            table.delete()
-            num_deleted += 1
-    print(f"Deleted {num_deleted} CI DynamoDB tables")
+            num_to_delete += 1
+    with tqdm(total=num_to_delete) as progress:
+        for table in all_tables:
+            if "integration_test" in table.name:
+                table.delete()
+                progress.update()
+    print(f"Deleted {num_to_delete} CI DynamoDB tables")
 
 
 if __name__ == "__main__":
