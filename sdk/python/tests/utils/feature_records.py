7a4d055f19d3710050a75f705ee5c789d7d6fa76
Kevin Zhang <kzhang@tecton.ai>
2022-08-18 19:53:46 -0700

    Fix lint
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- sdk/python/tests/utils/feature_records.py
+++ sdk/python/tests/utils/feature_records.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 from typing import Any, Dict, List, Optional
 
+import numpy as np
 import pandas as pd
 import pytest
-import numpy as np
 from pandas.testing import assert_frame_equal as pd_assert_frame_equal
 from pytz import utc
 
