c77a655c03499443e253e4cb9c7d7651685c7de3
Kevin Zhang <kzhang@tecton.ai>
2022-08-25 13:56:08 -0700

    fix: Fix release workflow (#3144)
    * Fix
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    * Fix helm charts
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>
    
    Signed-off-by: Kevin Zhang <kzhang@tecton.ai>

--- infra/scripts/helm/validate-helm-chart-versions.sh
+++ infra/scripts/helm/validate-helm-chart-versions.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Amount of file locations that need to be bumped in unison when versions increment
-UNIQUE_VERSIONS_COUNT=20
+UNIQUE_VERSIONS_COUNT=21 # Change in release 0.24.0
 
 if [ $# -ne 1 ]; then
     echo "Please provide a single semver version (without a \"v\" prefix) to test the repository against, e.g 0.99.0"
