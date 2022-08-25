67e93901c15fbd4eed28ef4ce883c5877598ec58
derek1032 <dchang@health2sync.com>
2022-08-25 11:07:34 +0000

    fix: FeastModuleImportError
    Signed-off-by: derek1032 <dchang@health2sync.com>

--- sdk/python/feast/infra/offline_stores/contrib/athena_repo_configuration.py
+++ sdk/python/feast/infra/offline_stores/contrib/athena_repo_configuration.py
@@ -1,7 +1,7 @@
 from tests.integration.feature_repos.integration_test_repo_config import (
     IntegrationTestRepoConfig,
 )
-from tests.integration.feature_repos.universal.data_sources.athena import (
+from feast.infra.offline_stores.contrib.athena_offline_store.tests.data_source import (
     AthenaDataSourceCreator,
 )
 
