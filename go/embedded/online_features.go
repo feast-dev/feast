36ee4ad5ce562c06d1b708ffea37445026124531
Achal Shah <achals@gmail.com>
2022-08-29 14:00:20 -0700

    ci: Fix java feature server Dockerfile to use updated builder
    Signed-off-by: Achal Shah <achals@gmail.com>

--- go/embedded/online_features.go
+++ go/embedded/online_features.go
@@ -378,12 +378,12 @@ func (s *OnlineFeatureService) StopGrpcServer() {
 }
 
 /*
-	Read Record Batch from memory managed by Python caller.
-	Python part uses C ABI interface to export this record into C Data Interface,
-	and then it provides pointers (dataPtr & schemaPtr) to the Go part.
-	Here we import this data from given pointers and wrap the underlying values
-	into Go Arrow Interface (array.Record).
-	See export code here https://github.com/feast-dev/feast/blob/master/sdk/python/feast/embedded_go/online_features_service.py
+Read Record Batch from memory managed by Python caller.
+Python part uses C ABI interface to export this record into C Data Interface,
+and then it provides pointers (dataPtr & schemaPtr) to the Go part.
+Here we import this data from given pointers and wrap the underlying values
+into Go Arrow Interface (array.Record).
+See export code here https://github.com/feast-dev/feast/blob/master/sdk/python/feast/embedded_go/online_features_service.py
 */
 func readArrowRecord(data DataTable) (arrow.Record, error) {
 	return cdata.ImportCRecordBatch(
