2624879cbc845930a6c46fbac444d4584af74c8d
sfc-gh-madkins <82121043+sfc-gh-madkins@users.noreply.github.com>
2022-09-21 22:35:25 -0500

    chore: Update snowflake dependencies (#3238)
    chore: Update snowflake python dependencies
    
    Signed-off-by: miles.adkins <miles.adkins@snowflake.com>
    
    Signed-off-by: miles.adkins <miles.adkins@snowflake.com>

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ AWS_REQUIRED = ["boto3>=1.17.0,<=1.20.23", "docker>=5.0.2", "s3fs>=0.4.0,<=2022.
 BYTEWAX_REQUIRED = ["bytewax==0.10.0", "docker>=5.0.2", "kubernetes<=20.13.0"]
 
 SNOWFLAKE_REQUIRED = [
-    "snowflake-connector-python[pandas]>=2.7.3,<=2.7.8",
+    "snowflake-connector-python[pandas]>=2.7.3,<3",
 ]
 
 SPARK_REQUIRED = [
