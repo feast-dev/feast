eacb815644b83a077b8eaa060a6990d051de6a21
Danny Chiao <danny@tecton.ai>
2022-12-14 21:28:11 -0500

    fix new file lint
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- sdk/python/feast/infra/registry/proto_registry_utils.py
+++ sdk/python/feast/infra/registry/proto_registry_utils.py
@@ -1,22 +1,8 @@
-import logging
-import uuid
-from datetime import datetime, timedelta
-from enum import Enum
-from pathlib import Path
-from threading import Lock
-from typing import Any, Dict, List, Optional
-from urllib.parse import urlparse
+from typing import List
 
-from google.protobuf.internal.containers import RepeatedCompositeFieldContainer
-from proto import Message
-
-from feast import usage
-from feast.base_feature_view import BaseFeatureView
 from feast.data_source import DataSource
 from feast.entity import Entity
 from feast.errors import (
-    ConflictingFeatureViewNames,
-    DataSourceNotFoundException,
     DataSourceObjectNotFoundException,
     EntityNotFoundException,
     FeatureServiceNotFoundException,
@@ -27,17 +13,9 @@ from feast.errors import (
 )
 from feast.feature_service import FeatureService
 from feast.feature_view import FeatureView
-from feast.importer import import_class
-from feast.infra.infra_object import Infra
-from feast.infra.registry import proto_registry_utils
-from feast.infra.registry.base_registry import BaseRegistry
-from feast.infra.registry.registry_store import NoopRegistryStore
 from feast.on_demand_feature_view import OnDemandFeatureView
 from feast.project_metadata import ProjectMetadata
-from feast.protos.feast.core.Registry_pb2 import ProjectMetadata as ProjectMetadataProto
 from feast.protos.feast.core.Registry_pb2 import Registry as RegistryProto
-from feast.repo_config import RegistryConfig
-from feast.repo_contents import RepoContents
 from feast.request_feature_view import RequestFeatureView
 from feast.saved_dataset import SavedDataset, ValidationReference
 from feast.stream_feature_view import StreamFeatureView
