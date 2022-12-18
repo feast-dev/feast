977dae17388cae387a707df6abda6fd2fe624c7c
Hai Nguyen <quanghai.ng1512@gmail.com>
2022-12-18 10:51:51 +0700

    chore: Minor fix imports regarding stream feature view
    Signed-off-by: Hai Nguyen <quanghai.ng1512@gmail.com>

--- sdk/python/feast/core/feature_views/__init__.py
+++ sdk/python/feast/core/feature_views/__init__.py
@@ -2,4 +2,4 @@ from .batch_fv import BatchFeatureView
 from .generic_fv import FeatureView
 from .on_demand_fv import OnDemandFeatureView, on_demand_feature_view
 from .request_fv import RequestFeatureView
-from .stream_fv import StreamFeatureView
+from .stream_fv import StreamFeatureView, stream_feature_view
