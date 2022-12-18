9834bf89ba2c9c61ad020102c3ea4c4d81d8c5d6
Hai Nguyen <quanghai.ng1512@gmail.com>
2022-12-18 10:35:08 +0700

    chore: Move implementation to `core` modules
    Signed-off-by: Hai Nguyen <quanghai.ng1512@gmail.com>

---
+++ sdk/python/feast/core/feature_views/__init__.py
@@ -0,0 +1,5 @@
+from .batch_fv import BatchFeatureView
+from .generic_fv import FeatureView
+from .on_demand_fv import OnDemandFeatureView, on_demand_feature_view
+from .request_fv import RequestFeatureView
+from .stream_fv import StreamFeatureView
