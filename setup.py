127a2e83af45d98b4254ec1a513b879b60cc6af7
franciscojavierarceo <francisco.arceo@affirm.com>
2022-12-29 13:06:44 -0700

    adding 0.25-affirm10 release

--- setup.py
+++ setup.py
@@ -40,11 +40,11 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.25+affirm2"
+VERSION = "0.25+affirm10"
 DESCRIPTION = "Python SDK for Feast"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
-REQUIRES_PYTHON = ">=3.7.0"
+REQUIRES_PYTHON = "===3.7.13"
 
 REQUIRED = [
     "click>=7.0.0,<9.0.0",
