1c90284bf2b9d9286517612653e012d051afc988
franciscojavierarceo <francisco.arceo@affirm.com>
2023-01-09 12:20:29 -0700

    publishing version 15

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.25+affirm14"
+VERSION = "0.25+affirm15"
 DESCRIPTION = "Python SDK for Feast"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
