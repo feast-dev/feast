e68e0bce052284b913e576af45c96e75b8ac2920
hao-affirm <104030690+hao-affirm@users.noreply.github.com>
2023-01-15 16:51:46 -0800

    add init
    Signed-off-by: hao-affirm <104030690+hao-affirm@users.noreply.github.com>

---
+++ sdk/python/feast/infra/materialization/contrib/spark/__init__.py
@@ -0,0 +1,11 @@
+from .spark_materialization_engine import (
+    SparkMaterializationEngine,
+    SparkMaterializationEngineConfig,
+    SparkMaterializationJob,
+)
+
+__all__ = [
+    "SparkMaterializationJob",
+    "SparkMaterializationEngine",
+    "SparkMaterializationEngineConfig",
+]
