fd7215e684be78b5988fa2dc2ba537147e6db260
hao-affirm <104030690+hao-affirm@users.noreply.github.com>
2023-01-26 11:55:58 -0800

    bump version
    Signed-off-by: hao-affirm <104030690+hao-affirm@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.28+affirm12"
+VERSION = "0.28+affirm13"
 DESCRIPTION = "Python SDK for Feast"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
