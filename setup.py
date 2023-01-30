c50ac6b30d7ae8c31d88b1c4cb3c8c414ad12afa
Danny Chiao <danny@tecton.ai>
2023-01-30 14:28:37 -0500

    pin mypy
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ CI_REQUIRED = (
         "minio==7.1.0",
         "mock==2.0.0",
         "moto<4",
-        "mypy>=0.931",
+        "mypy~=0.981",
         "mypy-protobuf==3.1",
         "avro==1.10.0",
         "gcsfs>=0.4.0,<=2022.01.0",
