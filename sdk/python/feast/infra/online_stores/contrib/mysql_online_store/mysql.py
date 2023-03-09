fd6d7b3436d15b68dffca8c4fc2c45b8f100e294
Ross Briden <ross.briden@affirm.com>
2023-03-09 15:53:08 -0800

    cursor fixes

--- sdk/python/feast/infra/online_stores/contrib/mysql_online_store/mysql.py
+++ sdk/python/feast/infra/online_stores/contrib/mysql_online_store/mysql.py
@@ -61,7 +61,7 @@ class MySQLOnlineStore(OnlineStore):
                 port=online_store_config.port or 3306,
                 autocommit=True,
             )
-        return self._tls_conn
+        return self._tls.conn
 
     def unpack_write_entity(self, entity: EntityKeyProto,
                                   values: Dict[str, ValueProto],
@@ -91,64 +91,63 @@ class MySQLOnlineStore(OnlineStore):
             raise ValueError(f'More columns in new entry {len(data)} than old entry {len(old_data)}')
 
         conn = self._get_conn(config)
-        cur = conn.cursor()
+        with conn.cursor() as cur:
+            project = config.project
+            for i in range(len(data)):
+                entity_key, values, timestamp, created_ts = self.unpack_write_entity(**data[i])
+                if old_data:
+                    old_entity_key, old_values, old_timestamp, old_created_ts = self.unpack_write_entity(**old_data[i])
 
-        project = config.project
-        for i in range(len(data)):
-            entity_key, values, timestamp, created_ts = self.unpack_write_entity(**data[i])
-            if old_data:
-                old_entity_key, old_values, old_timestamp, old_created_ts = self.unpack_write_entity(**old_data[i])
+                    # RB: this should be consistent between old and new
+                    if entity_key != old_entity_key:
+                        raise ValueError(f'New entity key {entity_key} != old entity key {old_entity_key}')
 
-                # RB: this should be consistent between old and new
-                if entity_key != old_entity_key:
-                    raise ValueError(f'New entity key {entity_key} != old entity key {old_entity_key}')
-
-                if values.keys() != old_values.keys():
-                    raise ValueError(f'New features {values.keys()} != old features {old_values.keys()}')
-            try:
-                for feature_name, val in values.items():
-                    if old_data:
-                        old_val = old_values[feature_name]
-                        cur.execute(
-                            f"""
-                                    UPDATE {_table_id(project, table)}
-                                    SET value=%s, event_ts=%s, created_ts=%s
-                                    WHERE entity_key=%s and feature_name=%s and value=%s and event_ts=%s and created_ts=%s
-                                    """,
-                            (
-                                val.SerializeToString(),
-                                timestamp,
-                                created_ts,
-                                old_val.SerializeToString(),
-                                old_timestamp,
-                                old_created_ts
-                            ),
-                        )
-                        if cur.rowcount == 0:
-                            conn.rollback()
-                            return False
-                    else:
-                        cur.execute(
-                            f"""
-                                    INSERT INTO {_table_id(project, table)}
-                                    (entity_key, feature_name, value, event_ts, created_ts)
-                                    values (%s, %s, %s, %s, %s)
-                                    """,
-                            (
-                                entity_key,
-                                feature_name,
-                                val.SerializeToString(),
-                                timestamp,
-                                created_ts,
-                            ),
-                        )
-            except pymysql.IntegrityError as e:
-                if e.args[0] == "ER_DUP_ENTRY":
-                    conn.rollback()
-                    return False
-                raise
-            conn.commit()
-            return True
+                    if values.keys() != old_values.keys():
+                        raise ValueError(f'New features {values.keys()} != old features {old_values.keys()}')
+                try:
+                    for feature_name, val in values.items():
+                        if old_data:
+                            old_val = old_values[feature_name]
+                            cur.execute(
+                                f"""
+                                        UPDATE {_table_id(project, table)}
+                                        SET value=%s, event_ts=%s, created_ts=%s
+                                        WHERE entity_key=%s and feature_name=%s and value=%s and event_ts=%s and created_ts=%s
+                                        """,
+                                (
+                                    val.SerializeToString(),
+                                    timestamp,
+                                    created_ts,
+                                    old_val.SerializeToString(),
+                                    old_timestamp,
+                                    old_created_ts
+                                ),
+                            )
+                            if cur.rowcount == 0:
+                                conn.rollback()
+                                return False
+                        else:
+                            cur.execute(
+                                f"""
+                                        INSERT INTO {_table_id(project, table)}
+                                        (entity_key, feature_name, value, event_ts, created_ts)
+                                        values (%s, %s, %s, %s, %s)
+                                        """,
+                                (
+                                    entity_key,
+                                    feature_name,
+                                    val.SerializeToString(),
+                                    timestamp,
+                                    created_ts,
+                                ),
+                            )
+                except pymysql.IntegrityError as e:
+                    if e.args[0] == "ER_DUP_ENTRY":
+                        conn.rollback()
+                        return False
+                    raise
+                conn.commit()
+        return True
 
     def online_write_batch(
         self,
@@ -161,33 +160,32 @@ class MySQLOnlineStore(OnlineStore):
     ) -> None:
 
         conn = self._get_conn(config)
-        cur = conn.cursor()
+        with conn.cursor() as cur:
+            project = config.project
 
-        project = config.project
+            for entity_key, values, timestamp, created_ts in data:
+                entity_key_bin = serialize_entity_key(
+                    entity_key,
+                    entity_key_serialization_version=2,
+                ).hex()
+                timestamp = _to_naive_utc(timestamp)
+                if created_ts is not None:
+                    created_ts = _to_naive_utc(created_ts)
 
-        for entity_key, values, timestamp, created_ts in data:
-            entity_key_bin = serialize_entity_key(
-                entity_key,
-                entity_key_serialization_version=2,
-            ).hex()
-            timestamp = _to_naive_utc(timestamp)
-            if created_ts is not None:
-                created_ts = _to_naive_utc(created_ts)
-
-            for feature_name, val in values.items():
-                self.write_to_table(
-                    created_ts,
-                    cur,
-                    entity_key_bin,
-                    feature_name,
-                    project,
-                    table,
-                    timestamp,
-                    val,
-                )
-            conn.commit()
-            if progress:
-                progress(1)
+                for feature_name, val in values.items():
+                    self.write_to_table(
+                        created_ts,
+                        cur,
+                        entity_key_bin,
+                        feature_name,
+                        project,
+                        table,
+                        timestamp,
+                        val,
+                    )
+                conn.commit()
+                if progress:
+                    progress(1)
 
     @staticmethod
     def write_to_table(
@@ -222,39 +220,39 @@ class MySQLOnlineStore(OnlineStore):
         config: RepoConfig,
         table: FeatureView,
         entity_keys: List[EntityKeyProto],
-        requested_features: Optional[List[str]] = None,
+        _: Optional[List[str]] = None,
     ) -> List[Tuple[Optional[datetime], Optional[Dict[str, ValueProto]]]]:
+
         conn = self._get_conn(config)
-        cur = conn.cursor()
+        with conn.cursor() as cur:
+            result: List[Tuple[Optional[datetime], Optional[Dict[str, Any]]]] = []
 
-        result: List[Tuple[Optional[datetime], Optional[Dict[str, Any]]]] = []
+            project = config.project
+            for entity_key in entity_keys:
+                entity_key_bin = serialize_entity_key(
+                    entity_key,
+                    entity_key_serialization_version=2,
+                ).hex()
 
-        project = config.project
-        for entity_key in entity_keys:
-            entity_key_bin = serialize_entity_key(
-                entity_key,
-                entity_key_serialization_version=2,
-            ).hex()
+                cur.execute(
+                    f"SELECT feature_name, value, event_ts FROM {_table_id(project, table)} WHERE entity_key = %s",
+                    (entity_key_bin,),
+                )
 
-            cur.execute(
-                f"SELECT feature_name, value, event_ts FROM {_table_id(project, table)} WHERE entity_key = %s",
-                (entity_key_bin,),
-            )
+                res = {}
+                res_ts: Optional[datetime] = None
+                records = cur.fetchall()
+                if records:
+                    for feature_name, val_bin, ts in records:
+                        val = ValueProto()
+                        val.ParseFromString(val_bin)
+                        res[feature_name] = val
+                        res_ts = ts
 
-            res = {}
-            res_ts: Optional[datetime] = None
-            records = cur.fetchall()
-            if records:
-                for feature_name, val_bin, ts in records:
-                    val = ValueProto()
-                    val.ParseFromString(val_bin)
-                    res[feature_name] = val
-                    res_ts = ts
-
-            if not res:
-                result.append((None, None))
-            else:
-                result.append((res_ts, res))
+                if not res:
+                    result.append((None, None))
+                else:
+                    result.append((res_ts, res))
         return result
 
     def update(
@@ -267,46 +265,44 @@ class MySQLOnlineStore(OnlineStore):
         partial: bool,
     ) -> None:
         conn = self._get_conn(config)
-        cur = conn.cursor()
-        project = config.project
-
-        # We don't create any special state for the entities in this implementation.
-        for table in tables_to_keep:
-            cur.execute(
-                f"""CREATE TABLE IF NOT EXISTS {_table_id(project, table)} (entity_key VARCHAR(512),
-                feature_name VARCHAR(256),
-                value BLOB,
-                event_ts timestamp NULL DEFAULT NULL,
-                created_ts timestamp NULL DEFAULT NULL,
-                PRIMARY KEY(entity_key, feature_name))"""
-            )
-
-            release_mode = ReleaseMode.update if 'release_mode' not in table.tags \
-                else ReleaseMode(table.tags['release_mode'])
-
-            if release_mode == ReleaseMode.overwrite:
-                cur.execute(f'DELETE FROM {_table_id(project, table)};')
-
-            cur.execute(
-                f"SHOW INDEXES FROM {_table_id(project, table)};"
-            )
-
-            index_exists = False
-            for index in cur.fetchall():
-                if index[2] == f"{_table_id(project, table)}_ek":
-                    if release_mode == ReleaseMode.overwrite:
-                        cur.execute(f"DROP INDEX {_table_id(project, table)}_ek ON {_table_id(project, table)};")
-                    else:
-                        index_exists = True
-                    break
-
-            if not index_exists:
+        with conn.cursor() as cur:
+            # We don't create any special state for the entities in this implementation.
+            for table in tables_to_keep:
                 cur.execute(
-                    f"ALTER TABLE {_table_id(project, table)} ADD INDEX {_table_id(project, table)}_ek (entity_key);"
+                    f"""CREATE TABLE IF NOT EXISTS {_table_id(project, table)} (entity_key VARCHAR(512),
+                    feature_name VARCHAR(256),
+                    value BLOB,
+                    event_ts timestamp NULL DEFAULT NULL,
+                    created_ts timestamp NULL DEFAULT NULL,
+                    PRIMARY KEY(entity_key, feature_name))"""
                 )
 
-        for table in tables_to_delete:
-            _drop_table_and_index(cur, project, table)
+                release_mode = ReleaseMode.update if 'release_mode' not in table.tags \
+                    else ReleaseMode(table.tags['release_mode'])
+
+                if release_mode == ReleaseMode.overwrite:
+                    cur.execute(f'DELETE FROM {_table_id(project, table)};')
+
+                cur.execute(
+                    f"SHOW INDEXES FROM {_table_id(project, table)};"
+                )
+
+                index_exists = False
+                for index in cur.fetchall():
+                    if index[2] == f"{_table_id(project, table)}_ek":
+                        if release_mode == ReleaseMode.overwrite:
+                            cur.execute(f"DROP INDEX {_table_id(project, table)}_ek ON {_table_id(project, table)};")
+                        else:
+                            index_exists = True
+                        break
+
+                if not index_exists:
+                    cur.execute(
+                        f"ALTER TABLE {_table_id(project, table)} ADD INDEX {_table_id(project, table)}_ek (entity_key);"
+                    )
+
+            for table in tables_to_delete:
+                _drop_table_and_index(cur, project, table)
 
     def teardown(
         self,
@@ -315,11 +311,11 @@ class MySQLOnlineStore(OnlineStore):
         entities: Sequence[Entity],
     ) -> None:
         conn = self._get_conn(config)
-        cur = conn.cursor()
-        project = config.project
+        with conn.cursor() as cur:
+            project = config.project
 
-        for table in tables:
-            _drop_table_and_index(cur, project, table)
+            for table in tables:
+                _drop_table_and_index(cur, project, table)
 
 
 def _drop_table_and_index(cur: Cursor, project: str, table: FeatureView) -> None:
