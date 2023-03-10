ac113d37cfac889243145142397b4cbffafc9a2f
Ross Briden <ross.briden@affirm.com>
2023-03-10 13:35:34 -0800

    init

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.28+affirm42"
+VERSION = "0.28+affirm43"
 DESCRIPTION = "Python SDK for Feast @ Affirm"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
