bfb26c36d5ff7200e40c35ea20147a4ba6bb884a
Danny Chiao <danny@tecton.ai>
2023-05-15 20:33:55 -0700

    chore: Adding logging to the feature server when the registry is refreshed (#3623)
    Adding logging to the feature server when the registry is refreshed
    
    Signed-off-by: Danny Chiao <danny@tecton.ai>

--- sdk/python/feast/infra/registry/sql.py
+++ sdk/python/feast/infra/registry/sql.py
@@ -1,3 +1,4 @@
+import logging
 import uuid
 from datetime import datetime, timedelta
 from enum import Enum
@@ -178,6 +179,8 @@ feast_metadata = Table(
     Column("last_updated_timestamp", BigInteger, nullable=False),
 )
 
+logger = logging.getLogger(__name__)
+
 
 class SqlRegistryConfig(RegistryConfig):
     registry_type: StrictStr = "sql"
@@ -256,6 +259,7 @@ class SqlRegistry(BaseRegistry):
             )
 
             if expired:
+                logger.info("Registry cache expired, so refreshing")
                 self.refresh()
 
     def get_stream_feature_view(
