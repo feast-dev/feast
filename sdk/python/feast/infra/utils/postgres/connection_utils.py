4de0d8c9245365ba6ec9f1c626716a8dfdb5073c
Natan Viana <natanvianat16@gmail.com>
2023-06-04 00:00:35 -0300

    fix: Implements connection pool for postgres online store (#3633)
    * Implements connection pool for postgres online store
    
    Signed-off-by: Natan Viana <natan.viana@buser.com.br>
    
    * Connection type configurable
    
    Signed-off-by: Natan Viana <natan.viana@buser.com.br>
    
    * Postgres online store with connection type configurable
    
    Signed-off-by: Natan Viana <natan.viana@buser.com.br>
    
    * Fix imports
    
    Signed-off-by: Natan Viana <natan.viana@buser.com.br>
    
    * Writes integration test to validate postgres connection pool
    
    Signed-off-by: Natan Viana <natan.viana@buser.com.br>
    
    * Fix
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Fix
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    * Fix
    
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    
    ---------
    
    Signed-off-by: Natan Viana <natan.viana@buser.com.br>
    Signed-off-by: Felix Wang <wangfelix98@gmail.com>
    Co-authored-by: Natan Viana <natan.viana@buser.com.br>
    Co-authored-by: Felix Wang <wangfelix98@gmail.com>
    Signed-off-by: zerafachris PERSONAL <zerafachris@gmail.com>

--- sdk/python/feast/infra/utils/postgres/connection_utils.py
+++ sdk/python/feast/infra/utils/postgres/connection_utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import psycopg2
 import psycopg2.extras
 import pyarrow as pa
+from psycopg2.pool import SimpleConnectionPool
 
 from feast.infra.utils.postgres.postgres_config import PostgreSQLConfig
 from feast.type_map import arrow_to_pg_type
@@ -22,8 +23,26 @@ def _get_conn(config: PostgreSQLConfig):
         sslcert=config.sslcert_path,
         sslrootcert=config.sslrootcert_path,
         options="-c search_path={}".format(config.db_schema or config.user),
+        keepalives_idle=config.keepalives_idle,
     )
     return conn
+
+
+def _get_connection_pool(config: PostgreSQLConfig):
+    return SimpleConnectionPool(
+        config.min_conn,
+        config.max_conn,
+        dbname=config.database,
+        host=config.host,
+        port=int(config.port),
+        user=config.user,
+        password=config.password,
+        sslmode=config.sslmode,
+        sslkey=config.sslkey_path,
+        sslcert=config.sslcert_path,
+        sslrootcert=config.sslrootcert_path,
+        options="-c search_path={}".format(config.db_schema or config.user),
+    )
 
 
 def _df_to_create_table_sql(entity_df, table_name) -> str:
