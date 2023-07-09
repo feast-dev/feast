fe462858ab441ba86f3d1cd381a4595602d756bd
Ross Briden <ross.briden@affirm.com>
2023-07-08 20:09:41 -0700

    increase setup version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.28+affirm92"
+VERSION = "0.28+affirm93"
 DESCRIPTION = "Python SDK for Feast @ Affirm"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
