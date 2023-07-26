aa8e1a92547ea8b55d2183efe185502f803a2af0
mbackes <mbackes@hotwire.com>
2023-07-26 16:54:57 -0700

    added feature view for vector dbs

---
+++ sdk/python/feast/expediagroup/vectordb/vector_feature_view.py
@@ -0,0 +1,205 @@
+import copy
+from datetime import datetime, timedelta
+from typing import Dict, List, Optional, Type
+
+from typeguard import typechecked
+
+from feast.data_source import DataSource
+from feast.entity import Entity
+from feast.feature_view import FeatureView
+from feast.field import Field
+from feast.expediagroup.vectordb.index_type import IndexType
+from feast.protos.feast.core.FeatureView_pb2 import FeatureView as FeatureViewProto
+from feast.protos.feast.core.VectorFeatureView_pb2 import VectorFeatureView as VectorFeatureViewProto
+
+from feast.usage import log_exceptions
+
+@typechecked
+class VectorFeatureView(FeatureView):
+    """
+    A FeatureView for vector databases.
+
+    Attributes:
+        name: The unique name of the feature view.
+        entities: The list of names of entities that this feature view is associated with.
+        ttl: The amount of time this group of features lives. A ttl of 0 indicates that
+            this group of features lives forever. Note that large ttl's or a ttl of 0
+            can result in extremely computationally intensive queries.
+        batch_source: The batch source of data where this group of features
+            is stored. This is optional ONLY if a push source is specified as the
+            stream_source, since push sources contain their own batch sources.
+        stream_source: The stream source of data where this group of features is stored.
+        vector_field: The name of the field containing the vector in the source
+        dimensions: The number of dimensions of the vector
+        index_algorithm: The indexing algorithm that should be used in the vector database
+        schema: The schema of the feature view, including feature, timestamp, and entity
+            columns. If not specified, can be inferred from the underlying data source.
+        entity_columns: The list of entity columns contained in the schema. If not specified,
+            can be inferred from the underlying data source.
+        features: The list of feature columns contained in the schema. If not specified,
+            can be inferred from the underlying data source.
+        online: A boolean indicating whether online retrieval is enabled for this feature
+            view.
+        description: A human-readable description.
+        tags: A dictionary of key-value pairs to store arbitrary metadata.
+        owner: The owner of the feature view, typically the email of the primary
+            maintainer.
+    """
+    vector_field: str
+    dimensions: int
+    index_algorithm: IndexType
+
+    @log_exceptions
+    def __init__(
+        self,
+        *,
+        name: str,
+        source: DataSource,
+        vector_field: str,
+        dimensions: int,
+        index_algorithm: IndexType,
+        schema: Optional[List[Field]] = None,
+        entities: List[Entity] = None,
+        ttl: Optional[timedelta] = timedelta(days=0),
+        online: bool = True,
+        description: str = "",
+        tags: Optional[Dict[str, str]] = None,
+        owner: str = "",
+    ):
+        """
+        Creates a VectorFeatureView object.
+
+        Args:
+            name: The unique name of the feature view.
+            source: The source of data for this group of features. May be a stream source, or a batch source.
+                If a stream source, the source should contain a batch_source for backfills & batch materialization.
+            vector_field: The name of the field containing the vector in the source
+            dimensions: The number of dimensions of the vector
+            index_algorithm: The indexing algorithm that should be used in the vector database
+            schema (optional): The schema of the feature view, including feature, timestamp,
+                and entity columns.
+            entities (optional): The list of entities with which this group of features is associated.
+            ttl (optional): The amount of time this group of features lives. A ttl of 0 indicates that
+                this group of features lives forever. Note that large ttl's or a ttl of 0
+                can result in extremely computationally intensive queries.
+            online (optional): A boolean indicating whether online retrieval is enabled for
+                this feature view.
+            description (optional): A human-readable description.
+            tags (optional): A dictionary of key-value pairs to store arbitrary metadata.
+            owner (optional): The owner of the feature view, typically the email of the
+                primary maintainer.
+        """
+        super().__init__(
+            name=name,
+            source=source,
+            schema=schema,
+            entities=entities,
+            ttl=ttl,
+            online=online,
+            description=description,
+            tags=tags,
+            owner=owner
+        )
+
+        self.vector_field = vector_field
+        self.dimensions = dimensions
+        self.index_algorithm = index_algorithm
+
+    def __hash__(self):
+        return super().__hash__()
+
+    def __copy__(self):
+        vector_feature_view = VectorFeatureView(
+            name=self.name,
+            source=self.stream_source if self.stream_source else self.batch_source,
+            vector_field=self.vector_field,
+            dimensions=self.dimensions,
+            index_algorithm=self.index_algorithm,
+            schema=self.schema,
+            ttl=self.ttl,
+            online=self.online,
+            description=self.description,
+            tags=self.tags,
+            owner=self.owner
+        )
+
+        # This is deliberately set outside of the FV initialization as we do not have the Entity objects.
+        vector_feature_view.entities = self.entities
+        vector_feature_view.features = copy.copy(self.features)
+        vector_feature_view.entity_columns = copy.copy(self.entity_columns)
+        vector_feature_view.projection = copy.copy(self.projection)
+        vector_feature_view.original_schema = copy.copy(self.original_schema)
+        vector_feature_view.original_entities = copy.copy(self.original_entities)
+
+        return vector_feature_view
+
+    def __eq__(self, other):
+        if not isinstance(other, VectorFeatureView):
+            raise TypeError(
+                "Comparisons should only involve VectorFeatureView class objects."
+            )
+
+        if not super().__eq__(other):
+            return False
+
+        if (self.vector_field != other.vector_field
+            or self.dimensions != other.dimensions
+            or self.index_algorithm != other.index_algorithm
+        ):
+            return False
+
+        return True
+
+    @property
+    def proto_class(self) -> Type[VectorFeatureViewProto]:
+        return VectorFeatureViewProto
+
+    def to_proto(self) -> VectorFeatureViewProto:
+        feature_view_proto = super().to_proto()
+
+        proto = VectorFeatureViewProto(
+            feature_view=feature_view_proto,
+            entities=[entity.to_proto() for entity in self.original_entities],
+            vector_field=self.vector_field,
+            dimensions=self.dimensions,
+            index_type=self.index_algorithm.value
+        )
+
+        return proto
+
+    @classmethod
+    def from_proto(cls, proto: VectorFeatureViewProto):
+        """
+        Creates a vector feature view from a protobuf representation of a vector feature view.
+
+        Args:
+            feature_view_proto: A protobuf representation of a vector feature view.
+
+        Returns:
+            A VectorFeatureViewProto object based on the vector feature view protobuf.
+        """
+        feature_view = FeatureView.from_proto(proto.feature_view)
+
+        # convert Entity protos to Entity - necessary because FeatureView doesn't preserve them properly
+        converted_entities = [Entity.from_proto(entity) for entity in proto.entities]
+
+        vector_field = proto.vector_field
+        dimensions = proto.dimensions
+        index_algorithm = IndexType[proto.index_type]
+
+        vector_feature_view = VectorFeatureView(
+            name=feature_view.name,
+            source=feature_view.stream_source if feature_view.stream_source else feature_view.batch_source,
+            vector_field=vector_field,
+            dimensions=dimensions,
+            index_algorithm=index_algorithm,
+            schema=feature_view.original_schema,
+            entities=converted_entities,
+            ttl=feature_view.ttl,
+            online=feature_view.online,
+            description=feature_view.description,
+            tags=feature_view.tags,
+            owner=feature_view.owner
+        )
+
+        return vector_feature_view
---
+++ sdk/python/tests/expediagroup/test_vector_feature_view.py
@@ -0,0 +1,234 @@
+from copy import copy
+from datetime import timedelta
+
+import pytest
+from pydantic import ValidationError
+
+from feast.aggregation import Aggregation
+from feast.batch_feature_view import BatchFeatureView
+from feast.data_format import AvroFormat
+from feast.data_source import KafkaSource, PushSource
+from feast.entity import Entity
+from feast.expediagroup.vectordb.index_type import IndexType
+from feast.expediagroup.vectordb.vector_feature_view import VectorFeatureView
+from feast.feature_view import FeatureView
+from feast.field import Field
+from feast.infra.offline_stores.file_source import FileSource
+from feast.protos.feast.core.DataSource_pb2 import DataSource
+from feast.protos.feast.core.FeatureView_pb2 import FeatureView as FeatureViewProto
+from feast.protos.feast.core.VectorFeatureView_pb2 import VectorFeatureView as VectorFeatureViewProto
+from feast.protos.feast.types.Value_pb2 import ValueType
+from feast.types import Float32
+from google.protobuf.duration_pb2 import Duration
+
+
+def test_create_vector_feature_view_with_conflicting_entities():
+    user1 = Entity(name="user1", join_keys=["user_id"])
+    user2 = Entity(name="user2", join_keys=["user_id"])
+    batch_source = FileSource(path="some path")
+
+    with pytest.raises(ValueError):
+        _ = VectorFeatureView(
+            name="test",
+            entities=[user1, user2],
+            ttl=timedelta(days=30),
+            source=batch_source,
+            vector_field="vector_field",
+            dimensions=32,
+            index_algorithm=IndexType.hnsw
+        )
+
+
+def test_hash():
+    file_source = FileSource(name="my-file-source", path="test.parquet")
+    feature_view_1 = VectorFeatureView(
+        name="my-feature-view",
+        entities=[],
+        schema=[
+            Field(name="feature1", dtype=Float32),
+            Field(name="feature2", dtype=Float32),
+        ],
+        source=file_source,
+        vector_field="vector_field",
+        dimensions=32,
+        index_algorithm=IndexType.hnsw
+    )
+    feature_view_2 = VectorFeatureView(
+        name="my-feature-view",
+        entities=[],
+        schema=[
+            Field(name="feature1", dtype=Float32),
+            Field(name="feature2", dtype=Float32),
+        ],
+        source=file_source,
+        vector_field="vector_field",
+        dimensions=32,
+        index_algorithm=IndexType.hnsw
+    )
+    feature_view_3 = VectorFeatureView(
+        name="my-feature-view",
+        entities=[],
+        schema=[Field(name="feature1", dtype=Float32)],
+        source=file_source,
+        vector_field="vector_field",
+        dimensions=32,
+        index_algorithm=IndexType.hnsw
+    )
+    feature_view_4 = VectorFeatureView(
+        name="my-feature-view",
+        entities=[],
+        schema=[Field(name="feature1", dtype=Float32)],
+        source=file_source,
+        vector_field="vector_field",
+        dimensions=32,
+        index_algorithm=IndexType.hnsw,
+        description="test"
+    )
+
+    s1 = {feature_view_1, feature_view_2}
+    assert len(s1) == 1
+
+    s2 = {feature_view_1, feature_view_3}
+    assert len(s2) == 2
+
+    s3 = {feature_view_3, feature_view_4}
+    assert len(s3) == 2
+
+    s4 = {feature_view_1, feature_view_2, feature_view_3, feature_view_4}
+    assert len(s4) == 3
+
+
+def test_vector_field_mapping():
+    feature_view_name = "name"
+    vector_field_name = "vector_field"
+    vector_dimensions = 32
+    vector_index_algorithm = IndexType.hnsw
+
+    user1 = Entity(name="user1")
+    batch_source = FileSource(path="some path")
+
+    feature_view = VectorFeatureView(
+        name=feature_view_name,
+        entities=[user1],
+        ttl=timedelta(days=30),
+        source=batch_source,
+        vector_field=vector_field_name,
+        dimensions=vector_dimensions,
+        index_algorithm=vector_index_algorithm
+    )
+
+    assert(feature_view.vector_field == vector_field_name)
+    assert(feature_view.dimensions == vector_dimensions)
+    assert(feature_view.index_algorithm == vector_index_algorithm)
+    assert(feature_view.name == feature_view_name)
+
+
+def test_copy():
+    user1 = Entity(name="user1")
+    batch_source = FileSource(path="some path")
+
+    feature_view = VectorFeatureView(
+        name="test",
+        entities=[user1],
+        ttl=timedelta(days=30),
+        source=batch_source,
+        vector_field="vector_field",
+        dimensions=32,
+        index_algorithm=IndexType.hnsw
+    )
+
+    feature_view_copy = copy(feature_view)
+    assert(feature_view_copy == feature_view)
+
+
+def test_join_keys():
+    entity1 = Entity(name="user1", join_keys=["feature1"])
+    entity2 = Entity(name="user2", join_keys=["feature2"])
+    batch_source = FileSource(path="some path")
+
+    feature_view = VectorFeatureView(
+        name="test",
+        entities=[entity1, entity2],
+        ttl=timedelta(days=30),
+        source=batch_source,
+        vector_field="vector_field",
+        dimensions=32,
+        index_algorithm=IndexType.hnsw,
+        schema=[
+            Field(name="feature1", dtype=Float32),
+            Field(name="feature2", dtype=Float32),
+        ]
+    )
+
+    join_keys = feature_view.join_keys
+    assert(isinstance(join_keys, list))
+    assert(len(join_keys) == 2)
+    assert(join_keys.__getitem__(0) == "feature1")
+    assert(join_keys.__getitem__(1) == "feature2")
+
+
+def test_from_proto():
+    vector_field_name = "vector_field"
+    vector_dimensions = 32
+    vector_index_algorithm = IndexType.hnsw
+
+    entity1 = Entity(name="user1", join_keys=["feature1"])
+    entities = [entity1]
+    file_source = FileSource(name="my-file-source", path="test.parquet")
+    feature_view = FeatureView(
+        name="my-feature-view",
+        entities=[entity1],
+        schema=[
+            Field(name="feature1", dtype=Float32),
+            Field(name="feature2", dtype=Float32),
+        ],
+        source=file_source,
+    )
+
+    feature_view_proto = feature_view.to_proto()
+    entities_proto = [entity.to_proto() for entity in entities]
+    vector_feature_view_proto = VectorFeatureViewProto(
+        feature_view=feature_view_proto,
+        entities=entities_proto,
+        vector_field=vector_field_name,
+        dimensions=vector_dimensions,
+        index_type=vector_index_algorithm.value
+    )
+
+    vector_feature_view = VectorFeatureView.from_proto(vector_feature_view_proto)
+    assert(isinstance(vector_feature_view, VectorFeatureView))
+    assert(vector_feature_view.original_entities == entities)
+    assert(vector_feature_view.vector_field == vector_field_name)
+    assert(vector_feature_view.dimensions == vector_dimensions)
+    assert(vector_feature_view.index_algorithm == vector_index_algorithm)
+
+
+def test_to_proto():
+    vector_field_name = "vector_field"
+    vector_dimensions = 32
+    vector_index_algorithm = IndexType.hnsw
+
+    entity1 = Entity(name="user1", join_keys=["feature1"])
+    entity2 = Entity(name="user2", join_keys=["feature2"])
+    batch_source = FileSource(path="some path")
+
+    vector_feature_view = VectorFeatureView(
+        name="test",
+        entities=[entity1, entity2],
+        ttl=timedelta(days=30),
+        source=batch_source,
+        vector_field=vector_field_name,
+        dimensions=vector_dimensions,
+        index_algorithm=vector_index_algorithm,
+        schema=[
+            Field(name="feature1", dtype=Float32),
+            Field(name="feature2", dtype=Float32),
+        ]
+    )
+
+    proto = vector_feature_view.to_proto()
+
+    assert(isinstance(proto, VectorFeatureViewProto))
+    assert(proto.vector_field == vector_field_name)
+    assert(proto.dimensions == vector_dimensions)
+    assert(proto.index_type == vector_index_algorithm.value)
