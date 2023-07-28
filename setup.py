060c28f552976695145b9aa4936096b2385fb08a
Ross Briden <ross.briden@affirm.com>
2023-07-28 14:36:12 -0700

    increment'

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.28+affirm103"
+VERSION = "0.28+affirm104"
 DESCRIPTION = "Python SDK for Feast @ Affirm"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
