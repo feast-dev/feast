5e516025a51d8e20d0e61fa0a757ccd0fb0a688c
Israel Agundis <69486978+IsraelAgundis@users.noreply.github.com>
2023-07-28 10:00:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ except ImportError:
     from distutils.core import setup
 
 NAME = "feast"
-VERSION = "0.28+affirm102"
+VERSION = "0.28+affirm103"
 DESCRIPTION = "Python SDK for Feast @ Affirm"
 URL = "https://github.com/feast-dev/feast"
 AUTHOR = "Feast"
