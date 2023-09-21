2192e6527fa10f1580e4dd8f350e05e45af981b7
James Crabtree <77012363+james-crabtree-sp@users.noreply.github.com>
2023-09-21 14:53:10 -0500

    feat: Add support for in_cluster config and additional labels for bytewax materialization (#3754)
    * SAASMLOPS-734 bytewax in-cluster config, custom labels, fix worker image deps
    
    Signed-off-by: James Crabtree <james.crabtree@sailpoint.com>
    
    * SAASMLOPS-769 make max parallelism configurable
    
    Signed-off-by: James Crabtree <james.crabtree@sailpoint.com>
    
    ---------
    
    Signed-off-by: James Crabtree <james.crabtree@sailpoint.com>

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ REDIS_REQUIRED = [
     "hiredis>=2.0.0,<3",
 ]
 
-AWS_REQUIRED = ["boto3>=1.17.0,<2", "docker>=5.0.2"]
+AWS_REQUIRED = ["boto3>=1.17.0,<2", "docker>=5.0.2", "s3fs"]
 
 BYTEWAX_REQUIRED = ["bytewax==0.15.1", "docker>=5.0.2", "kubernetes<=20.13.0"]
 
