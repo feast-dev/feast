497af8e2f62bdd8270ae6c50ba7756f31a8572ae
Shuchu Han <shuchu.han@gmail.com>
2024-02-07 21:34:13 -0500

    feat: fix few type checking errors.
    Signed-off-by: Shuchu Han <shuchu.han@gmail.com>

--- sdk/python/tests/unit/infra/offline_stores/test_offline_store.py
+++ sdk/python/tests/unit/infra/offline_stores/test_offline_store.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, no_type_check
+from typing import List, Optional
 from unittest.mock import MagicMock, patch
 
 import pandas as pd
@@ -60,15 +60,13 @@ class MockRetrievalJob(RetrievalJob):
         """
         return pyarrow.Table()
 
-    @no_type_check
     @property
-    def full_feature_names(self) -> bool:
+    def full_feature_names(self) -> bool:  # type: ignore
         """Returns True if full feature names should be applied to the results of the query."""
         return False
 
-    @no_type_check
     @property
-    def on_demand_feature_views(self) -> List[OnDemandFeatureView]:
+    def on_demand_feature_views(self) -> List[OnDemandFeatureView]:  # type: ignore
         """Returns a list containing all the on demand feature views to be handled."""
         return []
 
