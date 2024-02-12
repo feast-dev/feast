5f38daf30d7b0d4bbb75a06957c5d1aa4c7107fd
john.lemmon <john.lemmon@medely.com>
2024-02-12 14:09:26 -0600

    Bugfix for grabbing historical data from Snowflake with array type features that are null for an entity.
    Signed-off-by: john.lemmon <john.lemmon@medely.com>
    
    Update docs to reflect array support in Snowflake
    
    Signed-off-by: john.lemmon <john.lemmon@medely.com>

--- sdk/python/feast/infra/offline_stores/snowflake.py
+++ sdk/python/feast/infra/offline_stores/snowflake.py
@@ -463,7 +463,9 @@ class SnowflakeRetrievalJob(RetrievalJob):
                     Array(Float32),
                     Array(Bool),
                 ]:
-                    df[feature.name] = [json.loads(x) for x in df[feature.name]]
+                    df[feature.name] = [
+                        json.loads(x) if x else None for x in df[feature.name]
+                    ]
 
         return df
 
