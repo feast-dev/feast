9ec43d856a464e99d6cfc5038c10ccf010387abc
Hai Nguyen <quanghai.ng1512@gmail.com>
2024-02-18 21:09:24 +0700

    fix: Prune dependencies
    Signed-off-by: Hai Nguyen <quanghai.ng1512@gmail.com>

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ REQUIRED = [
     "click>=7.0.0,<9.0.0",
     "colorama>=0.3.9,<1",
     "dill~=0.3.0",
-    "fastavro>=1.1.0,<2",
     "grpcio>=1.56.2,<2",
     "grpcio-tools>=1.56.2,<2",
     "grpcio-reflection>=1.56.2,<2",
@@ -55,7 +54,6 @@ REQUIRED = [
     "mmh3",
     "numpy>=1.22,<1.25",
     "pandas>=1.4.3,<3",
-    "pandavro>=1.7.1",
     # Higher than 4.23.4 seems to cause a seg fault
     "protobuf<4.23.4,>3.20",
     "proto-plus>=1.20.0,<2",
