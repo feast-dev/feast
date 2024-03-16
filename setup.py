e73dae3cc8b0436c28daa9f4e7b7f56daf9cd21d
cburroughs <chris.burroughs@gmail.com>
2024-03-16 06:48:40 -0400

    chore: Loosen mypy-protobuf version constraint (#4009)
    chore: loosen mypy-protobuf version constraint
    
    Having this pinned to an exact version increases the likelihood of
    conflict with user code or other libraries.
    
    Changelog: https://github.com/nipunn1313/mypy-protobuf/blob/main/CHANGELOG.md
    
    Signed-off-by: Chris Burroughs <chris.burroughs@gmail.com>

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ REQUIRED = [
     "click>=7.0.0,<9.0.0",
     "colorama>=0.3.9,<1",
     "dill~=0.3.0",
-    "mypy-protobuf==3.1",
+    "mypy-protobuf>=3.1",
     "Jinja2>=2,<4",
     "jsonschema",
     "mmh3",
@@ -67,7 +67,8 @@ REQUIRED = [
     "fastapi>=0.68.0",
     "uvicorn[standard]>=0.14.0,<1",
     "gunicorn",
-    "dask>=2021.1.0",
+    # https://github.com/dask/dask/issues/10996
+    "dask>=2021.1.0,<2024.3.0",
     "bowler",  # Needed for automatic repo upgrades
     # FastAPI does not correctly pull starlette dependency on httpx see thread(https://github.com/tiangolo/fastapi/issues/5656).
     "httpx>=0.23.3",
@@ -222,7 +223,7 @@ DOCS_REQUIRED = CI_REQUIRED.copy()
 for _r in MYSQL_REQUIRED:
     DOCS_REQUIRED.remove(_r)
 
-DEV_REQUIRED = ["mypy-protobuf==3.1", "grpcio-testing~=1.0"] + CI_REQUIRED
+DEV_REQUIRED = ["grpcio-testing~=1.0"] + CI_REQUIRED
 
 # Get git repo root directory
 repo_root = str(pathlib.Path(__file__).resolve().parent)
@@ -400,7 +401,7 @@ setup(
         "setuptools_scm",
         "grpcio>=1.56.2,<2",
         "grpcio-tools>=1.56.2,<2",
-        "mypy-protobuf==3.1",
+        "mypy-protobuf>=3.1",
         "pybindgen==0.22.0",
     ],
     cmdclass={
