82fe5f1c2e2dac257a1bd032ca6409c575204670
cmuhao <sduxuhao@gmail.com>
2024-04-11 21:44:23 -0700

    format

--- sdk/python/tests/integration/feature_repos/universal/online_store/cassandra.py
+++ sdk/python/tests/integration/feature_repos/universal/online_store/cassandra.py
@@ -28,9 +28,7 @@ from tests.integration.feature_repos.universal.online_store_creator import (
 class CassandraOnlineStoreCreator(OnlineStoreCreator):
     def __init__(self, project_name: str, **kwargs):
         super().__init__(project_name)
-        self.container = CassandraContainer("cassandra:4.1.4").with_exposed_ports(
-            9042
-        )
+        self.container = CassandraContainer("cassandra:4.1.4").with_exposed_ports(9042)
 
     def create_online_store(self) -> Dict[str, object]:
         self.container.start()
