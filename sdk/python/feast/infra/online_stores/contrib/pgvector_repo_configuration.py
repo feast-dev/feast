20fb9226e9c67fb91388dcc6e13d5ae9d54a6aa4
cmuhao <sduxuhao@gmail.com>
2024-04-14 18:18:20 -0700

    add pgvector int test

--- sdk/python/feast/infra/online_stores/contrib/pgvector_repo_configuration.py
+++ sdk/python/feast/infra/online_stores/contrib/pgvector_repo_configuration.py
@@ -2,11 +2,11 @@ from tests.integration.feature_repos.integration_test_repo_config import (
     IntegrationTestRepoConfig,
 )
 from tests.integration.feature_repos.universal.online_store.postgres import (
-    PostgresOnlineStoreCreator,
+    PGVectorOnlineStoreCreator,
 )
 
 FULL_REPO_CONFIGS = [
     IntegrationTestRepoConfig(
-        online_store="postgres", online_store_creator=PostgresOnlineStoreCreator
-    )
+        online_store="pgvector", online_store_creator=PGVectorOnlineStoreCreator
+    ),
 ]
