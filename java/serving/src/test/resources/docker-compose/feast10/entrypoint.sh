a21bb621c915c9984f43e2c6867c8b5e0b516027
Tornike Gurgenidze <togurg14@freeuni.edu.ge>
2024-04-23 16:33:16 +0400

    chore: Move feast install to docker build in java it tests (#4126)
    * chore: Move feast install to docker build in java it tests
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * remove commented out lines in compose file
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * make local compose mode default
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * limit COPY contents
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * remove requirements.txt from java tests docker image
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * include pyproject.toml in dockerfile
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * change links to depends_on
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * try updating setup-python to v5
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * pin macos image to macos-12
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    * force rerun
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    
    ---------
    
    Signed-off-by: tokoko <togurg14@freeuni.edu.ge>
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>

--- java/serving/src/test/resources/docker-compose/feast10/entrypoint.sh
+++ java/serving/src/test/resources/docker-compose/feast10/entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# feast root directory is expected to be mounted (eg, by docker compose)
-cd /mnt/feast
-pip install -e '.[grpcio,redis]'
-
 cd /app
 python materialize.py
 feast serve_transformations --port 8080
