999cba1213f2ac1b7f9e021abd30c40046398430
cmuhao <sduxuhao@gmail.com>
2024-05-11 17:50:53 -0700

    add test
    Signed-off-by: cmuhao <sduxuhao@gmail.com>

---
+++ sdk/python/tests/integration/feature_repos/universal/online_store/elasticsearch.py
@@ -0,0 +1,29 @@
+import os
+from typing import Dict
+
+from testcontainers.elasticsearch import ElasticSearchContainer
+
+from tests.integration.feature_repos.universal.online_store_creator import (
+    OnlineStoreCreator,
+)
+
+
+class ElasticSearchOnlineStoreCreator(OnlineStoreCreator):
+
+    def __init__(self, project_name: str, **kwargs):
+        super().__init__(project_name)
+        self.container = ElasticSearchContainer(
+            "elasticsearch:8.3.3",
+
+        ).with_exposed_ports(9200)
+
+    def create_online_store(self) -> Dict[str, str]:
+        self.container.start()
+        return {
+            "host": "localhost",
+            "type": "elasticsearch",
+            "port": self.container.get_exposed_port(9200)
+        }
+
+    def teardown(self):
+        self.container.stop()
