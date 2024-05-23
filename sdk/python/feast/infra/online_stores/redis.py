38d70d1a394ce17a87ec51f782dbb0fad7384546
Jiwon Park <phil.park@sktelecom.com>
2024-05-24 07:59:27 +0900

    fix: Set default to True for backwards compatibility
    Signed-off-by: Jiwon Park <phil.park@sktelecom.com>

--- sdk/python/feast/infra/online_stores/redis.py
+++ sdk/python/feast/infra/online_stores/redis.py
@@ -77,7 +77,7 @@ class RedisOnlineStoreConfig(FeastConfigBaseModel):
     key_ttl_seconds: Optional[int] = None
     """(Optional) redis key bin ttl (in seconds) for expiring entities"""
 
-    full_scan_for_deletion: Optional[bool] = False
+    full_scan_for_deletion: Optional[bool] = True
     """(Optional) whether to scan for deletion of features"""
 
 
