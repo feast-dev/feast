0578c30054892237ecdf2133273342f571d3e589
cmuhao <sduxuhao@gmail.com>
2024-06-16 13:42:48 -0700

    fix test
    Signed-off-by: cmuhao <sduxuhao@gmail.com>

--- sdk/python/feast/repo_config.py
+++ sdk/python/feast/repo_config.py
@@ -177,10 +177,10 @@ class RepoConfig(FeastBaseModel):
     A value of 2 uses a newer serialization scheme, supported as of Feast 0.23.
     A value of 3 uses the latest serialization scheme, supported as of Feast 0.38.
     The main difference between the three schema is that
-    v1. the serialization scheme v1 stored `long` values as `int`s, which would result in errors trying to serialize a range of values.
-    v2 fixes this error, but v1 is kept around to ensure backwards compatibility - specifically the ability to read
+    v1: the serialization scheme v1 stored `long` values as `int`s, which would result in errors trying to serialize a range of values.
+    v2: fixes this error, but v1 is kept around to ensure backwards compatibility - specifically the ability to read
     feature values for entities that have already been written into the online store.
-    v3 add entity_key value length to serialized bytes to enable deserialization, which can be used in retrieval of entity_key in document retrieval.
+    v3: add entity_key value length to serialized bytes to enable deserialization, which can be used in retrieval of entity_key in document retrieval.
     """
 
     coerce_tz_aware: Optional[bool] = True
