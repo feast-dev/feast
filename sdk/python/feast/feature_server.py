4cb24fc93ee2eae46428d0f92a47f08a8ba84564
Francisco Arceo <arceofrancisco@gmail.com>
2024-06-17 14:51:00 -0400

    chore: Removing warning from feature server (#4281)
    * chore: Removing logging from feature server
    
    Signed-off-by: Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
    
    * linter
    
    Signed-off-by: Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>
    Co-authored-by: Francisco Javier Arceo <franciscojavierarceo@users.noreply.github.com>

--- sdk/python/feast/feature_server.py
+++ sdk/python/feast/feature_server.py
@@ -2,7 +2,6 @@ import json
 import sys
 import threading
 import traceback
-import warnings
 from contextlib import asynccontextmanager
 from typing import List, Optional
 
@@ -147,10 +146,6 @@ def get_app(
 
     @app.post("/write-to-online-store")
     def write_to_online_store(body=Depends(get_body)):
-        warnings.warn(
-            "write_to_online_store is deprecated. Please consider using /push instead",
-            RuntimeWarning,
-        )
         try:
             request = WriteToFeatureStoreRequest(**json.loads(body))
             df = pd.DataFrame(request.df)
