b6cec74f17be257a35a1f71b48097390e9566710
Daniele Martinoli <86618610+dmartinol@users.noreply.github.com>
2024-06-24 11:36:12 +0200

    using pytest.mark.parametrize tests
    Signed-off-by: Daniele Martinoli <86618610+dmartinol@users.noreply.github.com>
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

--- sdk/python/tests/unit/permissions/test_role_manager.py
+++ sdk/python/tests/unit/permissions/test_role_manager.py
@@ -1,17 +1,24 @@
 import assertpy
+import pytest
 
 from feast.permissions.role_manager import RoleManager
 
 
-def test_has_roles():
+@pytest.mark.parametrize(
+    "user, roles, result",
+    [
+        ("c", [], False),
+        ("a", ["a1"], True),
+        ("a", ["a1", "a2"], True),
+        ("a", ["a1", "a2", "a3"], False),
+        ("b", ["a1", "a3"], False),
+        ("b", ["b1", "b2"], True),
+    ],
+)
+def test_has_roles(user, roles, result):
     rm = RoleManager()
     rm.add_roles_for_user("a", ["a1", "a2"])
     rm.add_roles_for_user("b", ["b1", "b2"])
-
-    assertpy.assert_that(rm.has_roles_for_user("c", list())).is_false()
-    assertpy.assert_that(rm.has_roles_for_user("a", ["a1"])).is_true()
-    assertpy.assert_that(rm.has_roles_for_user("a", ["a1", "a2"])).is_true()
-    assertpy.assert_that(rm.has_roles_for_user("a", ["a1", "a2", "a3"])).is_false()
-    assertpy.assert_that(rm.has_roles_for_user("a", ["a1", "a3"])).is_false()
-    assertpy.assert_that(rm.has_roles_for_user("b", ["a1", "a3"])).is_false()
-    assertpy.assert_that(rm.has_roles_for_user("b", ["b1", "b2"])).is_true()
+    assertpy.assert_that(rm.has_roles_for_user(user=user, roles=roles)).is_equal_to(
+        result
+    )
