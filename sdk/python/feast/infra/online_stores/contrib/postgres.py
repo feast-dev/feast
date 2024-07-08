0d89d1519fc6b8ddd05a2588138e2e85f5a921b1
Tom Steenbergen <41334387+TomSteenbergen@users.noreply.github.com>
2024-07-08 12:27:39 +0200

    fix: Remove redundant batching in PostgreSQLOnlineStore.online_write_batch and fix progress bar (#4331)
    * Remove batching and fix tqdm progress bar
    
    Signed-off-by: TomSteenbergen <tomsteenbergen1995@gmail.com>
    
    * Comment
    
    Signed-off-by: TomSteenbergen <tomsteenbergen1995@gmail.com>
    
    * Remove test changes
    
    Signed-off-by: TomSteenbergen <tomsteenbergen1995@gmail.com>
    
    * Update comment
    
    Signed-off-by: TomSteenbergen <tomsteenbergen1995@gmail.com>
    
    ---------
    
    Signed-off-by: TomSteenbergen <tomsteenbergen1995@gmail.com>

--- sdk/python/feast/infra/online_stores/contrib/postgres.py
+++ sdk/python/feast/infra/online_stores/contrib/postgres.py
@@ -75,7 +75,6 @@ class PostgreSQLOnlineStore(OnlineStore):
             Tuple[EntityKeyProto, Dict[str, ValueProto], datetime, Optional[datetime]]
         ],
         progress: Optional[Callable[[int], Any]],
-        batch_size: int = 5000,
     ) -> None:
         # Format insert values
         insert_values = []
@@ -118,15 +117,13 @@ class PostgreSQLOnlineStore(OnlineStore):
         """
         ).format(sql.Identifier(_table_id(config.project, table)))
 
-        # Push data in batches to online store
+        # Push data into the online store
         with self._get_conn(config) as conn, conn.cursor() as cur:
-            for i in range(0, len(insert_values), batch_size):
-                cur_batch = insert_values[i : i + batch_size]
-                cur.executemany(sql_query, cur_batch)
-                conn.commit()
+            cur.executemany(sql_query, insert_values)
+            conn.commit()
 
-                if progress:
-                    progress(len(cur_batch))
+        if progress:
+            progress(len(data))
 
     def online_read(
         self,
