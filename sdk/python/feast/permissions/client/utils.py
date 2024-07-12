946db88052e8044c8806b81de42dd5898f4a2eec
Theodor Mihalache <tmihalac@redhat.com>
2024-07-12 12:07:39 -0400

    Client module-grpc - Made changes following code review
    Signed-off-by: Theodor Mihalache <tmihalac@redhat.com>
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

--- sdk/python/feast/permissions/client/utils.py
+++ sdk/python/feast/permissions/client/utils.py
@@ -1,3 +1,6 @@
+import pyarrow.flight as fl
+
+from feast.permissions.auth.auth_type import AuthType
 from feast.permissions.auth_model import AuthConfig
 from feast.permissions.client.auth_client_manager import get_auth_client_manager
 
@@ -9,3 +12,10 @@ def create_auth_header(
     token = auth_client_manager.get_token()
 
     return (("authorization", "Bearer " + token),)
+
+
+def create_flight_call_options(auth_config: AuthConfig) -> fl.FlightCallOptions:
+    if auth_config.type != AuthType.NONE.value:
+        headers = create_auth_header(auth_config)
+        return fl.FlightCallOptions(headers=headers)
+    return fl.FlightCallOptions()
