3ab7087bd3b5bc093766a08fedddca27a442b650
Abdul Hameed <ahameed@redhat.com>
2024-07-17 13:50:38 -0400

    Added support to read the token from enviroment variable to run from local
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

--- sdk/python/feast/permissions/client/kubernetes_auth_client_manager.py
+++ sdk/python/feast/permissions/client/kubernetes_auth_client_manager.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from feast.permissions.auth_model import KubernetesAuthConfig
 from feast.permissions.client.auth_client_manager import AuthenticationClientManager
@@ -9,14 +10,34 @@ logger = logging.getLogger(__name__)
 class KubernetesAuthClientManager(AuthenticationClientManager):
     def __init__(self, auth_config: KubernetesAuthConfig):
         self.auth_config = auth_config
+        self.token_file_path = "/var/run/secrets/kubernetes.io/serviceaccount/token"
 
     def get_token(self):
         try:
-            with open(
-                "/var/run/secrets/kubernetes.io/serviceaccount/token", "r"
-            ) as file:
+            token = self._read_token_from_file()
+            return token
+        except Exception as e:
+            logger.exception(f"Error reading token from file: {e}")
+            logger.info("Attempting to read token from environment variable.")
+            try:
+                token = self._read_token_from_env()
+                return token
+            except Exception as env_e:
+                logger.exception(
+                    f"Error reading token from environment variable: {env_e}"
+                )
+                raise env_e
+
+    def _read_token_from_file(self):
+        try:
+            with open(self.token_file_path, "r") as file:
                 token = file.read().strip()
             return token
         except Exception as e:
-            logger.exception(f"Error reading token: {e}")
             raise e
+
+    def _read_token_from_env(self):
+        token = os.getenv("LOCAL_K8S_TOKEN")
+        if not token:
+            raise FileNotFoundError("LOCAL_K8S_TOKEN environment variable is not set.")
+        return token
