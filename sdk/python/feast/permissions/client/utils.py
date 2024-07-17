f9d8e85d040d6d96865a9fac8aa4ace4023d859f
Abdul Hameed <ahameed@redhat.com>
2024-07-17 15:42:47 -0400

    Fix the header for arrow fligth
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

--- sdk/python/feast/permissions/client/utils.py
+++ sdk/python/feast/permissions/client/utils.py
@@ -15,7 +15,9 @@ def create_auth_header(
 
 
 def create_flight_call_options(auth_config: AuthConfig) -> fl.FlightCallOptions:
-    if auth_config.type != AuthType.NONE.value:
-        headers = create_auth_header(auth_config)
+    if auth_config.type != AuthType.NONE:
+        auth_client_manager = get_auth_client_manager(auth_config)
+        token = auth_client_manager.get_token()
+        headers = [(b"authorization", b"Bearer " + token.encode('utf-8'))]
         return fl.FlightCallOptions(headers=headers)
     return fl.FlightCallOptions()
