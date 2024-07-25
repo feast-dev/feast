cf46b6733719ee5a17eb4c8b5a828f525c5c78f6
lokeshrangineni <lokeshforjava@gmail.com>
2024-07-25 17:32:25 -0400

    Moved the common fixtures to the root conftest.py or auth_permissions_util.py  (#54)
    * Moved the common fixtures to the root conftest.py or auth_permissions_util.py
    
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    
    * Adding missed dependency and regenerated the requirements files.
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    
    * Addinig missing changes from the original PR.
    
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    
    ---------
    
    Signed-off-by: Lokesh Rangineni <lokeshforjava@gmail.com>
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

---
+++ sdk/python/tests/integration/permissions/test_auth_permission.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+@pytest.mark.integration
+def test_oidc_auth_permissions(start_keycloak_server, temp_dir):
+    print("starting the test")
+    ## TODO:
+    # 1 create the store
+    # 2 create the permissions
+    # 3 Spin up feature server
+    # 4 test remote online read with no auth and oidc auth.
+    # 5 spin up the registry server and test the no auth and oidc auth on all end points.
