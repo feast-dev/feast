73070ca3ef5c6fabad2891fff3edecedb8cfa315
Daniele Martinoli <dmartino@redhat.com>
2024-08-29 14:38:27 +0200

    fixed merge error
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- sdk/python/feast/permissions/server/grpc.py
+++ sdk/python/feast/permissions/server/grpc.py
@@ -3,7 +3,6 @@ import logging
 
 import grpc
 
-from feast.errors import FeastObjectNotFoundException, to_error_detail
 from feast.permissions.auth.auth_manager import (
     get_auth_manager,
 )
@@ -31,55 +30,3 @@ class AuthInterceptor(grpc.ServerInterceptor):
             sm.set_current_user(current_user)
 
         return continuation(handler_call_details)
-
-
-class ErrorInterceptor(grpc.ServerInterceptor):
-    def intercept_service(self, continuation, handler_call_details):
-        def exception_wrapper(behavior, request, context):
-            try:
-                return behavior(request, context)
-            except grpc.RpcError as e:
-                context.abort(e.code(), e.details())
-            except Exception as e:
-                context.abort(
-                    _error_to_status_code(e),
-                    to_error_detail(e),
-                )
-
-        handler = continuation(handler_call_details)
-        if handler is None:
-            return None
-
-        if handler.unary_unary:
-            return grpc.unary_unary_rpc_method_handler(
-                lambda req, ctx: exception_wrapper(handler.unary_unary, req, ctx),
-                request_deserializer=handler.request_deserializer,
-                response_serializer=handler.response_serializer,
-            )
-        elif handler.unary_stream:
-            return grpc.unary_stream_rpc_method_handler(
-                lambda req, ctx: exception_wrapper(handler.unary_stream, req, ctx),
-                request_deserializer=handler.request_deserializer,
-                response_serializer=handler.response_serializer,
-            )
-        elif handler.stream_unary:
-            return grpc.stream_unary_rpc_method_handler(
-                lambda req, ctx: exception_wrapper(handler.stream_unary, req, ctx),
-                request_deserializer=handler.request_deserializer,
-                response_serializer=handler.response_serializer,
-            )
-        elif handler.stream_stream:
-            return grpc.stream_stream_rpc_method_handler(
-                lambda req, ctx: exception_wrapper(handler.stream_stream, req, ctx),
-                request_deserializer=handler.request_deserializer,
-                response_serializer=handler.response_serializer,
-            )
-        return handler
-
-
-def _error_to_status_code(error: Exception) -> grpc.StatusCode:
-    if isinstance(error, FeastObjectNotFoundException):
-        return grpc.StatusCode.NOT_FOUND
-    if isinstance(error, FeastObjectNotFoundException):
-        return grpc.StatusCode.PERMISSION_DENIED
-    return grpc.StatusCode.INTERNAL
