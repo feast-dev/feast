3a33368af59a2fb3931b3f91b8b1c0b090732125
Francisco Javier Arceo <farceo@redhat.com>
2024-09-24 23:30:00 -0400

    removed print
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- sdk/python/feast/infra/passthrough_provider.py
+++ sdk/python/feast/infra/passthrough_provider.py
@@ -286,8 +286,6 @@ class PassthroughProvider(Provider):
                 for entity in feature_view.entity_columns
             }
             rows_to_write = _convert_arrow_to_proto(table, feature_view, join_keys)
-            print(rows_to_write)
-            # List[Tuple[EntityKeyProto, Dict[str, ValueProto], datetime, Optional[datetime]]]
 
             self.online_write_batch(
                 self.repo_config, feature_view, rows_to_write, progress=None
