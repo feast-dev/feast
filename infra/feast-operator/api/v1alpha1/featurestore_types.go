66a77a05e7acb0fac5a6014758032533b92c10e5
Tommy Hughes <tohughes@redhat.com>
2024-10-01 15:51:35 -0500

    add spec.feastProject field
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- infra/feast-operator/api/v1alpha1/featurestore_types.go
+++ infra/feast-operator/api/v1alpha1/featurestore_types.go
@@ -28,8 +28,8 @@ type FeatureStoreSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of FeatureStore. Edit featurestore_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// FeastProject ...
+	FeastProject string `json:"feastProject"`
 }
 
 // FeatureStoreStatus defines the observed state of FeatureStore
