1e61591de451d7af4aa3517cdc379906f1f3dac8
hkuepers <hanno.kuepers@ratepay.com>
2024-10-04 13:17:50 +0200

    Add connection_name field to Snowflake config
    Signed-off-by: hkuepers <hanno.kuepers@ratepay.com>

--- sdk/python/feast/infra/materialization/snowflake_engine.py
+++ sdk/python/feast/infra/materialization/snowflake_engine.py
@@ -46,7 +46,10 @@ class SnowflakeMaterializationEngineConfig(FeastConfigBaseModel):
     """ Type selector"""
 
     config_path: Optional[str] = os.path.expanduser("~/.snowsql/config")
-    """ Snowflake config path -- absolute path required (Cant use ~)"""
+    """ Snowflake snowsql config path -- absolute path required (Cant use ~)"""
+
+    connection_name: Optional[str] = None
+    """ Snowflake connector connection name -- typically defined in ~/.snowflake/connections.toml """
 
     account: Optional[str] = None
     """ Snowflake deployment identifier -- drop .snowflakecomputing.com"""
