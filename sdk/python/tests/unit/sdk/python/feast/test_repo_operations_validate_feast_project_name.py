945b0faadd40c8dc76d104bce14ee902bd513127
lokeshrangineni <19699092+lokeshrangineni@users.noreply.github.com>
2024-10-14 16:12:04 -0400

    fix: Updating the documentation and adding tests for project length (#4628)
    Updated the documentation to make it clear after spending some time.
     Addressing the issue - https://github.com/feast-dev/feast/issues/4626
    
    Signed-off-by: lrangine <19699092+lokeshrangineni@users.noreply.github.com>

---
+++ sdk/python/tests/unit/sdk/python/feast/test_repo_operations_validate_feast_project_name.py
@@ -0,0 +1,26 @@
+from sdk.python.feast.repo_operations import is_valid_name
+
+
+def test_is_valid_name():
+    test_cases = [
+        # Valid project name cases
+        ("valid_name1", True),
+        ("username_1234", True),
+        ("valid123", True),
+        ("1234567890123456", True),
+        ("invalid_name_", True),
+        ("12345678901234567", True),
+        ("too_long_name_123", True),
+        # Invalid project name cases
+        ("_invalidName", False),
+        ("invalid-Name", False),
+        ("invalid name", False),
+        ("invalid@name", False),
+        ("invalid$name", False),
+        ("__", False),
+    ]
+
+    for name, expected in test_cases:
+        assert (
+            is_valid_name(name) == expected
+        ), f"Failed for project invalid name: {name}"
