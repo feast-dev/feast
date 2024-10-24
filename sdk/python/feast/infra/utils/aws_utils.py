7991146830f72c5abd9f8767ab6fb21812668db6
Rob Howley <howley.robert@gmail.com>
2024-10-24 09:19:41 -0400

    add async writer for dynamo
    Signed-off-by: Rob Howley <howley.robert@gmail.com>

--- sdk/python/feast/infra/utils/aws_utils.py
+++ sdk/python/feast/infra/utils/aws_utils.py
@@ -1,4 +1,6 @@
+import asyncio
 import contextlib
+import itertools
 import os
 import tempfile
 import uuid
@@ -10,6 +12,7 @@ import pyarrow
 import pyarrow as pa
 import pyarrow.parquet as pq
 from tenacity import (
+    AsyncRetrying,
     retry,
     retry_if_exception_type,
     stop_after_attempt,
@@ -1076,3 +1079,54 @@ def upload_arrow_table_to_athena(
         # Clean up S3 temporary data
         # for file_path in uploaded_files:
         #     s3_resource.Object(bucket, file_path).delete()
+
+
+class DynamoUnprocessedWriteItems(Exception):
+    pass
+
+
+async def dynamo_write_items_async(
+    dynamo_client, table_name: str, items: list[Any]
+) -> None:
+    DYNAMO_MAX_WRITE_BATCH_SIZE = 25
+
+    async def _do_write(items):
+        item_iter = iter(items)
+        item_batches = []
+        while True:
+            item_batch = [
+                item
+                for item in itertools.islice(item_iter, DYNAMO_MAX_WRITE_BATCH_SIZE)
+            ]
+            if not item_batch:
+                break
+
+            item_batches.append(item_batch)
+
+        return await asyncio.gather(
+            *[
+                dynamo_client.batch_write_item(
+                    RequestItems={table_name: item_batch},
+                )
+                for item_batch in item_batches
+            ]
+        )
+
+    put_items = [{"PutRequest": {"Item": item}} for item in items]
+
+    retries = AsyncRetrying(
+        retry=retry_if_exception_type(DynamoUnprocessedWriteItems),
+        wait=wait_exponential(multiplier=1, max=4),
+        reraise=True,
+    )
+
+    for attempt in retries:
+        with attempt:
+            response_batches = await _do_write(put_items)
+
+            put_items = []
+            for response in response_batches:
+                put_items.extend(response["UnprocessedItems"])
+
+            if put_items:
+                raise DynamoUnprocessedWriteItems()
--- sdk/python/tests/integration/online_store/test_push_features_to_online_store.py
+++ sdk/python/tests/integration/online_store/test_push_features_to_online_store.py
@@ -8,29 +8,51 @@ from tests.integration.feature_repos.repo_configuration import (
 from tests.integration.feature_repos.universal.entities import location
 
 
-@pytest.mark.integration
-@pytest.mark.universal_online_stores
-def test_push_features_and_read(environment, universal_data_sources):
+@pytest.fixture
+def store(environment, universal_data_sources):
     store = environment.feature_store
     _, _, data_sources = universal_data_sources
     feature_views = construct_universal_feature_views(data_sources)
     location_fv = feature_views.pushed_locations
     store.apply([location(), location_fv])
+    return store
 
+
+def _ingest_df():
     data = {
         "location_id": [1],
         "temperature": [4],
         "event_timestamp": [pd.Timestamp(_utc_now()).round("ms")],
         "created": [pd.Timestamp(_utc_now()).round("ms")],
     }
-    df_ingest = pd.DataFrame(data)
+    return pd.DataFrame(data)
 
-    store.push("location_stats_push_source", df_ingest)
+
+def assert_response(online_resp):
+    online_resp_dict = online_resp.to_dict()
+    assert online_resp_dict["location_id"] == [1]
+    assert online_resp_dict["temperature"] == [4]
+
+
+@pytest.mark.integration
+@pytest.mark.universal_online_stores
+def test_push_features_and_read(store):
+    store.push("location_stats_push_source", _ingest_df())
 
     online_resp = store.get_online_features(
         features=["pushable_location_stats:temperature"],
         entity_rows=[{"location_id": 1}],
     )
-    online_resp_dict = online_resp.to_dict()
-    assert online_resp_dict["location_id"] == [1]
-    assert online_resp_dict["temperature"] == [4]
+    assert_response(online_resp)
+
+
+@pytest.mark.integration
+@pytest.mark.universal_online_stores(only=["dynamodb"])
+async def test_push_features_and_read_async(store):
+    await store.push_async("location_stats_push_source", _ingest_df())
+
+    online_resp = await store.get_online_features_async(
+        features=["pushable_location_stats:temperature"],
+        entity_rows=[{"location_id": 1}],
+    )
+    assert_response(online_resp)
