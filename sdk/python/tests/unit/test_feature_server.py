d96ad8fdbf959ef29f3c39589085b7cc318371a4
Rob Howley <howley.robert@gmail.com>
2024-10-25 11:23:45 -0400

    make feature server tests unit tests, remove integration
    Signed-off-by: Rob Howley <howley.robert@gmail.com>

--- sdk/python/tests/unit/test_feature_server.py
+++ sdk/python/tests/unit/test_feature_server.py
@@ -1,14 +1,102 @@
 import json
-from unittest.mock import AsyncMock, MagicMock, patch
+from unittest.mock import AsyncMock, MagicMock
 
 import pytest
 from fastapi.testclient import TestClient
+from pytest_lazyfixture import lazy_fixture
 
-from feast import FeatureStore
 from feast.data_source import PushMode
+from feast.errors import PushSourceNotFoundException
 from feast.feature_server import get_app
 from feast.utils import _utc_now
 from tests.foo_provider import FooProvider
+
+
+@pytest.fixture
+def mock_fs_factory():
+    def builder(**async_support):
+        provider = FooProvider.with_async_support(**async_support)
+        fs = MagicMock()
+        fs._get_provider.return_value = provider
+        fs.get_online_features = MagicMock()
+        fs.push = MagicMock()
+        fs.get_online_features_async = AsyncMock()
+        fs.push_async = AsyncMock()
+        return fs
+
+    return builder
+
+
+def get_online_features_body():
+    return {
+        "features": [
+            "driver_stats:conv_rate",
+            "driver_stats:acc_rate",
+            "driver_stats:avg_daily_trips",
+        ],
+        "entities": {"driver_id": [5001, 5002]},
+    }
+
+
+def push_body(push_mode=PushMode.ONLINE, temp=100):
+    return {
+        "push_source_name": "location_stats_push_source",
+        "df": {
+            "location_id": [1],
+            "temperature": [temp],
+            "event_timestamp": [str(_utc_now())],
+            "created": [str(_utc_now())],
+        },
+        "to": push_mode.name.lower(),
+    }
+
+
+@pytest.mark.parametrize(
+    "async_online_read",
+    [
+        (True,),
+        (False,),
+    ],
+)
+def test_get_online_features_async_supported(async_online_read, mock_fs_factory):
+    fs = mock_fs_factory(online_read=async_online_read)
+    client = TestClient(get_app(fs))
+    client.post("/get-online-features", data=get_online_features_body())
+    assert fs.get_online_features.assert_called() == (not async_online_read)
+    assert fs.get_online_features_async.await_count == async_online_read
+
+
+@pytest.mark.parametrize(
+    "test_feature_store",
+    [lazy_fixture("feature_store_with_local_registry")],
+)
+def test_get_online_features(test_feature_store):
+    request_payload = get_online_features_body()
+
+    client = TestClient(get_app(test_feature_store))
+    response = client.post("/get-online-features", data=request_payload)
+
+    # Check entities and features are present
+    parsed_response = json.loads(response.text)
+    assert "metadata" in parsed_response
+    metadata = parsed_response["metadata"]
+    expected_features = ["driver_id", "conv_rate", "acc_rate", "avg_daily_trips"]
+    response_feature_names = metadata["feature_names"]
+    assert len(response_feature_names) == len(expected_features)
+    for expected_feature in expected_features:
+        assert expected_feature in response_feature_names
+    assert "results" in parsed_response
+    results = parsed_response["results"]
+    for result in results:
+        # Same order as in metadata
+        assert len(result["statuses"]) == 2  # Requested two entities
+        for status in result["statuses"]:
+            assert status == "PRESENT"
+    results_driver_id_index = response_feature_names.index("driver_id")
+    assert (
+        results[results_driver_id_index]["values"]
+        == request_payload["entities"]["driver_id"]
+    )
 
 
 @pytest.mark.parametrize(
@@ -22,26 +110,56 @@ from tests.foo_provider import FooProvider
         (False, PushMode.ONLINE, 0),
     ],
 )
-def test_push_online_async_supported(online_write, push_mode, async_count, environment):
-    push_payload = json.dumps(
-        {
-            "push_source_name": "location_stats_push_source",
-            "df": {
-                "location_id": [1],
-                "temperature": [100],
-                "event_timestamp": [str(_utc_now())],
-                "created": [str(_utc_now())],
-            },
-            "to": push_mode.name.lower(),
-        }
-    )
+def test_push_online_async_supported(
+    online_write, push_mode, async_count, mock_fs_factory
+):
+    fs = mock_fs_factory(online_write=online_write)
+    client = TestClient(get_app(fs))
+    client.post("/push", data=push_body(push_mode))
+    assert fs.push.call_count == 1 - async_count
+    assert fs.push_async.await_count == async_count
 
-    provider = FooProvider.with_async_support(online_write=online_write)
-    with patch.object(FeatureStore, "_get_provider", return_value=provider):
-        fs = environment.feature_store
-        fs.push = MagicMock()
-        fs.push_async = AsyncMock()
-        client = TestClient(get_app(fs))
-        client.post("/push", data=push_payload)
-        assert fs.push.call_count == 1 - async_count
-        assert fs.push_async.await_count == async_count
+
+@pytest.mark.parametrize(
+    "test_feature_store",
+    [lazy_fixture("feature_store_with_local_registry")],
+)
+async def test_push(test_feature_store):
+    request_payload = push_body(temp=55)
+    client = TestClient(get_app(test_feature_store))
+    response = client.post("/push", data=request_payload)
+
+    # Check new pushed temperature is fetched
+    assert response.status_code == 200
+    actual = client.post(
+        "/get-online-features",
+        data={
+            "features": ["pushable_location_stats:temperature"],
+            "entities": {"location_id": request_payload["df"]["location_id"]},
+        },
+    )
+    assert actual == request_payload["df"]["temperature"]
+
+
+@pytest.mark.parametrize(
+    "test_feature_store",
+    [lazy_fixture("feature_store_with_local_registry")],
+)
+def test_push_source_does_not_exist(test_feature_store):
+    with pytest.raises(
+        PushSourceNotFoundException,
+        match="Unable to find push source 'push_source_does_not_exist'",
+    ):
+        client = TestClient(get_app(test_feature_store))
+        client.post(
+            "/push",
+            data={
+                "push_source_name": "push_source_does_not_exist",
+                "df": {
+                    "location_id": [1],
+                    "temperature": [100],
+                    "event_timestamp": [str(_utc_now())],
+                    "created": [str(_utc_now())],
+                },
+            },
+        )
