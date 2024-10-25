bf1d6c8bdc51c1e194ef0991d293b9323dfb5b8f
Rob Howley <howley.robert@gmail.com>
2024-10-24 23:43:47 -0400

    missed the asyncio.run, remove it
    Signed-off-by: Rob Howley <howley.robert@gmail.com>

--- sdk/python/tests/integration/online_store/test_universal_online.py
+++ sdk/python/tests/integration/online_store/test_universal_online.py
@@ -496,15 +496,13 @@ async def test_async_online_retrieval_with_event_timestamps(
     )
     await fs.initialize()
 
-    response = asyncio.run(
-        fs.get_online_features_async(
-            features=[
-                "driver_stats:avg_daily_trips",
-                "driver_stats:acc_rate",
-                "driver_stats:conv_rate",
-            ],
-            entity_rows=[{"driver_id": 1}, {"driver_id": 2}],
-        )
+    response = await fs.get_online_features_async(
+        features=[
+            "driver_stats:avg_daily_trips",
+            "driver_stats:acc_rate",
+            "driver_stats:conv_rate",
+        ],
+        entity_rows=[{"driver_id": 1}, {"driver_id": 2}],
     )
     df = response.to_df(True)
 
