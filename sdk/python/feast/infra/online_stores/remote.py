7c054f921bbac0759841bfeba652e142af242a0a
lrangine <19699092+lokeshrangineni@users.noreply.github.com>
2024-10-25 10:15:14 -0400

    * Adding the SSL support for online_server. * Adding the SSL support for remote online client. * Adding the integration test to run the remote online server in SSL and non SSL mode. * Incorporated code review comments
    Signed-off-by: lrangine <19699092+lokeshrangineni@users.noreply.github.com>
    
    incorporating code review comments.
    
    Signed-off-by: lrangine <19699092+lokeshrangineni@users.noreply.github.com>

--- sdk/python/feast/infra/online_stores/remote.py
+++ sdk/python/feast/infra/online_stores/remote.py
@@ -41,6 +41,10 @@ class RemoteOnlineStoreConfig(FeastConfigBaseModel):
     """ str: Path to metadata store.
     If type is 'remote', then this is a URL for registry server """
 
+    ssl_cert_path: StrictStr = ""
+    """ str: Path to the public certificate when the online server starts in SSL mode. This may be needed if the online server started with a self-signed certificate, typically this file ends with `*.crt`, `*.cer`, or `*.pem`.
+    If type is 'remote', then this configuration is needed to connect to remote online server in SSL mode. """
+
 
 class RemoteOnlineStore(OnlineStore):
     """
@@ -170,6 +174,13 @@ class RemoteOnlineStore(OnlineStore):
 def get_remote_online_features(
     session: requests.Session, config: RepoConfig, req_body: str
 ) -> requests.Response:
-    return session.post(
-        f"{config.online_store.path}/get-online-features", data=req_body
-    )
+    if config.online_store.ssl_cert_path:
+        return session.post(
+            f"{config.online_store.path}/get-online-features",
+            data=req_body,
+            verify=config.online_store.ssl_cert_path,
+        )
+    else:
+        return session.post(
+            f"{config.online_store.path}/get-online-features", data=req_body
+        )
