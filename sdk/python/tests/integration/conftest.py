c349c7bea200b45fa2707802a4292c3a70ec22b4
Daniele Martinoli <dmartino@redhat.com>
2024-10-29 10:03:26 +0100

    Adding explicit wait to container to validate CI failures
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- sdk/python/tests/integration/conftest.py
+++ sdk/python/tests/integration/conftest.py
@@ -1,6 +1,7 @@
 import logging
 
 import pytest
+from testcontainers.core.waiting_utils import wait_for_logs
 from testcontainers.keycloak import KeycloakContainer
 from testcontainers.minio import MinioContainer
 from testcontainers.mysql import MySqlContainer
@@ -15,6 +16,15 @@ logger = logging.getLogger(__name__)
 def start_keycloak_server():
     logger.info("Starting keycloak instance")
     with KeycloakContainer("quay.io/keycloak/keycloak:24.0.1") as keycloak_container:
+        log_string_to_wait_for = "Running the server in development mode"
+        waited = wait_for_logs(
+            container=keycloak_container,
+            predicate=log_string_to_wait_for,
+            timeout=30,
+            interval=10,
+        )
+        logger.info("Waited for %s seconds until keycloak container was up", waited)
+
         setup_permissions_on_keycloak(keycloak_container.get_client())
         yield keycloak_container.get_url()
 
@@ -24,19 +34,61 @@ def mysql_server():
     container = MySqlContainer("mysql:latest")
     container.start()
 
-    yield container
+    def wait_for_sql_query(container, query, timeout=30):
+        from time import sleep, time
 
-    container.stop()
+        start_time = time()
+
+        while time() - start_time < timeout:
+            command = f"mysql -uroot -p{container.root_password} -e '{query};'"
+            logger.info(f"Trying {query} command {command}")
+            try:
+                container.exec(command)
+                logger.info(f"Completed in {time() - start_time}s")
+                return  # Success, exit the function
+            except Exception:
+                logger.info(f"Retry in {1}s")
+                sleep(1)  # Wait and retry
+
+        raise Exception(f"Timeout waiting for SQL query: {query}")
+
+    wait_for_sql_query(container, "SELECT 10")
+
+    yield container
 
 
 @pytest.fixture(scope="session")
 def postgres_server():
     container = PostgresContainer()
+    logger.info(f"Starting container {container._name} from {container.image}")
     container.start()
 
-    yield container
+    def wait_for_sql_query(connection_url, query, timeout=30):
+        from time import sleep, time
 
-    container.stop()
+        import psycopg
+
+        start_time = time()
+        connection_url = connection_url.replace("+psycopg2", "")
+
+        while time() - start_time < timeout:
+            logger.info(f"Trying {query} from {connection_url}")
+            try:
+                conn = psycopg.connect(connection_url)
+                cur = conn.cursor()
+                cur.execute(query)
+                cur.close()
+                conn.close()
+                logger.info(f"Completed in {time() - start_time}s")
+                return  # Success, exit the function
+            except psycopg.Error:
+                logger.info(f"Retry in {1}s")
+                sleep(1)  # Wait and retry
+
+        raise Exception(f"Timeout waiting for SQL query: {query}")
+
+    wait_for_sql_query(container.get_connection_url(), "SELECT 10")
+    yield container
 
 
 @pytest.fixture(scope="session")
