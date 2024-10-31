9d9d24a90ee5e153739a7a4df3e937442e21cb9d
Tomasz Wrona <tomasz@cast.ai>
2024-10-31 11:46:55 +0100

    Fix frozen mypy errors
    Signed-off-by: Tomasz Wrona <tomasz@cast.ai>

--- sdk/python/feast/infra/utils/clickhouse/clickhouse_config.py
+++ sdk/python/feast/infra/utils/clickhouse/clickhouse_config.py
@@ -1,12 +1,14 @@
-from pydantic import StrictStr
+from pydantic import StrictStr, ConfigDict
 
 from feast.repo_config import FeastConfigBaseModel
 
 
-class ClickhouseConfig(FeastConfigBaseModel, frozen=True):
+class ClickhouseConfig(FeastConfigBaseModel):
     host: StrictStr
     port: int = 8123
     database: StrictStr
     user: StrictStr
     password: StrictStr
     use_temporary_tables_for_entity_df: bool = True
+
+    model_config = ConfigDict(frozen=True)
