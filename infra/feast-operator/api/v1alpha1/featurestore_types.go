c5a5d24e1625583573e0e98bc0a43912b134bf78
Daniele Martinoli <dmartino@redhat.com>
2024-10-31 17:20:19 +0100

    Initial commit: persistence defs and validation tests
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- infra/feast-operator/api/v1alpha1/featurestore_types.go
+++ infra/feast-operator/api/v1alpha1/featurestore_types.go
@@ -17,6 +17,8 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -51,6 +53,121 @@ type FeatureStoreSpec struct {
 	// +kubebuilder:validation:Pattern="^[A-Za-z0-9][A-Za-z0-9_]*$"
 	// FeastProject is the Feast project id. This can be any alphanumeric string with underscores, but it cannot start with an underscore. Required.
 	FeastProject string `json:"feastProject"`
+	// +optional
+	Services *Services `json:"services"`
+}
+
+type Services struct {
+	// +optional
+	OnlineStore *OnlineStore `json:"onlineStore,omitempty"`
+	// +optional
+	OfflineStore *OfflineStore `json:"offlineStore,omitempty"`
+	// +optional
+	Registry *Registry `json:"registry,omitempty"`
+}
+type OnlineStore struct {
+	// +optional
+	Persistence *OnlineStorePersistence `json:"persistence,omitempty"`
+}
+type OfflineStore struct {
+	// +optional
+	Persistence *OfflineStorePersistence `json:"persistence,omitempty"`
+}
+type Registry struct {
+	// +optional
+	Persistence *RegistryPersistence `json:"persistence,omitempty"`
+}
+
+// +kubebuilder:validation:XValidation:rule="!(has(self.file) && has(self.store))",message="One file or store persistence is allowed."
+// +kubebuilder:validation:XValidation:rule="has(self.file) || has(self.store)", message="Either file or store persistence are required."
+type OnlineStorePersistence struct {
+	// +optional
+	FilePersistence *OnlineStoreFilePersistence `json:"file,omitempty"`
+	// +optional
+	StorePersistence *OnlineStoreStorePersistence `json:"store,omitempty"`
+}
+
+// +kubebuilder:validation:XValidation:rule="!(has(self.file) && has(self.store))",message="One file or store persistence is allowed."
+// +kubebuilder:validation:XValidation:rule="has(self.file) || has(self.store)", message="Either file or store persistence are required."
+type OfflineStorePersistence struct {
+	// +optional
+	FilePersistence *OfflineStoreFilePersistence `json:"file,omitempty"`
+	// +optional
+	StorePersistence *OfflineStoreStorePersistence `json:"store,omitempty"`
+}
+
+// +kubebuilder:validation:XValidation:rule="!(has(self.file) && has(self.store))",message="One file or store persistence is allowed."
+// +kubebuilder:validation:XValidation:rule="has(self.file) || has(self.store)", message="Either file or store persistence are required."
+type RegistryPersistence struct {
+	// +optional
+	FilePersistence *RegistryFilePersistence `json:"file,omitempty"`
+	// +optional
+	StorePersistence *RegistryStorePersistence `json:"store,omitempty"`
+}
+
+// +kubebuilder:validation:XValidation:rule="!has(self.pvc) ? self.path.startsWith('/') : true",message="Ephemeral stores must have absolute paths."
+// +kubebuilder:validation:XValidation:rule="has(self.pvc) ? !self.path.startsWith('/') : true",message="PVC path must be a file name only, with no slashes."
+// +kubebuilder:validation:XValidation:rule="!self.path.startsWith('s3://') && !self.path.startsWith('gs://')",message="Online store does not support S3 or GS buckets."
+type OnlineStoreFilePersistence struct {
+	// +optional
+	Path string `json:"path,omitempty"`
+	// +optional
+	PvcStore *PvcStore `json:"pvc,omitempty"`
+}
+type OfflineStoreFilePersistence struct {
+	// +optional
+	// +default:value=dask
+	// +kubebuilder:validation:Enum=dask;duckdb
+	Type string `json:"type,omitempty"`
+	// +optional
+	PvcStore *PvcStore `json:"pvc,omitempty"`
+}
+
+// +kubebuilder:validation:XValidation:rule="!has(self.pvc) ? (self.path.startsWith('/') || self.path.startsWith('s3://') || self.path.startsWith('gs://')) : true",message="Ephemeral stores must have absolute paths."
+// +kubebuilder:validation:XValidation:rule="has(self.pvc) ? !self.path.startsWith('/') : true",message="PVC path must be a file name only, with no slashes."
+// +kubebuilder:validation:XValidation:rule="has(self.pvc) ? !(self.path.startsWith('s3://') || self.path.startsWith('gs://')) : true",message="PVC persistence does not support S3 or GS buckets."
+// +kubebuilder:validation:XValidation:rule="has(self.s3_additional_kwargs) ? self.path.startsWith('s3://') : true",message="Additional S3 settings are available only for S3 buckets."
+type RegistryFilePersistence struct {
+	// +optional
+	Path string `json:"path,omitempty"`
+	// +optional
+	PvcStore *PvcStore `json:"pvc,omitempty"`
+	// +optional
+	S3AddtlKwargs *map[string]string `json:"s3_additional_kwargs,omitempty"`
+}
+type OnlineStoreStorePersistence struct {
+	// +kubebuilder:validation:Enum=snowflake;redis;ikv;datastore;dynamodb;bigtable;postgres;cassandra;mysql;hazelcast;singlestore
+	Type string `json:"type"`
+	// +optional
+	SecretRef corev1.LocalObjectReference `json:"secretRef"`
+}
+type OfflineStoreStorePersistence struct {
+	// +kubebuilder:validation:Enum=snowflake;bigquery;redshift;spark;postgres;trino;mssql
+	Type string `json:"type"`
+	// +optional
+	SecretRef corev1.LocalObjectReference `json:"secretRef"`
+}
+type RegistryStorePersistence struct {
+	// +kubebuilder:validation:Enum=sql;snowflake
+	Type string `json:"type"`
+	// +optional
+	SecretRef corev1.LocalObjectReference `json:"secretRef"`
+}
+
+type PvcStore struct {
+	// The name of the PersistenceVolumeClaim to be created, or reused it is already there
+	Name string `json:"name,omitempty"`
+	// Name of the StorageClass required by the claim.
+	// Only for newly created PVC.
+	// +optional
+	StorageClassName *string `json:"storageClassName,omitempty"`
+	// Default value depend on Feast service.
+	// Only for newly created PVC.
+	// +optional
+	Storage *resource.Quantity `json:"storage,omitempty"`
+	// Default path depend on Feast service.
+	// +optional
+	MountPath *string `json:"mountPath,omitempty"`
 }
 
 // FeatureStoreStatus defines the observed state of FeatureStore
---
+++ infra/feast-operator/test/api/featurestore_types_test.go
@@ -0,0 +1,416 @@
+package api
+
+import (
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	feastdevv1alpha1 "github.com/feast-dev/feast/infra/feast-operator/api/v1alpha1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Function to create invalid OnlineStore resource
+func createFeatureStore() *feastdevv1alpha1.FeatureStore {
+	return &feastdevv1alpha1.FeatureStore{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      resourceName,
+			Namespace: namespaceName,
+		},
+		Spec: feastdevv1alpha1.FeatureStoreSpec{
+			FeastProject: "test_project",
+		},
+	}
+}
+
+func attemptInvalidCreationAndAsserts(ctx context.Context, featurestore *feastdevv1alpha1.FeatureStore, matcher string) {
+	By("Creating the resource")
+	logger := log.FromContext(ctx)
+	logger.Info("Creating", "FeatureStore", featurestore)
+	err := k8sClient.Create(ctx, featurestore)
+	logger.Info("Got", "err", err)
+	Expect(err).ToNot(BeNil())
+	Expect(err.Error()).Should(ContainSubstring(matcher))
+}
+
+func onlineStoreWithBothFileAndStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OnlineStore: &feastdevv1alpha1.OnlineStore{
+			Persistence: &feastdevv1alpha1.OnlineStorePersistence{
+				FilePersistence: &feastdevv1alpha1.OnlineStoreFilePersistence{
+					Path: "/data/online_store.db",
+				},
+				StorePersistence: &feastdevv1alpha1.OnlineStoreStorePersistence{
+					Type: "postgres",
+				},
+			},
+		},
+	}
+	return copy
+}
+func onlineStoreWithNeitherFileNorStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OnlineStore: &feastdevv1alpha1.OnlineStore{
+			Persistence: &feastdevv1alpha1.OnlineStorePersistence{},
+		},
+	}
+	return copy
+}
+func onlineStoreWithUnmanagedStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OnlineStore: &feastdevv1alpha1.OnlineStore{
+			Persistence: &feastdevv1alpha1.OnlineStorePersistence{
+				StorePersistence: &feastdevv1alpha1.OnlineStoreStorePersistence{
+					Type: "unmanaged",
+				},
+			},
+		},
+	}
+	return copy
+}
+
+func onlineStoreWithAbsolutePathForPvc(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OnlineStore: &feastdevv1alpha1.OnlineStore{
+			Persistence: &feastdevv1alpha1.OnlineStorePersistence{
+				FilePersistence: &feastdevv1alpha1.OnlineStoreFilePersistence{
+					Path: "/data/online_store.db",
+					PvcStore: &feastdevv1alpha1.PvcStore{
+						Name: "example-pvc",
+					},
+				},
+			},
+		},
+	}
+	return copy
+}
+func onlineStoreWithS3BucketForPvc(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OnlineStore: &feastdevv1alpha1.OnlineStore{
+			Persistence: &feastdevv1alpha1.OnlineStorePersistence{
+				FilePersistence: &feastdevv1alpha1.OnlineStoreFilePersistence{
+					Path: "s3://bucket/online_store.db",
+					PvcStore: &feastdevv1alpha1.PvcStore{
+						Name: "example-pvc",
+					},
+				},
+			},
+		},
+	}
+	return copy
+}
+func onlineStoreWithGsBucketForPvc(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OnlineStore: &feastdevv1alpha1.OnlineStore{
+			Persistence: &feastdevv1alpha1.OnlineStorePersistence{
+				FilePersistence: &feastdevv1alpha1.OnlineStoreFilePersistence{
+					Path: "gs://bucket/online_store.db",
+					PvcStore: &feastdevv1alpha1.PvcStore{
+						Name: "example-pvc",
+					},
+				},
+			},
+		},
+	}
+	return copy
+}
+func onlineStoreWithRelativePathForEphemeral(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OnlineStore: &feastdevv1alpha1.OnlineStore{
+			Persistence: &feastdevv1alpha1.OnlineStorePersistence{
+				FilePersistence: &feastdevv1alpha1.OnlineStoreFilePersistence{
+					Path: "data/online_store.db",
+				},
+			},
+		},
+	}
+	return copy
+}
+
+func offlineStoreWithBothFileAndStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OfflineStore: &feastdevv1alpha1.OfflineStore{
+			Persistence: &feastdevv1alpha1.OfflineStorePersistence{
+				FilePersistence: &feastdevv1alpha1.OfflineStoreFilePersistence{},
+				StorePersistence: &feastdevv1alpha1.OfflineStoreStorePersistence{
+					Type: "postgres",
+				},
+			},
+		},
+	}
+	return copy
+}
+func offlineStoreWithNeitherFileNorStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OfflineStore: &feastdevv1alpha1.OfflineStore{
+			Persistence: &feastdevv1alpha1.OfflineStorePersistence{},
+		},
+	}
+	return copy
+}
+func offlineStoreWithUnmanagedStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OfflineStore: &feastdevv1alpha1.OfflineStore{
+			Persistence: &feastdevv1alpha1.OfflineStorePersistence{
+				StorePersistence: &feastdevv1alpha1.OfflineStoreStorePersistence{
+					Type: "unmanaged",
+				},
+			},
+		},
+	}
+	return copy
+}
+func offlineStoreWithUnmanagedFileType(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		OfflineStore: &feastdevv1alpha1.OfflineStore{
+			Persistence: &feastdevv1alpha1.OfflineStorePersistence{
+				FilePersistence: &feastdevv1alpha1.OfflineStoreFilePersistence{
+					Type: "unmanaged",
+				},
+			},
+		},
+	}
+	return copy
+}
+
+func registryWithBothFileAndStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				FilePersistence: &feastdevv1alpha1.RegistryFilePersistence{
+					Path: "/data/online_store.db",
+				},
+				StorePersistence: &feastdevv1alpha1.RegistryStorePersistence{
+					Type: "sql",
+				},
+			},
+		},
+	}
+	return copy
+}
+func registryWithNeitherFileNorStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{},
+		},
+	}
+	return copy
+}
+func registryWithAbsolutePathForPvc(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				FilePersistence: &feastdevv1alpha1.RegistryFilePersistence{
+					Path: "/data/registry.db",
+					PvcStore: &feastdevv1alpha1.PvcStore{
+						Name: "example-pvc",
+					},
+				},
+			},
+		},
+	}
+	return copy
+}
+func registryWithS3BucketForPvc(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				FilePersistence: &feastdevv1alpha1.RegistryFilePersistence{
+					Path: "s3://bucket/registry.db",
+					PvcStore: &feastdevv1alpha1.PvcStore{
+						Name: "example-pvc",
+					},
+				},
+			},
+		},
+	}
+	return copy
+}
+func registryWithGsBucketForPvc(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				FilePersistence: &feastdevv1alpha1.RegistryFilePersistence{
+					Path: "gs://bucket/registry.db",
+					PvcStore: &feastdevv1alpha1.PvcStore{
+						Name: "example-pvc",
+					},
+				},
+			},
+		},
+	}
+	return copy
+}
+func registryWithRelativePathForEphemeral(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				FilePersistence: &feastdevv1alpha1.RegistryFilePersistence{
+					Path: "data/online_store.db",
+				},
+			},
+		},
+	}
+	return copy
+}
+func registryWithUnmanagedStore(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				StorePersistence: &feastdevv1alpha1.RegistryStorePersistence{
+					Type: "unmanaged",
+				},
+			},
+		},
+	}
+	return copy
+}
+func registryWithS3AdditionalKeywordsForFile(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				FilePersistence: &feastdevv1alpha1.RegistryFilePersistence{
+					Path:          "/data/online_store.db",
+					S3AddtlKwargs: &map[string]string{},
+				},
+			},
+		},
+	}
+	return copy
+}
+func registryWithS3AdditionalKeywordsForGsBucket(featureStore *feastdevv1alpha1.FeatureStore) *feastdevv1alpha1.FeatureStore {
+	copy := featureStore.DeepCopy()
+	copy.Spec.Services = &feastdevv1alpha1.Services{
+		Registry: &feastdevv1alpha1.Registry{
+			Persistence: &feastdevv1alpha1.RegistryPersistence{
+				FilePersistence: &feastdevv1alpha1.RegistryFilePersistence{
+					Path:          "gs://online_store.db",
+					S3AddtlKwargs: &map[string]string{},
+				},
+			},
+		},
+	}
+	return copy
+}
+
+const resourceName = "test-resource"
+const namespaceName = "default"
+
+func initContext() (context.Context, *feastdevv1alpha1.FeatureStore) {
+	ctx := context.Background()
+
+	typeNamespacedName := types.NamespacedName{
+		Name:      resourceName,
+		Namespace: "default",
+	}
+	featurestore := createFeatureStore()
+
+	BeforeEach(func() {
+		By("verifying the custom resource FeatureStore is not there")
+		err := k8sClient.Get(ctx, typeNamespacedName, featurestore)
+		Expect(err != nil && errors.IsNotFound(err))
+	})
+	AfterEach(func() {
+		By("verifying the custom resource FeatureStore is not there")
+		err := k8sClient.Get(ctx, typeNamespacedName, featurestore)
+		Expect(err != nil && errors.IsNotFound(err))
+	})
+
+	return ctx, featurestore
+}
+
+var _ = Describe("FeatureStore API", func() {
+	Context("When creating an invalid Online Store", func() {
+		ctx, featurestore := initContext()
+
+		It("should fail when both file and store settings are given", func() {
+			attemptInvalidCreationAndAsserts(ctx, onlineStoreWithBothFileAndStore(featurestore), "One file or store persistence is allowed")
+		})
+		It("should fail when neither file nor store settings are given", func() {
+			attemptInvalidCreationAndAsserts(ctx, onlineStoreWithNeitherFileNorStore(featurestore), "Either file or store persistence are required")
+		})
+		It("should fail when unsupported offline store is given", func() {
+			attemptInvalidCreationAndAsserts(ctx, onlineStoreWithUnmanagedStore(featurestore), "Unsupported value:")
+		})
+		It("should fail when PVC persistence has absolute path", func() {
+			attemptInvalidCreationAndAsserts(ctx, onlineStoreWithAbsolutePathForPvc(featurestore), "PVC path must be a file name only")
+		})
+		It("should fail when PVC persistence has S3 bucket", func() {
+			attemptInvalidCreationAndAsserts(ctx, onlineStoreWithS3BucketForPvc(featurestore), "Online store does not support S3 or GS")
+		})
+		It("should fail when PVC persistence has GS bucket", func() {
+			attemptInvalidCreationAndAsserts(ctx, onlineStoreWithGsBucketForPvc(featurestore), "Online store does not support S3 or GS")
+		})
+		It("should fail when ephemeral persistence has relative path", func() {
+			attemptInvalidCreationAndAsserts(ctx, onlineStoreWithRelativePathForEphemeral(featurestore), "Ephemeral stores must have absolute paths")
+		})
+	})
+
+	Context("When creating an invalid Offline Store", func() {
+		ctx, featurestore := initContext()
+
+		It("should fail when both file and store settings are given", func() {
+			attemptInvalidCreationAndAsserts(ctx, offlineStoreWithBothFileAndStore(featurestore), "One file or store persistence is allowed")
+		})
+		It("should fail when neither file nor store settings are given", func() {
+			attemptInvalidCreationAndAsserts(ctx, offlineStoreWithNeitherFileNorStore(featurestore), "Either file or store persistence are required")
+		})
+		It("should fail when unsupported offline store is given", func() {
+			attemptInvalidCreationAndAsserts(ctx, offlineStoreWithUnmanagedStore(featurestore), "Unsupported value:")
+		})
+		It("should fail when PVC persistence has absolute path", func() {
+			attemptInvalidCreationAndAsserts(ctx, offlineStoreWithUnmanagedFileType(featurestore), "Unsupported value")
+		})
+	})
+
+	Context("When creating an invalid Registry", func() {
+		ctx, featurestore := initContext()
+		It("should fail when both file and store settings are given", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithBothFileAndStore(featurestore), "One file or store persistence is allowed")
+		})
+		It("should fail when neither file nor store settings are given", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithNeitherFileNorStore(featurestore), "Either file or store persistence are required")
+		})
+		It("should fail when PVC persistence has absolute path", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithAbsolutePathForPvc(featurestore), "PVC path must be a file name only")
+		})
+		It("should fail when PVC persistence has S3 bucket", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithS3BucketForPvc(featurestore), "PVC persistence does not support S3 or GS buckets")
+		})
+		It("should fail when PVC persistence has GS bucket", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithGsBucketForPvc(featurestore), "PVC persistence does not support S3 or GS buckets")
+		})
+		It("should fail when unsupported registry store is given", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithUnmanagedStore(featurestore), "Unsupported value:")
+		})
+		It("should fail when ephemeral persistence has relative path", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithRelativePathForEphemeral(featurestore), "Ephemeral stores must have absolute paths")
+		})
+		It("should fail when additional S3 settings are provided to non S3 bucket", func() {
+			attemptInvalidCreationAndAsserts(ctx, registryWithS3AdditionalKeywordsForFile(featurestore), "Additional S3 settings are available only for S3 buckets")
+			attemptInvalidCreationAndAsserts(ctx, registryWithS3AdditionalKeywordsForGsBucket(featurestore), "Additional S3 settings are available only for S3 buckets")
+		})
+	})
+})
