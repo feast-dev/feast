9b075069b15d71914dcebbf7054affc2d3d02e7a
Daniele Martinoli <dmartino@redhat.com>
2024-11-06 16:42:08 +0100

    removed global cfg variable
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- infra/feast-operator/internal/controller/suite_test.go
+++ infra/feast-operator/internal/controller/suite_test.go
@@ -26,7 +26,6 @@ import (
 	. "github.com/onsi/gomega"
 
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -39,7 +38,6 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
 var k8sClient client.Client
 var testEnv *envtest.Environment
 
@@ -67,8 +65,7 @@ var _ = BeforeSuite(func() {
 	}
 
 	var err error
-	// cfg is defined in this file globally.
-	cfg, err = testEnv.Start()
+	cfg, err := testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
 	Expect(cfg).NotTo(BeNil())
 
