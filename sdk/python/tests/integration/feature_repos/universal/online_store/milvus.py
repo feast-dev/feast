4dad303c8c9dee2ebac136a293bb45f55b2ca86a
Francisco Javier Arceo <farceo@redhat.com>
2024-11-11 22:01:43 -0500

    updated
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- sdk/python/tests/integration/feature_repos/universal/online_store/milvus.py
+++ sdk/python/tests/integration/feature_repos/universal/online_store/milvus.py
@@ -1,4 +1,3 @@
-import os
 import time
 from typing import Any, Dict
 
@@ -13,7 +12,9 @@ from tests.integration.feature_repos.universal.online_store_creator import (
 class MilvusOnlineStoreCreator(OnlineStoreCreator):
     def __init__(self, project_name: str, **kwargs):
         super().__init__(project_name)
-        self.container = DockerContainer("milvusdb/milvus:v2.2.9").with_exposed_ports(19530)
+        self.container = DockerContainer("milvusdb/milvus:v2.2.9").with_exposed_ports(
+            19530
+        )
 
     def create_online_store(self) -> Dict[str, Any]:
         self.container.start()
@@ -24,16 +25,20 @@ class MilvusOnlineStoreCreator(OnlineStoreCreator):
         max_attempts = 12
         for attempt in range(1, max_attempts + 1):
             try:
-                print(f"Attempting to connect to Milvus at {host}:{port}, attempt {attempt}")
-                connections.connect(alias='default', host=host, port=port)
-                if connections.has_connection(alias='default'):
+                print(
+                    f"Attempting to connect to Milvus at {host}:{port}, attempt {attempt}"
+                )
+                connections.connect(alias="default", host=host, port=port)
+                if connections.has_connection(alias="default"):
                     print("Successfully connected to Milvus")
                     break
             except Exception as e:
                 print(f"Connection attempt failed: {e}")
                 time.sleep(5)
         else:
-            raise RuntimeError("Cannot connect to Milvus server after multiple attempts")
+            raise RuntimeError(
+                "Cannot connect to Milvus server after multiple attempts"
+            )
 
         return {
             "type": "milvus",
