3638676be0a863574f999e78196a636a854c99ca
lrangine <19699092+lokeshrangineni@users.noreply.github.com>
2024-11-15 11:00:44 -0500

    Adding github action to do the operator end-to-end testing. This action will create a kind cluster and runs the tests on it.
    Signed-off-by: lrangine <19699092+lokeshrangineni@users.noreply.github.com>

--- infra/feast-operator/test/utils/utils.go
+++ infra/feast-operator/test/utils/utils.go
@@ -109,6 +109,7 @@ func LoadImageToKindClusterWithName(name string) error {
 	if v, ok := os.LookupEnv("KIND_CLUSTER"); ok {
 		cluster = v
 	}
+	fmt.Println("cluster used in the test is -", cluster)
 	kindOptions := []string{"load", "docker-image", name, "--name", cluster}
 	cmd := exec.Command("kind", kindOptions...)
 	_, err := Run(cmd)
