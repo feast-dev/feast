8a847f44fab28204e559cd5126f7c17a6f9c991f
Harri Lehtola <1781172+peruukki@users.noreply.github.com>
2024-12-01 12:22:43 +0200

    chore: Upgrade TypeScript to latest version 5.7.2 in /ui (#4788)
    Lock the TypeScript minor version by using `~` instead of `^` in the
    version specifier, see
    https://github.com/fastify/fastify-type-provider-typebox/pull/169#issuecomment-2421682145.
    
    After upgrading, the TypeScript compiler complained that the `long`
    package has no default export. Related fixes:
    
    - Add the latest `long` version as a direct dependency since our code
      uses it directly; no compilation errors with the latest version
    
    - The `long` package exports a `Long` class, so use a capital first
      letter in the import name to match that
    
    Signed-off-by: Harri Lehtola <peruukki@hotmail.com>
    Signed-off-by: Theodor Mihalache <tmihalac@redhat.com>

--- ui/src/utils/timestamp.ts
+++ ui/src/utils/timestamp.ts
@@ -1,9 +1,9 @@
-import long from 'long';
+import Long from 'long';
 import { google } from '../protos';
 
 export function toDate(ts: google.protobuf.ITimestamp) {
     var seconds: number;
-    if (ts.seconds instanceof long) {
+    if (ts.seconds instanceof Long) {
         seconds = ts.seconds.low
     } else {
         seconds = ts.seconds!;
