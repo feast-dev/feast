0c2d4eef5f774ad3793e53dcc34bcfe7a0b70988
Dimitris Stafylarakis <xanias@gmail.com>
2025-01-05 10:26:21 +0100

    remove unused online features service
    Signed-off-by: Dimitris Stafylarakis <xanias@gmail.com>

--- sdk/python/feast/feature_logging.py
+++ sdk/python/feast/feature_logging.py
@@ -5,7 +5,6 @@ from typing import TYPE_CHECKING, Dict, Optional, Type, cast
 import pyarrow as pa
 
 from feast.data_source import DataSource
-from feast.embedded_go.type_map import FEAST_TYPE_TO_ARROW_TYPE, PA_TIMESTAMP_TYPE
 from feast.errors import (
     FeastObjectNotFoundException,
     FeatureViewNotFoundException,
@@ -15,6 +14,8 @@ from feast.feature_view import DUMMY_ENTITY_ID
 from feast.protos.feast.core.FeatureService_pb2 import (
     LoggingConfig as LoggingConfigProto,
 )
+from feast.protos.feast.types import Value_pb2
+from feast.types import Array, PrimitiveFeastType
 
 if TYPE_CHECKING:
     from feast.feature_service import FeatureService
@@ -24,6 +25,60 @@ if TYPE_CHECKING:
 REQUEST_ID_FIELD = "__request_id"
 LOG_TIMESTAMP_FIELD = "__log_timestamp"
 LOG_DATE_FIELD = "__log_date"
+
+PA_TIMESTAMP_TYPE = pa.timestamp("s", tz=timezone.utc)
+
+ARROW_TYPE_TO_PROTO_FIELD = {
+    pa.int32(): "int32_val",
+    pa.int64(): "int64_val",
+    pa.float32(): "float_val",
+    pa.float64(): "double_val",
+    pa.bool_(): "bool_val",
+    pa.string(): "string_val",
+    pa.binary(): "bytes_val",
+    PA_TIMESTAMP_TYPE: "unix_timestamp_val",
+}
+
+ARROW_LIST_TYPE_TO_PROTO_FIELD = {
+    pa.int32(): "int32_list_val",
+    pa.int64(): "int64_list_val",
+    pa.float32(): "float_list_val",
+    pa.float64(): "double_list_val",
+    pa.bool_(): "bool_list_val",
+    pa.string(): "string_list_val",
+    pa.binary(): "bytes_list_val",
+    PA_TIMESTAMP_TYPE: "unix_timestamp_list_val",
+}
+
+ARROW_LIST_TYPE_TO_PROTO_LIST_CLASS = {
+    pa.int32(): Value_pb2.Int32List,
+    pa.int64(): Value_pb2.Int64List,
+    pa.float32(): Value_pb2.FloatList,
+    pa.float64(): Value_pb2.DoubleList,
+    pa.bool_(): Value_pb2.BoolList,
+    pa.string(): Value_pb2.StringList,
+    pa.binary(): Value_pb2.BytesList,
+    PA_TIMESTAMP_TYPE: Value_pb2.Int64List,
+}
+
+FEAST_TYPE_TO_ARROW_TYPE = {
+    PrimitiveFeastType.INT32: pa.int32(),
+    PrimitiveFeastType.INT64: pa.int64(),
+    PrimitiveFeastType.FLOAT32: pa.float32(),
+    PrimitiveFeastType.FLOAT64: pa.float64(),
+    PrimitiveFeastType.STRING: pa.string(),
+    PrimitiveFeastType.BYTES: pa.binary(),
+    PrimitiveFeastType.BOOL: pa.bool_(),
+    PrimitiveFeastType.UNIX_TIMESTAMP: pa.timestamp("s"),
+    Array(PrimitiveFeastType.INT32): pa.list_(pa.int32()),
+    Array(PrimitiveFeastType.INT64): pa.list_(pa.int64()),
+    Array(PrimitiveFeastType.FLOAT32): pa.list_(pa.float32()),
+    Array(PrimitiveFeastType.FLOAT64): pa.list_(pa.float64()),
+    Array(PrimitiveFeastType.STRING): pa.list_(pa.string()),
+    Array(PrimitiveFeastType.BYTES): pa.list_(pa.binary()),
+    Array(PrimitiveFeastType.BOOL): pa.list_(pa.bool_()),
+    Array(PrimitiveFeastType.UNIX_TIMESTAMP): pa.list_(pa.timestamp("s")),
+}
 
 
 class LoggingSource:
