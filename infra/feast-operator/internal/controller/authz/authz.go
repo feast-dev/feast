eab0f0a81e8e9ec07995ef8fed47787bf432ea89
Tommy Hughes <tohughes@redhat.com>
2025-02-14 14:54:46 -0600

    resolve infinite reconciler loop in authz controller
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- infra/feast-operator/internal/controller/authz/authz.go
+++ infra/feast-operator/internal/controller/authz/authz.go
@@ -134,11 +134,11 @@ func (authz *FeastAuthorization) initFeastRoleBinding() *rbacv1.RoleBinding {
 
 func (authz *FeastAuthorization) setFeastRoleBinding(roleBinding *rbacv1.RoleBinding) error {
 	roleBinding.Labels = authz.getLabels()
-	roleBinding.Subjects = append(roleBinding.Subjects, rbacv1.Subject{
+	roleBinding.Subjects = []rbacv1.Subject{{
 		Kind:      rbacv1.ServiceAccountKind,
 		Name:      services.GetFeastName(authz.Handler.FeatureStore),
 		Namespace: authz.Handler.FeatureStore.Namespace,
-	})
+	}}
 	roleBinding.RoleRef = rbacv1.RoleRef{
 		APIGroup: rbacv1.GroupName,
 		Kind:     "Role",
