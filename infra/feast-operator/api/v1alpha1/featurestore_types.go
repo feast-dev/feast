623c1a13cfd25ee67ad682475a232cdc7340f3e4
Elliot Scribner <elliot.scribner@couchbase.com>
2025-02-18 11:02:22 -0800

    Added `couchbase.offline` to operator types
    Signed-off-by: Elliot Scribner <elliot.scribner@couchbase.com>

--- infra/feast-operator/api/v1alpha1/featurestore_types.go
+++ infra/feast-operator/api/v1alpha1/featurestore_types.go
@@ -148,7 +148,7 @@ var ValidOfflineStoreFilePersistenceTypes = []string{
 // OfflineStoreDBStorePersistence configures the DB store persistence for the offline store service
 type OfflineStoreDBStorePersistence struct {
 	// Type of the persistence type you want to use.
-	// +kubebuilder:validation:Enum=snowflake.offline;bigquery;redshift;spark;postgres;trino;athena;mssql
+	// +kubebuilder:validation:Enum=snowflake.offline;bigquery;redshift;spark;postgres;trino;athena;mssql;couchbase.offline
 	Type string `json:"type"`
 	// Data store parameters should be placed as-is from the "feature_store.yaml" under the secret key. "registry_type" & "type" fields should be removed.
 	SecretRef corev1.LocalObjectReference `json:"secretRef"`
@@ -165,6 +165,7 @@ var ValidOfflineStoreDBStorePersistenceTypes = []string{
 	"trino",
 	"athena",
 	"mssql",
+	"couchbase.offline",
 }
 
 // OnlineStore configures the online store service
