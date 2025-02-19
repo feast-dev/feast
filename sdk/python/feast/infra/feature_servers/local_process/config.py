5b331b3c8de1855d1311fc45e7c496fb7c383943
Shuchu Han <shuchu.han@gmail.com>
2025-02-18 22:01:41 -0500

    fix: Fix lint issue.
    Signed-off-by: Shuchu Han <shuchu.han@gmail.com>

--- sdk/python/feast/infra/feature_servers/local_process/config.py
+++ sdk/python/feast/infra/feature_servers/local_process/config.py
@@ -6,6 +6,6 @@ from feast.infra.feature_servers.base_config import BaseFeatureServerConfig
 class LocalFeatureServerConfig(BaseFeatureServerConfig):
     # Feature server type selector.
     type: Literal["local"] = "local"
-   
+
     # The endpoint definition for transformation_service
     transformation_service_endpoint: str = "localhost:6569"
