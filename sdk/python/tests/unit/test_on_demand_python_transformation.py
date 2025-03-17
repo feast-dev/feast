b8de03a786e52590a19233f723b64b8e404b15ca
Francisco Javier Arceo <farceo@redhat.com>
2025-03-16 23:20:18 -0400

    missed import
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- sdk/python/tests/unit/test_on_demand_python_transformation.py
+++ sdk/python/tests/unit/test_on_demand_python_transformation.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import re
 import sqlite3
 import sys
@@ -802,9 +803,10 @@ def test_invalid_python_transformation_raises_type_error_on_apply():
         ):
             store.apply([request_source, python_view])
 
+
 @pytest.mark.skipif(
     not (sys.version_info[0:2] in [(3, 10), (3, 11)] and platform.system() == "Darwin"),
-    reason="Only works on Python 3.10 and 3.11 on macOS"
+    reason="Only works on Python 3.10 and 3.11 on macOS",
 )
 class TestOnDemandTransformationsWithWrites(unittest.TestCase):
     def test_stored_writes(self):
