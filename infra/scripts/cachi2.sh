1f88f7a1bcd3f2ecd8f6aa7bd1e3e5c52e994131
Tommy Hughes <tohughes@redhat.com>
2025-03-19 16:34:09 -0500

    no-binary test
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- infra/scripts/cachi2.sh
+++ infra/scripts/cachi2.sh
@@ -13,7 +13,7 @@ cd ${PROJECT_ROOT_DIR}
 
 ############################
 
-alias cachi2='docker run --rm -ti -v "$PWD:$PWD:z" -w "$PWD" quay.io/konflux-ci/cachi2:f7a61b067f4446e4982d0e3b9545ce4aa0d8284f'
+#alias cachi2='docker run --rm -ti -v "$PWD:$PWD:z" -w "$PWD" quay.io/konflux-ci/cachi2:f7a61b067f4446e4982d0e3b9545ce4aa0d8284f'
 
 rm -rf ./cachi2-output ./cachi2.env ./pydantic-core ./maturin ./ahash
 #git clone --branch v0.8.11 https://github.com/tkaitchuck/ahash
@@ -47,7 +47,7 @@ rm -rf ./cachi2-output ./cachi2.env ./pydantic-core ./maturin ./ahash
 #cachi2 fetch-deps cargo --source pydantic-core
 #uv export --all-groups --format requirements-txt -o pydantic-core/requirements.txt --project pydantic-core -p 3.11
 #cachi2 inject-files --for-output-dir /tmp/cachi2-output cachi2-output
-cachi2 fetch-deps \
+docker run --rm -ti -v "$PWD:$PWD:z" -w "$PWD" quay.io/konflux-ci/cachi2:f7a61b067f4446e4982d0e3b9545ce4aa0d8284f fetch-deps \
   --output ./cachi2-output \
   '{
   "type": "pip",
@@ -64,7 +64,8 @@ cachi2 fetch-deps \
 #"pydantic-core/requirements.txt",
 #"sdk/python/requirements/py3.11-pydantic-requirements.txt"
 
-cachi2 generate-env ./cachi2-output -o ./cachi2.env --for-output-dir /tmp/cachi2-output
+docker run --rm -ti -v "$PWD:$PWD:z" -w "$PWD" quay.io/konflux-ci/cachi2:f7a61b067f4446e4982d0e3b9545ce4aa0d8284f generate-env \
+  ./cachi2-output -o ./cachi2.env --for-output-dir /tmp/cachi2-output
 
 ## pydantic-core builder w/ pip install
 #docker build \
