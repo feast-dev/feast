9a6d34c1ee47bb318c879e4e5e6c1f4cd9553757
vineet-belur <vbelur@gmail.com>
2025-03-19 23:02:38 -0500

    refactor eg-uri

--- java/serving-client/src/test/java/eg/feast/RequestUtilTest.java
+++ java/serving-client/src/test/java/eg/feast/RequestUtilTest.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dev.feast;
+package eg.feast;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
