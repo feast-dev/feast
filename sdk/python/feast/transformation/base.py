36b9ec1f6a74c441a1dbb6e0accbb9d52fad11b4
HaoXuAI <sduxuhao@gmail.com>
2025-03-21 16:18:10 -0700

    fix conflict
    Signed-off-by: HaoXuAI <sduxuhao@gmail.com>

--- sdk/python/feast/transformation/base.py
+++ sdk/python/feast/transformation/base.py
@@ -84,8 +84,7 @@ class Transformation(ABC):
     def infer_features(self, *args, **kwargs) -> Any:
         raise NotImplementedError
 
-    def __deepcopy__(self,
-                     memo: Optional[Dict[int, Any]] = None) -> "Transformation":
+    def __deepcopy__(self, memo: Optional[Dict[int, Any]] = None) -> "Transformation":
         return Transformation(mode=self.mode, udf=self.udf, udf_string=self.udf_string)
 
 
