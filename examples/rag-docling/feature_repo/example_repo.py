1953f8613c00312d1321ea458b19d0f5bb02db4d
Francisco Javier Arceo <farceo@redhat.com>
2025-03-26 23:18:50 -0400

    updated repo exaxmple
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- examples/rag-docling/feature_repo/example_repo.py
+++ examples/rag-docling/feature_repo/example_repo.py
@@ -1,12 +1,13 @@
 from datetime import timedelta
 
+import pandas as pd
 from feast import (
     FeatureView,
     Field,
     FileSource,
 )
 from feast.data_format import ParquetFormat
-from feast.types import Float64, Array, String, ValueType, Bytes
+from feast.types import Float32, Array, String, ValueType, PdfBytes
 from feast import Entity, RequestSource
 from feast.on_demand_feature_view import on_demand_feature_view
 from sentence_transformers import SentenceTransformer
@@ -16,6 +17,14 @@ chunk = Entity(
     name="chunk_id",
     description="Chunk ID",
     value_type=ValueType.STRING,
+    join_keys=["chunk_id"],
+)
+
+document = Entity(
+    name="document_id",
+    description="Document ID",
+    value_type=ValueType.STRING,
+    join_keys=["document_id"],
 )
 
 parquet_file_path = "./data/docling_samples.parquet"
@@ -37,22 +46,17 @@ EMBED_MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
 embedding_model = SentenceTransformer(EMBED_MODEL_ID)
 
 
-@on_demand_feature_view(
-    sources=[input_request],
-    schema=[
-        Field(name="query_embedding", dtype=Array(Float64), vector_index=False),
-    ],
-    mode="python",
-    singleton=True,
-    write_to_online_store=False,
-)
 def embed_chunk(inputs: Dict[str, Any]) -> Dict[str, List[float]]:
     output = {
         "query_embedding": embedding_model.encode([
-            inputs["query_string"]], normalize_embeddings=True,
-        ).tolist()[0]
+            inputs["query_string"]
+        ], normalize_embeddings=True).tolist()[0]
     }
     return output
+
+def embed_text(text: str) -> list[float]:
+    """Generate an embedding for a given text."""
+    return embedding_model.encode([text], normalize_embeddings=True).tolist()[0]
 
 
 docling_example_feature_view = FeatureView(
@@ -60,11 +64,10 @@ docling_example_feature_view = FeatureView(
     entities=[chunk],
     schema=[
         Field(name="file_name", dtype=String),
-        # Field(name="full_document_markdown", dtype=String),
         Field(name="raw_chunk_markdown", dtype=String),
         Field(
             name="vector",
-            dtype=Array(Float64),
+            dtype=Array(Float32),
             vector_index=True,
             vector_search_metric="COSINE",
         ),
@@ -100,57 +103,48 @@ chunker = HybridChunker(tokenizer=tokenizer, max_tokens=MAX_TOKENS, merge_peers=
 input_request_pdf = RequestSource(
     name="pdf_request_source",
     schema=[
-        Field(name="pdf_bytes", dtype=Bytes),
+        Field(name="document_id", dtype=String),        
+        Field(name="pdf_bytes", dtype=PdfBytes),
         Field(name="file_name", dtype=String),
     ],
 )
 
-import pandas as pd
 @on_demand_feature_view(
     sources=[input_request_pdf],
     schema=[
-        Field(name="document_id", dtype=String, vector_index=False),
-        Field(name="chunk_id", dtype=Array(String), vector_index=False),
-        Field(name="chunk_text", dtype=Array(String), vector_index=False),
-        Field(name="vector", dtype=Array(Float64), vector_index=False),
+        Field(name="document_id", dtype=String),
+        Field(name="chunk_id", dtype=String),
+        Field(name="chunk_text", dtype=String),
+        Field(
+            name="vector", 
+            dtype=Array(Float32), 
+            vector_index=False
+        ),
     ],
     mode="python",
-    singleton=False,
-    write_to_online_store=False,
-    explode=True,
+    singleton=True,
+    write_to_online_store=True,
 )
 def docling_transform_docs(inputs: Dict[str, Any]):
-    try:
-        buf = io.BytesIO(
-            inputs['pdf_bytes'],
-        )
-
-        source = DocumentStream(name=inputs['file_name'], stream=buf)
-        converter = DocumentConverter()
-        result = converter.convert(source)
-        document_id = generate_chunk_id(inputs["file_name"])
-        output, chunks, embeddings, chunk_ids = [], [], [], []
-        for chunk in chunker.chunk(dl_doc=result.document):
-            raw_chunk = chunker.serialize(chunk=chunk)
-            embedding = embed_chunk(raw_chunk)
-            chunk_id = generate_chunk_id(inputs["file_name"], raw_chunk)
-            # chunk_dict = {
-            #     "document_id": document_id,
-            #     "chunk_id": chunk_id,
-            #     "vector": embedding,
-            #     "chunk_text": raw_chunk,
-            # }
-            chunks.append(raw_chunk)
-            chunk_ids.append(chunk_id)
-            embeddings.append(embedding)
-            # output.append(chunk_dict)
-        return {"document_id": document_id, "chunk_id": chunks, "vector": embedding, "chunk_text": chunks}
-
-
-    except Exception as e:
-        print(e)
-        return {"document_id": "missing", "chunk_id": ["missing"], "vector": [[0.1, 0.2]], "chunk_text": ["missing"]}
-
-    return pd.DataFrame(output)
-
-
+    document_ids, chunks, embeddings, chunk_ids = [], [], [], []
+    buf = io.BytesIO(
+        inputs["pdf_bytes"],
+    )
+    source = DocumentStream(name=inputs["file_name"], stream=buf)
+    converter = DocumentConverter()
+    result = converter.convert(source)
+    for i, chunk in enumerate(chunker.chunk(dl_doc=result.document)):
+        raw_chunk = chunker.serialize(chunk=chunk)
+        # embedding = embed_chunk(raw_chunk).get("query_embedding", [])
+        embedding = embed_text(raw_chunk)
+        chunk_id = f"chunk-{i}"
+        document_ids.append(inputs["document_id"])
+        chunks.append(raw_chunk)
+        chunk_ids.append(chunk_id)
+        embeddings.append(embedding)
+    return {
+        "document_id": document_ids,
+        "chunk_id": chunk_ids,
+        "vector": embeddings,
+        "chunk_text": chunks,
+    }
