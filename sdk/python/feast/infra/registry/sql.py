d1e3db61a6c63d5cec2381553f226694b6cbf495
Zach <54722467+zabarn@users.noreply.github.com>
2025-04-16 16:07:39 -0500

    feat: Log execution times of self.proto() in sql registry (#214)
    * feat: Log execution times of self.proto() in sql registry
    
    * fix: use correct time import
    
    * fix: block on executor map to get longest and full execution time
    
    * lint
    
    * actually fix threaded timer
    
    ---------
    
    Co-authored-by: Zach Barnett <zbarnett@expediagroup.com>

--- sdk/python/feast/infra/registry/sql.py
+++ sdk/python/feast/infra/registry/sql.py
@@ -1,4 +1,5 @@
 import logging
+import time
 import uuid
 from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timezone
@@ -992,13 +993,27 @@ class SqlRegistry(CachingRegistry):
         ]
 
         if self.thread_pool_executor_worker_count == 0:
+            logger.info("Starting timer for single threaded self.proto()")
+            start = time.time()
             for project in filtered_project_list:
                 process_project(project)
+            end = time.time()
+            logger.info(
+                f"Single threaded self.proto() took {end - start} seconds to process {len(filtered_project_list)} projects"
+            )
         else:
+            logger.info("Starting timer for multi threaded self.proto()")
+            start = time.time()
+
             with ThreadPoolExecutor(
                 max_workers=self.thread_pool_executor_worker_count
             ) as executor:
-                executor.map(process_project, filtered_project_list)
+                list(executor.map(process_project, filtered_project_list))
+
+            end = time.time()
+            logger.info(
+                f"Multi threaded self.proto() took {end - start} seconds to process {len(filtered_project_list)} projects"
+            )
 
         if last_updated_timestamps:
             r.last_updated.FromDatetime(max(last_updated_timestamps))
