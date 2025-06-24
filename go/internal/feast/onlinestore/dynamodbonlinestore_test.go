b1abd414a23900f66da1768959141350be4335fb
iamcodingcat <joyh951021@gmail.com>
2025-06-24 22:04:54 +0900

    test: add dynamodb online store
    Signed-off-by: iamcodingcat <joyh951021@gmail.com>

---
+++ go/internal/feast/onlinestore/dynamodbonlinestore_test.go
@@ -0,0 +1,24 @@
+package onlinestore
+
+import (
+	"testing"
+
+	"github.com/feast-dev/feast/go/internal/feast/registry"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDynamodbOnlineStore(t *testing.T) {
+	var config = map[string]interface{}{
+		"batch_size":           40,
+		"region":               "us-east-1",
+		"max_pool_connections": 4,
+		"consistent_reads":     "true",
+	}
+	rc := &registry.RepoConfig{
+		OnlineStore:                   config,
+		EntityKeySerializationVersion: 2,
+	}
+	_, err := NewDynamodbOnlineStore("test", rc, config)
+	assert.Nil(t, err)
+}
