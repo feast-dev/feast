6fd1c3b2d66d76c14678073961b225386ab3227a
vbhagwat <vbhagwat@expediagroup.com>
2025-06-26 14:27:12 -0700

    fix: Formatting

--- java/serving-client/src/main/java/dev/feast/FeastClient.java
+++ java/serving-client/src/main/java/dev/feast/FeastClient.java
@@ -413,32 +413,34 @@ public class FeastClient implements AutoCloseable {
   }
 
   /**
- * Retrieves online features from Feast using the provided request, entity rows, and project name.
- *
- * @param getOnlineFeaturesRequest The request object containing feature references.
- * @param entities List of {@link Row} objects representing the entities to retrieve features for.
- * @param project The Feast project to retrieve features from.
- * @return A list of {@link Row} containing the retrieved feature data.
- */
+   * Retrieves online features from Feast using the provided request, entity rows, and project name.
+   *
+   * @param getOnlineFeaturesRequest The request object containing feature references.
+   * @param entities List of {@link Row} objects representing the entities to retrieve features for.
+   * @param project The Feast project to retrieve features from.
+   * @return A list of {@link Row} containing the retrieved feature data.
+   */
   public List<Row> getOnlineFeatures(
       GetOnlineFeaturesRequest getOnlineFeaturesRequest, List<Row> entities, String project) {
     return getOnlineFeatures(getOnlineFeaturesRequest, entities);
   }
 
   /**
- * Get online features range from Feast without indicating a project — uses the default project.
- *
- * <p>See {@link #getOnlineFeaturesRange(List, List, List, int, boolean, String)} for project-specific queries.
- *
- * @param featureRefs List of string feature references to retrieve in the format
- *                    {@code featureTable:feature}, where 'featureTable' and 'feature' refer to
- *                    the FeatureTable and Feature names respectively. Only the Feature name is required.
- * @param entities List of {@link RangeRow} objects representing the entities for which to retrieve features.
- * @param sortKeyFilters List of field names to use for sorting the feature results.
- * @param limit Maximum number of results to return.
- * @param reverseSortOrder If true, the results will be returned in descending order.
- * @return List of {@link RangeRow} containing the retrieved feature data.
- */
+   * Get online features range from Feast without indicating a project — uses the default project.
+   *
+   * <p>See {@link #getOnlineFeaturesRange(List, List, List, int, boolean, String)} for
+   * project-specific queries.
+   *
+   * @param featureRefs List of string feature references to retrieve in the format {@code
+   *     featureTable:feature}, where 'featureTable' and 'feature' refer to the FeatureTable and
+   *     Feature names respectively. Only the Feature name is required.
+   * @param entities List of {@link RangeRow} objects representing the entities for which to
+   *     retrieve features.
+   * @param sortKeyFilters List of field names to use for sorting the feature results.
+   * @param limit Maximum number of results to return.
+   * @param reverseSortOrder If true, the results will be returned in descending order.
+   * @return List of {@link RangeRow} containing the retrieved feature data.
+   */
   public List<RangeRow> getOnlineFeaturesRange(
       GetOnlineFeaturesRangeRequest request, List<Row> entities) {
 
