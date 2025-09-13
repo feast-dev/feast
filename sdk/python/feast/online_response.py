e786ef16fd77e5769ae59114cf18a1a3f9afa083
HaoXuAI <sduxuhao@gmail.com>
2025-09-12 21:56:35 -0700

    linting
    Signed-off-by: HaoXuAI <sduxuhao@gmail.com>

--- sdk/python/feast/online_response.py
+++ sdk/python/feast/online_response.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Any, Dict, List, Union, TypeAlias
+from typing import TYPE_CHECKING, Any, Dict, List, TypeAlias, Union
 
 import pandas as pd
 import pyarrow as pa
