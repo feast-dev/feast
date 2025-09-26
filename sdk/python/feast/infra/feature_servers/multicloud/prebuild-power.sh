73d4b4933579f0c8c70d67b836da6f3d6e6ea3e3
Puneet Sharma <puneet.sharma21@ibm.com>
2025-09-26 16:52:38 +0530

    ODH Feature Server Power Support  (#94)
    * Updating dockerfile and prebuild script for power
    
    * adding milvus-lite pre-build
    
    * fixing pyarrow
    
    * modify build and enable pyarrow S3 & SUBSTRAIT
    
    * add prebuild-power.sh on multicloud dir
    
    * move to multicloud dir
    
    * updating rpms for ppc
    
    * replace CentOS Stream repo
    
    * adding rpms lock
    
    * adding type rpm
    
    Signed-off-by: puneetsharma21 <puneet.sharma21@ibm.com>
    
    * adding rpms path
    
    * Replace dnf install with rpm -ivh for epel-release to avoid dependency resolution and ensure direct installation
    
    * removing epel
    
    * packages build from source
    
    * updated gcc13
    
    * adding build
    
    * adding build==1.3.0
    
    * adding py packages
    
    * updated req
    
    * adding duckdb
    
    * adding duckdb sha
    
    * cloning duckdb
    
    * cloning duckdb prefetch
    
    * clone duckdb
    
    * changes
    
    * changes
    
    * fix pyarrow unbound variables
    
    * update rpms lock
    
    * updated prebuild-power.sh
    
    * changes
    
    * updated req.txt
    
    * updated req.txt
    
    * adding urllib3
    
    * changes
    
    * updating req.txt
    
    * updted
    
    * updated build
    
    * updated
    
    * perl&ncurses
    
    * updating version conflict
    
    * updating power
    
    * updating hermetic
    
    * updated prebuild power
    
    * adding artifacts.lock.yaml
    
    * Move artifacts.lock.yaml into multicloud directory
    
    * cloning repo
    
    * adding generic type
    
    * solving formatting issue
    
    * Prefetched artifacts from cachi2
    
    * Entering DuckDB source directory
    
    * building duckdb
    
    * Added prebuild-power.sh to build DuckDB, Boost, GFlags, and RapidJSON on ppc64le
    
    * fixing pyarrow build issue
    
    * updating rpms
    
    * set hermetic false
    
    * installing perl and ncurses-devel
    
    * building milvus-lite
    
    * updated pipeline timeout
    
    * building milvus-lite
    
    * running all packages
    
    * Cleanup and unset SETUPTOOLS_SCM_PRETEND_VERSION
    
    * cleanup: remove unnecessary comments
    
    ---------
    
    Signed-off-by: puneetsharma21 <puneet.sharma21@ibm.com>

---
+++ sdk/python/feast/infra/feature_servers/multicloud/prebuild-power.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+set -Eeuo pipefail
+trap 'echo "[prebuild-power] failed at line $LINENO"; exit 1' ERR
+shopt -s dotglob nullglob
+
+PYTHON_VERSION=3.11
+WORKDIR=$(pwd)
+CMAKE_VERSION=3.30.5
+CMAKE_REQUIRED_VERSION=3.30.5
+
+dnf install -y gcc-toolset-13 make cmake ninja-build libomp-devel \
+               git python${PYTHON_VERSION} python${PYTHON_VERSION}-devel python${PYTHON_VERSION}-pip \
+               openssl openssl-devel zlib-devel libuuid-devel 
+
+# Enable GCC toolset
+source /opt/rh/gcc-toolset-13/enable
+export CXX=/opt/rh/gcc-toolset-13/root/usr/bin/g++
+
+# Ensure CXXFLAGS and LINKFLAGS are initialized
+: "${CMAKE_ARGS:=""}"
+: "${CXXFLAGS:=""}"
+: "${CFLAGS:=""}"
+: "${LINKFLAGS:=""}"
+
+# Installing Python build dependencies
+python${PYTHON_VERSION} -m pip install build wheel setuptools ninja pybind11 numpy setuptools_scm Cython==3.0.8
+
+# Directory to collect built wheels
+mkdir -p /wheelhouse
+
+#######################################################
+# Build DuckDB (Python package)
+#######################################################
+echo "Entering DuckDB source directory..."
+cd /tmp/duckdb-1.1.3/tools/pythonpkg
+export SETUPTOOLS_SCM_PRETEND_VERSION=1.1.3
+python${PYTHON_VERSION} -m build --wheel --no-isolation
+# Cleanup
+unset SETUPTOOLS_SCM_PRETEND_VERSION
+ls dist/*.whl >/dev/null
+cp -v dist/*.whl /wheelhouse/
+cd $WORKDIR
+
+#######################################################
+# Build gRPC  (Python package)
+#######################################################
+echo "Building grpcio..."
+export GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+pip install grpcio==1.62.3
+
+#######################################################
+# Build Pyarrow  (Python package)
+#######################################################
+echo "Entering Pyarrow source directory..."
+cd /tmp/arrow-apache-arrow-17.0.0
+cd cpp
+mkdir -p release && cd release
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=/usr/local \
+      -DARROW_PYTHON=ON \
+      -DARROW_PARQUET=ON \
+      -DARROW_ORC=ON \
+      -DARROW_FILESYSTEM=ON \
+      -DARROW_WITH_LZ4=ON \
+      -DARROW_WITH_ZSTD=ON \
+      -DARROW_WITH_SNAPPY=ON \
+      -DARROW_JSON=ON \
+      -DARROW_CSV=ON \
+      -DARROW_DATASET=ON \
+      -DARROW_S3=ON \
+      -DARROW_BUILD_TESTS=OFF \
+      -DARROW_SUBSTRAIT=ON \
+      -DProtobuf_SOURCE=BUNDLED \
+      -DARROW_DEPENDENCY_SOURCE=BUNDLED \
+    ..
+make -j$(nproc)
+make install
+cd ../../python
+export BUILD_TYPE=release
+python${PYTHON_VERSION} setup.py build_ext --build-type=$BUILD_TYPE --bundle-arrow-cpp bdist_wheel
+ls dist/*.whl >/dev/null
+cp -v dist/*.whl /wheelhouse/
+cd ../../..
+
+#######################################################
+# Build Milvus-Lite  (Python package)
+#######################################################
+echo "Building milvus-lite..."
+dnf remove -y gcc-toolset-13
+
+dnf install -y perl ncurses-devel wget openblas-devel cargo gcc gcc-c++ libstdc++-static which libaio \
+               libtool m4 autoconf automake zlib-devel libffi-devel scl-utils xz
+
+export CC=gcc
+export CXX=g++
+export CXXFLAGS="-std=c++17"
+
+python${PYTHON_VERSION} -m pip install conan==1.64.1 setuptools==70.0.0
+
+git clone https://github.com/milvus-io/milvus-lite
+cd milvus-lite/python
+git checkout v2.4.12
+git submodule update --init --recursive
+python${PYTHON_VERSION} -m pip install -v -e .
