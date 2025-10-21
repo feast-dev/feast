b8a8ab68077a2d3c25b35c49649fde8d97b66dfa
Aniket Paluskar <apaluska@redhat.com>
2025-10-21 17:41:35 +0530

    Added start_date & end_date range for historical data retrieval from RemoteOfflineStore
    Signed-off-by: Aniket Paluskar <apaluska@redhat.com>

--- sdk/python/feast/offline_server.py
+++ sdk/python/feast/offline_server.py
@@ -449,6 +449,17 @@ class OfflineServer(fl.FlightServerBase):
                 resource=feature_view, actions=[AuthzedAction.READ_OFFLINE]
             )
 
+    # Extract and deserialize start_date/end_date if present
+        kwargs = {}
+        if "start_date" in command and command["start_date"] is not None:
+            kwargs["start_date"] = utils.make_tzaware(
+                datetime.fromisoformat(command["start_date"])
+            )
+        if "end_date" in command and command["end_date"] is not None:
+            kwargs["end_date"] = utils.make_tzaware(
+                datetime.fromisoformat(command["end_date"])
+            )
+        
         retJob = self.offline_store.get_historical_features(
             config=self.store.config,
             feature_views=feature_views,
@@ -457,6 +468,7 @@ class OfflineServer(fl.FlightServerBase):
             registry=self.store.registry,
             project=project,
             full_feature_names=full_feature_names,
+            **kwargs,
         )
 
         return retJob
