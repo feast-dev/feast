fabce765804a8577ef31fb83525adc5df5748c44
Aniket Paluskar <64416568+aniketpalu@users.noreply.github.com>
2025-12-10 12:48:29 +0530

    fix: SQLite online store deletes tables from other projects in shared registry scenarios (#5766)
    * Added project prefix filtering when getting infra difference
    
    Signed-off-by: Aniket Paluskar <apaluska@redhat.com>
    
    * Minoring formatting changes
    
    Signed-off-by: Aniket Paluskar <apaluska@redhat.com>
    
    ---------
    
    Signed-off-by: Aniket Paluskar <apaluska@redhat.com>

--- sdk/python/feast/feature_store.py
+++ sdk/python/feast/feature_store.py
@@ -78,7 +78,6 @@ from feast.on_demand_feature_view import OnDemandFeatureView
 from feast.online_response import OnlineResponse
 from feast.permissions.permission import Permission
 from feast.project import Project
-from feast.protos.feast.core.InfraObject_pb2 import Infra as InfraProto
 from feast.protos.feast.serving.ServingService_pb2 import (
     FieldStatus,
     GetOnlineFeaturesResponse,
@@ -792,12 +791,13 @@ class FeatureStore:
         # Compute the desired difference between the current infra, as stored in the registry,
         # and the desired infra.
         self._registry.refresh(project=self.project)
-        current_infra_proto = InfraProto()
-        current_infra_proto.CopyFrom(self._registry.proto().infra)
+        current_infra_proto = self._registry.get_infra(self.project).to_proto()
         desired_registry_proto = desired_repo_contents.to_registry_proto()
         new_infra = self._provider.plan_infra(self.config, desired_registry_proto)
         new_infra_proto = new_infra.to_proto()
-        infra_diff = diff_infra_protos(current_infra_proto, new_infra_proto)
+        infra_diff = diff_infra_protos(
+            current_infra_proto, new_infra_proto, project=self.project
+        )
 
         return registry_diff, infra_diff, new_infra
 
