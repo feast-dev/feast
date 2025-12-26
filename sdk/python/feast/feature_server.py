b6299d2cc7d6618ee3b909dbca76a51ce490008c
Francisco Javier Arceo <farceo@redhat.com>
2025-12-25 22:00:12 -0500

    fix
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- sdk/python/feast/feature_server.py
+++ sdk/python/feast/feature_server.py
@@ -380,7 +380,7 @@ def get_app(
             feature_view_name=feature_view_name,
             df=df,
             allow_registry_cache=allow_registry_cache,
-            transform_on_write=request.transform_on_write,
+            transform=request.transform_on_write,
         )
 
     @app.get("/health")
