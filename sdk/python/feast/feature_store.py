dde05bd48ac2fb7c8870aa092f8fccc879188e17
Francisco Javier Arceo <farceo@redhat.com>
2026-01-05 11:57:24 -0500

    lint
    Signed-off-by: Francisco Javier Arceo <farceo@redhat.com>

--- sdk/python/feast/feature_store.py
+++ sdk/python/feast/feature_store.py
@@ -1019,7 +1019,9 @@ class FeatureStore:
 
                 # Create ODFV with same transformation logic and correct mode
                 # Include both FeatureViews and RequestSources in sources
-                sources_list: List[Union[FeatureView, RequestSource]] = list(fv.source_views or [])
+                sources_list: List[Union[FeatureView, RequestSource]] = list(
+                    fv.source_views or []
+                )
                 if hasattr(fv, "source_request_sources") and fv.source_request_sources:
                     sources_list.extend(fv.source_request_sources.values())
 
@@ -1294,7 +1296,9 @@ class FeatureStore:
         # Handle FeatureViews with feature_transformation for historical retrieval
         # These are supported by extracting their source views and applying transformations later
         regular_feature_views: List[Union[FeatureView, OnDemandFeatureView]] = []
-        unified_transformation_views: List[Tuple[Union[FeatureView, OnDemandFeatureView], List[str]]] = []
+        unified_transformation_views: List[
+            Tuple[Union[FeatureView, OnDemandFeatureView], List[str]]
+        ] = []
         source_feature_views: List[Union[FeatureView, OnDemandFeatureView]] = []
 
         # Separate FeatureViews with transformations from regular ones
