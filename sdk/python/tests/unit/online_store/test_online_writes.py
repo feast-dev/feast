46898b5331ae4b5148fe332b8716a4f9d6247739
Casey Clements <casey.clements@mongodb.com>
2026-02-20 13:28:40 -0500

    Format
    Signed-off-by: Casey Clements <casey.clements@mongodb.com>

--- sdk/python/tests/unit/online_store/test_online_writes.py
+++ sdk/python/tests/unit/online_store/test_online_writes.py
@@ -33,9 +33,6 @@ from feast import (
 )
 from feast.driver_test_data import create_driver_hourly_stats_df
 from feast.field import Field
-from feast.infra.online_stores.contrib.mongodb_online_store.mongodb import (
-    MongoDBOnlineStoreConfig,
-)
 from feast.infra.online_stores.sqlite import SqliteOnlineStoreConfig
 from feast.on_demand_feature_view import on_demand_feature_view
 from feast.types import Array, Float32, Float64, Int64, PdfBytes, String, ValueType
