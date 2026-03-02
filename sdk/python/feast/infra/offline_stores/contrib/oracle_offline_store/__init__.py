d4387a883ab4bca6caefd2f9178ff7bc901910d7
Aniket Paluskar <apaluska@redhat.com>
2026-03-02 17:10:10 +0530

    Resolved import error by removing OracleSource import from the __init__
    Signed-off-by: Aniket Paluskar <apaluska@redhat.com>

--- sdk/python/feast/infra/offline_stores/contrib/oracle_offline_store/__init__.py
+++ sdk/python/feast/infra/offline_stores/contrib/oracle_offline_store/__init__.py
@@ -1,13 +1,1 @@
-from feast.infra.offline_stores.contrib.oracle_offline_store.oracle import (
-    OracleOfflineStore,
-    OracleOfflineStoreConfig,
-)
-from feast.infra.offline_stores.contrib.oracle_offline_store.oracle_source import (
-    OracleSource,
-)
 
-__all__ = [
-    "OracleSource",
-    "OracleOfflineStore",
-    "OracleOfflineStoreConfig",
-]
