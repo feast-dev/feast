4dce5bfcce3eed787926b7be7f1ab315180d6451
yuanjun220 <1069645408@qq.com>
2026-03-04 23:12:36 +0800

    fix: Remote Online Store Type Inference Error with All-NULL Columns

--- sdk/python/feast/type_map.py
+++ sdk/python/feast/type_map.py
@@ -1014,9 +1014,6 @@ def python_values_to_proto_values(
         else:
             value_type = python_type_to_feast_value_type("", sample)
 
-    if value_type == ValueType.UNKNOWN:
-        raise TypeError("Couldn't infer value type from empty value")
-
     proto_values = _python_value_to_proto_value(value_type, values)
 
     if len(proto_values) != len(values):
