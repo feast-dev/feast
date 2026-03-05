0d80772361182980068eb5ff5ececb1aee58c0bb
Casey Clements <casey.clements@mongodb.com>
2026-03-05 18:09:23 -0500

    fix: add mongodb to kubebuilder Enum annotations for OnlineStoreDBStorePersistence
    The +kubebuilder:validation:Enum annotation on the Type field of
    OnlineStoreDBStorePersistence was not updated when mongodb was added to
    ValidOnlineStoreDBStorePersistenceTypes. This annotation drives CRD
    OpenAPI schema validation at Kubernetes admission time, so any
    FeatureStore CR specifying type: mongodb would be rejected by the API
    server. Updated both api/v1 and api/v1alpha1.
    
    Signed-off-by: Casey Clements <casey.clements@mongodb.com>

--- infra/feast-operator/api/v1alpha1/featurestore_types.go
+++ infra/feast-operator/api/v1alpha1/featurestore_types.go
@@ -371,7 +371,7 @@ type OnlineStoreFilePersistence struct {
 // OnlineStoreDBStorePersistence configures the DB store persistence for the online store service
 type OnlineStoreDBStorePersistence struct {
 	// Type of the persistence type you want to use.
-	// +kubebuilder:validation:Enum=snowflake.online;redis;datastore;dynamodb;bigtable;postgres;cassandra;mysql;hazelcast;singlestore;hbase;elasticsearch;qdrant;couchbase.online;milvus;hybrid
+	// +kubebuilder:validation:Enum=snowflake.online;redis;datastore;dynamodb;bigtable;postgres;cassandra;mysql;hazelcast;singlestore;hbase;elasticsearch;qdrant;couchbase.online;milvus;hybrid;mongodb
 	Type string `json:"type"`
 	// Data store parameters should be placed as-is from the "feature_store.yaml" under the secret key. "registry_type" & "type" fields should be removed.
 	SecretRef corev1.LocalObjectReference `json:"secretRef"`
