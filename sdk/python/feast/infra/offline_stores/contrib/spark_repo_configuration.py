d33f2f2da9ee25969e98fedf8da12e9b972b3ddd
tokoko <togurgenidze@gmail.com>
2026-03-05 22:34:36 +0400

    chore: pre-commit
    Signed-off-by: tokoko <togurgenidze@gmail.com>

--- sdk/python/feast/infra/offline_stores/contrib/spark_repo_configuration.py
+++ sdk/python/feast/infra/offline_stores/contrib/spark_repo_configuration.py
@@ -1,10 +1,11 @@
 from feast.infra.offline_stores.contrib.spark_offline_store.tests.data_source import (
     SparkDataSourceCreator,
 )
-from tests.universal.feature_repos.repo_configuration import REDIS_CONFIG
 from tests.universal.feature_repos.universal.online_store.redis import (
     RedisOnlineStoreCreator,
 )
+
+REDIS_CONFIG = {"type": "redis", "connection_string": "localhost:6379,db=0"}
 
 AVAILABLE_OFFLINE_STORES = [
     ("local", SparkDataSourceCreator),
