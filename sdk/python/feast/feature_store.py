71ca21af0a9b084228c459ad1e8cd6857088eff5
Vanshika Vanshika <vvanshik@redhat.com>
2026-04-09 18:01:28 +0530

    fix issues
    Signed-off-by: Vanshika Vanshika <vvanshik@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- sdk/python/feast/feature_store.py
+++ sdk/python/feast/feature_store.py
@@ -3003,7 +3003,11 @@ class FeatureStore:
             online_features_response=online_features_response,
             data=requested_features_data,
         )
-        return OnlineResponse(online_features_response)
+        feature_types = {
+            f.name: f.dtype.to_value_type()
+            for f in requested_feature_view.features
+        }
+        return OnlineResponse(online_features_response, feature_types=feature_types)
 
     def retrieve_online_documents_v2(
         self,
@@ -3293,7 +3297,12 @@ class FeatureStore:
             online_features_response.metadata.feature_names.val.extend(
                 features_to_request
             )
-            return OnlineResponse(online_features_response)
+            feature_types = {
+                f.name: f.dtype.to_value_type() for f in table.features
+            }
+            return OnlineResponse(
+                online_features_response, feature_types=feature_types
+            )
 
         table_entity_values, idxs, output_len = utils._get_unique_entities_from_values(
             entity_key_dict,
@@ -3316,7 +3325,10 @@ class FeatureStore:
             data=entity_key_dict,
         )
 
-        return OnlineResponse(online_features_response)
+        feature_types = {
+            f.name: f.dtype.to_value_type() for f in table.features
+        }
+        return OnlineResponse(online_features_response, feature_types=feature_types)
 
     def serve(
         self,
